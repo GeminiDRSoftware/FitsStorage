3823e56be574c69ae0624641efe5bf9e4c9b8dc6
Paul Hirst <phirst@gemini.edu>
2009-11-20 22:02:50 +0000

    updates

--- FitsStorageWebSummary.py
+++ FitsStorageWebSummary.py
@@ -1,4 +1,21 @@
 from FitsStorage import *
+from mod_python import apache
+
+def summary(req, progid, obsid, date):
+  req.content_type = "text/html"
+  req.write("<html>")
+  title = "FITS header summary table"
+  if(len(progid)>0):
+    title += "; Program ID: %s" % (progid)
+  if(len(obsid)>0):
+    title += "; Observation ID: %s" % (obsid)
+  if(len(date)>0):
+    title += "; Date: %s" % (date)
+  req.write("<head><title>%s</title></head>\n" % (title))
+  req.write("<body>")
+  req.write("<H1>%s</H1>" % (title))
+  webhdrsummary(req, list_headers(progid, obsid, date))
+  return apache.OK
 
 def list_headers(progid, obsid, date):
   # We want to select Header object for which diskfile.present is true
@@ -18,16 +35,33 @@ def list_headers(progid, obsid, date):
     # make two datetimes one at start and end of date
     # check it's between these two
 
+  # We should order by datetime
+  query = query.order_by(Header.utdatetime)
+
   # Return the list of DiskFile objects
   return query.all()
 
 def webhdrsummary(req, headers):
   # Given a list of header instances and an apache request oject
-  # Write a header summary to the request object
+  # Write a header summary table to the request object
+  req.write("<TABLE border=1>")
+  req.write("<TR>")
+  req.write("<TH>Filename</TH>")
+  req.write("<TH>Program ID</TH>")
+  req.write("<TH>Observation ID</TH>")
+  req.write("<TH>Data Label</TH>")
+  req.write("<TH>Instrument</TH>")
+  req.write("<TH>Date Time</TH>")
+  req.write("<TH>ObsType</TH>")
+  req.write("</TR>")
   for h in headers:
-    req.write("Filename: %s\n" % (h.diskfile.file.filename))
-    req.write("-- Progid: %s\n" % (h.progid))
-    req.write("-- Obsid: %s\n" % (h.obsid))
-    req.write("-- DataLabel: %s\n" % (h.datalab))
-    req.write("\n")
-
+    req.write("<TR>")
+    req.write("<TD>%s</TD>" % (h.diskfile.file.filename))
+    req.write("<TD>%s</TD>" % (h.progid))
+    req.write("<TD>%s</TD>" % (h.obsid))
+    req.write("<TD>%s</TD>" % (h.datalab))
+    req.write("<TD>%s</TD>" % (h.instrument))
+    req.write("<TD>%s</TD>" % (h.utdatetime))
+    req.write("<TD>%s</TD>" % (h.obstype))
+    req.write("</TR>\n")
+  req.write("</TABLE>\n")
