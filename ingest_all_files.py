ea288dc43968c1b216b908012e666f8afc093bec
Paul Hirst <phirst@gemini.edu>
2009-12-01 22:50:36 +0000

    add file_re option

--- ingest_all_files.py
+++ ingest_all_files.py
@@ -1,6 +1,7 @@
 import FitsStorage
 import FitsStorageUtils
 import os
+import re
 
 from optparse import OptionParser
 
@@ -10,18 +11,29 @@ from optparse import OptionParser
 path=''
 
 fulldirpath = os.path.join(FitsStorage.storage_root, path)
-print "Ingesting all files in: ", fulldirpath
+print "Ingesting files from: ", fulldirpath
 
 parser = OptionParser()
 parser.add_option("--force-crc", action="store_true", dest="force_crc", help="Force crc check on pre-existing files")
+parser.add_option("--file-re", action="store", type="string", dest="file_re", help="python regular expression string to select files by")
 
 (options, args) = parser.parse_args()
 
 filelist = os.listdir(fulldirpath)
-n=len(filelist)
+
+cre=re.compile(options.file_re)
+
+files=[]
+if(options.file_re):
+  for filename in filelist:
+    if(cre.search(filename)):
+      files.append(filename)
+else:
+  files = filelist
 
 i=0
-for filename in filelist:
+n=len(files)
+for filename in files:
   i+=1
   print "-- Ingesting (%d/%d): %s" % (i, n, filename)
   FitsStorageUtils.ingest_file(filename, path, options.force_crc)
