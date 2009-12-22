5358d9a059b5d2dc7e2e5ded2382e49dae19f77e
Paul Hirst <phirst@gemini.edu>
2009-12-22 20:17:56 +0000

    move config to FitsStorageConfig.py

---
+++ FitsStorageConfig.py
@@ -0,0 +1,13 @@
+# These are config parameters that are imported into the FitsStorage namespace
+# We put them in a separate file to ease install issues
+
+
+# Configure the path to the storage root here 
+storage_root = '/net/wikiwiki/dataflow'
+
+# Configure the path the data postgres database here
+fits_database = 'postgres:///fitsdata'
+
+# Configure the site and other misc stuff here
+fits_installation = "fits-install"
+
