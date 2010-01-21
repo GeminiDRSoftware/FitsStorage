d1b26b22fb1547ed8460dcae63f5c27859401da6
fitsdata <fitsdata>
2010-01-21 22:20:36 +0000

    added documentation

--- CadcCRC.py
+++ CadcCRC.py
@@ -1,3 +1,10 @@
+"""
+This is the CadcCRC module. It provides an interface to the CRC function
+used by CADC at the GSA.
+
+The full path filename of the executable to run is contained in the
+cadcCRC_bin string.
+"""
 import subprocess
 import os
 import re
@@ -9,6 +16,10 @@ cadcCRC_bin = '/data/extern/bin/cadcCRC'
 cre=re.compile('\S*\s*([0123456789abcdef]*)\n')
 
 def cadcCRC(filename):
+  """
+  Runs the executable on the specified filename.
+  Retuns a string containing the CRC string.
+  """
   # First check that the filename exists is readable and is a file
   exists = os.access(filename, os.F_OK | os.R_OK)
   isfile = os.path.isfile(filename)
