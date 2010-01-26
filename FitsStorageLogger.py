f7ec96d4250dc2b70f74b521bc32d81fc9634ee1
fitsdata <fitsdata>
2010-01-26 03:25:40 +0000

    pid in format, info level by default

--- FitsStorageLogger.py
+++ FitsStorageLogger.py
@@ -8,14 +8,14 @@ logger = logging.getLogger()
 
 # This is where we set what level messages we want to log.
 # This should default to INFO and be setable to debug with a command line argument
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 # Create log message handlers 
 filehandler=logging.handlers.TimedRotatingFileHandler(FitsStorageConfig.fits_log_file, backupCount=10, when='midnight', interval=1)
 streamhandler=logging.StreamHandler()
 
 # Create log formatter
-formatter = logging.Formatter("%(asctime)s %(module)s:%(lineno)d %(levelname)s: %(message)s")
+formatter = logging.Formatter("%(asctime)s %(process)d:%(module)s:%(lineno)d %(levelname)s: %(message)s")
 
 # Add formater to handlers
 filehandler.setFormatter(formatter)
