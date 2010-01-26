b5facdf817007bcaa45844f9fc443dc9a69d66ac
fitsdata <fitsdata>
2010-01-26 03:53:43 +0000

    options for running as demon and debugging

--- add_to_ingest_queue.py
+++ add_to_ingest_queue.py
@@ -3,7 +3,7 @@ sys.path=['/opt/sqlalchemy/lib/python2.5/site-packages', '/astro/iraf/x86_64/gem
 
 import FitsStorage
 import FitsStorageConfig
-from FitsStorageLogger import logger
+from FitsStorageLogger import *
 from FitsStorageUtils import *
 import os
 import re
@@ -14,7 +14,14 @@ import time
 from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("--file-re", action="store", type="string", dest="file_re", help="python regular expression string to select files by. Special values are today, twoday, fourday to include only files from today, the last two days, or the last four days respectively (days counted as UTC days)")
+parser.add_option("--debug", action="store_true", dest="debug", help="Increase log level to debug")
+parser.add_option("--demon", action="store_true", dest="demon", help="Run as a background demon, do not generate stdout")
+
 (options, args) = parser.parse_args()
+
+# Logging level to debug? Include stdio log?
+setdebug(options.debug)
+setdemon(options.demon)
 
 # Annouce startup
 now = datetime.datetime.now()
