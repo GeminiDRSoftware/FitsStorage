2ce47b2e839e1c23477281b243dc631d2c0d30e5
fitsdata <fitsdata>
2010-01-26 03:50:16 +0000

    use logger

--- FitsStorageUtils.py
+++ FitsStorageUtils.py
@@ -61,19 +61,18 @@ def ingest_file(session, filename, path, force_crc, skip_fv, skip_wmd):
 
   # First check if the file exists
   if(not(file.exists())):
-    print "cannot access ", file.fullpath
+    logger.error("cannot access ", file.fullpath)
     return
 
   # Check if this filename is already in the database
 
   query = session.query(File).filter(File.filename==file.filename).filter(File.path==file.path)
   if(query.first()):
-    #print "Already in file table"
+    logger.debug("Already in file table")
     # This will throw an error if there is more than one entry
-    # There's a way to handle that nicely internally if we want.
     file = query.one()
   else:
-    print "Adding new file table entry"
+    logger.debug("Adding new file table entry")
     file = File(filename, path)
     session.add(file)
     session.commit();
@@ -82,38 +81,38 @@ def ingest_file(session, filename, path, force_crc, skip_fv, skip_wmd):
   query = session.query(DiskFile).filter(DiskFile.file_id==file.id).filter(DiskFile.present==True)
   if(query.first()):
     # Yes, it's already there.
-    #print "already present in diskfile table..."
+    logger.debug("already present in diskfile table...")
     # Ensure there's only one and get an instance of it
     diskfile = query.one()
     # Has the file changed since we last recorded it?
     # By default check lastmod time first
     # there is a subelty wrt timezones here.
     if((diskfile.lastmod.replace(tzinfo=None) != diskfile.file.lastmod()) or force_crc):
-      #print "lastmod time indicates file modification"
+      logger.debug("lastmod time indicates file modification")
       # Check the CRC to be sure if it's changed
       if(diskfile.ccrc == diskfile.file.ccrc()):
-        #print "crc indicates no change"
+        logger.debug("crc indicates no change")
         add_diskfile=0
       else:
-        print "crc indicates file has changed - reingesting"
+        logger.debug("crc indicates file has changed - reingesting")
         # Set the present flag on the current one to false and create a new entry
         diskfile.present=False
         add_diskfile=1
     else:
-      #print "lastmod time indicates file unchanged, not checking further"
+      logger.debug("lastmod time indicates file unchanged, not checking further")
       add_diskfile=0
 
   else:
     # No not present, insert into diskfile table
-    #print "No DiskFile exists"
+    logger.debug("No DiskFile exists")
     add_diskfile=1
   
   if(add_diskfile):
-    print "Adding new DiskFile entry"
+    logger.debug("Adding new DiskFile entry")
     diskfile = DiskFile(file, skip_fv, skip_wmd)
     session.add(diskfile)
     session.commit()
-    print "Adding new Header entry"
+    logger.debug("Adding new Header entry")
     header = Header(diskfile)
     session.add(header)
     session.commit()
