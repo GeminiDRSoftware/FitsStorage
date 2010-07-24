860c76a903c815ccf877bd3a9fc40b5569cdc731
fitsdata <fitsdata>
2010-07-24 00:20:04 +0000

    close the http connection explicitly

--- MissingCalibrationEmail.py
+++ MissingCalibrationEmail.py
@@ -4,6 +4,13 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import datetime
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("--emailto", action="store", dest="toaddr", default="gnda@gemini.edu", help="Email Address to send to")
+parser.add_option("--emailfrom", action="store", dest="fromaddr", default="fitsdata@gemini.edu", help="Email Address to send from")
+parser.add_option("--replyto", action="store", dest="replyto", default="gnda@gemini.edu", help="Set a Reply-To email header")
+(options, args) = parser.parse_args()
 
 # Work out the date range to query
 utcnow = datetime.datetime.utcnow()
@@ -13,7 +20,9 @@ daterange="%s-%s" % (utcthen.date().strftime("%Y%m%d"), utcnow.date().strftime("
 
 url = "http://fits/calibrations/GMOS/Win/%s/arc/warnings" % daterange
 
-html = urllib2.urlopen(url).read()
+f = urllib2.urlopen(url)
+html = f.read()
+f.close()
 
 cremissing = re.compile('Counted (\d*) potential missing Calibrations')
 crewarning = re.compile('Query generated (\d*) warnings')
@@ -22,8 +31,6 @@ warnings = int(crewarning.search(html).group(1))
 missing = int(cremissing.search(html).group(1))
 
 mailhost = "smtp.gemini.edu"
-fromaddr = "fitsdata@gemini.edu"
-toaddr = "gnda@gemini.edu"
 
 if(missing==0):
   subject = "No missing calibrations this week. Yay!"
@@ -38,12 +45,13 @@ part1 = MIMEText(text, 'plain')
 part2 = MIMEText(html, 'html')
 
 msg['Subject'] = subject
-msg['From'] = fromaddr
-msg['To'] = toaddr
+msg['From'] = options.fromaddr
+msg['To'] = options.toaddr
+msg['Reply-To'] = options.replyto
 
 msg.attach(part1)
 msg.attach(part2)
 
 smtp = smtplib.SMTP("smtp.gemini.edu")
-smtp.sendmail(fromaddr, [toaddr], msg.as_string())
+smtp.sendmail(options.fromaddr, [options.toaddr], msg.as_string())
 smtp.quit()
