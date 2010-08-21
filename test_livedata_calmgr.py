58ea7a4743a531718edc9b91d653bedbed9d9503
fitsdata <fitsdata>
2010-08-21 13:03:25 +0000

    tidy

--- test_livedata_calmgr.py
+++ test_livedata_calmgr.py
@@ -3,8 +3,8 @@ sys.path.append("/opt/gemini_python")
 from astrodata import AstroData
 import urllib
 
+# This is a GMOS_N imaging science dataset
 ad = AstroData("/net/wikiwiki/dataflow/N20100820S0241.fits")
-
 
 desc_dict = {'instrument':ad.instrument(),
              'data_label':ad.data_label(),
@@ -17,18 +17,13 @@ desc_dict = {'instrument':ad.instrument(),
              'ut_time':ad.ut_time(),
              }
 type_list = ad.types
-
 ad.close()
 
 sequence = (('descriptors', desc_dict), ('types', type_list))
-
 postdata = urllib.urlencode(sequence)
 
 url = "http://hbffits1/calmgr/processed_bias/"
-
 u = urllib.urlopen(url, postdata)
-
 print u.read()
-
 u.close()
 
