ade03de3a173951775c15c42df9a49457ea43923
fitsdata <fitsdata>
2010-12-28 19:30:01 +0000

    add basic email

--- delete_files.py
+++ delete_files.py
@@ -7,10 +7,10 @@ from FitsStorageLogger import *
 from FitsStorageUtils import *
 import CadcCRC
 import datetime
-
-
 import urllib
 from xml.dom.minidom import parseString
+import os
+import smtplib
 
 
 # Option Parsing
@@ -19,11 +19,17 @@ parser = OptionParser()
 parser.add_option("--tapeserver", action="store", type="string", dest="tapeserver", default="hbffitstape1", help="The Fits Storage Tape server to use to check the files are on tape")
 parser.add_option("--path", action="store", type="string", dest="path", default="", help="Path within the storage root")
 parser.add_option("--file-pre", action="store", type="string", dest="filepre", help="File prefix to operate on, eg N20090130, N200812 etc")
+parser.add_option("--maxnum", type="int", action="store", dest="maxnum", help="Delete at most X files.")
+parser.add_option("--maxgb", type="float", action="store", dest="maxgb", help="Delete at most X GB of files")
+parser.add_option("--auto", action="store_true", dest="auto", help="Delete old files to get to pre-defined free space")
+parser.add_option("--oldbyfilename", action="store_true", dest="oldbyfilename", help="Sort by filename to determine oldest files")
+parser.add_option("--oldbylastmod", action="store_true", dest="oldbylastmod", help="Sort by lastmod to determine oldest files")
 parser.add_option("--yesimsure", action="store_true", dest="yesimsure", help="Needed when file count is large")
 parser.add_option("--notpresent", action="store_true", dest="notpresent", help="Include files that are marked as not present")
 parser.add_option("--mintapes", action="store", type="int", dest="mintapes", default=2, help="Minimum number of tapes file must be on to be eligable for deletion")
 parser.add_option("--skip-crc", action="store_true", dest="skipcrc", help="Do not bother to verify the crc of the file on disk")
 parser.add_option("--dryrun", action="store_true", dest="dryrun", help="Dry Run - do not actually do anything")
+parser.add_option("--emailto", action="store", type="string", dest="emailto", help="Email address to send message to")
 parser.add_option("--debug", action="store_true", dest="debug", help="Increase log level to debug")
 parser.add_option("--demon", action="store_true", dest="demon", help="Run as a background demon, do not generate stdout")
 
@@ -34,16 +40,53 @@ setdebug(options.debug)
 setdemon(options.demon)
 
 
+msg = ""
 # Annouce startup
 logger.info("*********  delete_files.py - starting up at %s" % datetime.datetime.now())
 
 session = sessionfactory()
-likestr = "%s%%" % options.filepre
-query = session.query(DiskFile.id).select_from(join(File, DiskFile)).filter(File.filename.like(likestr)).filter(DiskFile.canonical==True)
+
+query = session.query(DiskFile.id).select_from(join(File, DiskFile)).filter(DiskFile.canonical==True)
+
+if(options.auto):
+  # chdir to the storage root to kick the automounter
+  cwd = os.getcwd()
+  os.chdir(storage_root)
+  s = os.statvfs(storage_root)
+  os.chdir(cwd)
+  gbavail = s.f_bsize * s.f_bavail / (1024 * 1024 * 1024)
+  numfiles = s.f_files - s.f_favail
+  logger.debug("Disk has %d files and %.2f GB available" % (numfiles, gbavail))
+  numtodelete = numfiles - target_max_files
+  if(numtodelete > 0):
+    logger.debug("Need to delete %d files" % numtodelete)
+    if(options.maxnum and (numtodelete < options.maxnum)):
+      options.maxnum = numtodelete
+    else:
+      options.maxnum = numtodelete
+
+  gbtodelete = target_gb_free - gbavail
+  if(gbtodelete > 0):
+    logger.debug("Need to delete %.2f GB" % gbtodelete)
+    if(options.maxgb and (gbtodelete < options.maxgb)):
+      options.maxgb = gbtodelete
+    else:
+      options.maxgb = gbtodelete
+
+if(options.filepre):
+  likestr = "%s%%" % options.filepre
+  query = query.filter(File.filename.like(likestr))
+
 if(not options.notpresent):
   query = query.filter(DiskFile.present==True)
 
-query = query.order_by(File.filename)
+if(options.oldbyfilename):
+  query = query.order_by(File.filename)
+else:
+  query = query.order_by(desc(DiskFile.lastmod))
+
+if(options.maxnum):
+  query = query.limit(options.maxnum)
 
 diskfileids = query.all()
 
@@ -57,6 +100,8 @@ if(len(diskfileids) > 2000 and not options.yesimsure):
   logger.error("To proceed with this many files, you must say --yesimsure")
   session.close()
   sys.exit(1)
+
+sumbytes = 0;
 
 for diskfileid in diskfileids:
 
@@ -104,12 +149,14 @@ for diskfileid in diskfileids:
           logger.debug("Found it on tape id %d" % tapeid)
           tapeids.append(tapeid)
 
-      logger.info("File %s - %s is on %d tapes: %s" % (dbfilename, fileccrc, len(tapeids), tapeids))
       if(len(tapeids) >= options.mintapes):
+        sumbytes += diskfile.size
         if(options.dryrun):
-          logger.info("Dry run - not actually deleting file %s" % fullpath)
+          logger.info("Dry run - not actually deleting File %s - %s which is on %d tapes: %s" % (fullpath, fileccrc, len(tapeids), tapeids))
+          msg+="Dry run - not deleting File %s - %s which is on %d tapes: %s\n" % (fullpath, fileccrc, len(tapeids), tapeids)
         else:
-          logger.info("Deleting File %s" % fullpath)
+          logger.info("Deleting File %s - %s which is on %d tapes: %s" % (fullpath, fileccrc, len(tapeids), tapeids))
+          msg+="Deleting File %s - %s which is on %d tapes: %s\n" % (fullpath, fileccrc, len(tapeids), tapeids) 
           try:
             os.unlink(fullpath)
             logger.debug("Marking diskfile id %d as not present" % diskfile.id)
@@ -119,6 +166,26 @@ for diskfileid in diskfileids:
             logger.error("Could not unlink file %s: %s - %s" % (fullpath, sys.exc_info()[0], sys.exc_info()[1]))
       else:
         logger.info("File %s is not on sufficient tapes to be elligable for deletion" % dbfilename)
+    if(options.maxgb):
+      sumgb = sumbytes / 1.0E9
+      if(sumgb>options.maxgb):
+        logger.info("Allready deleted %.2f GB - stopping now" % sumgb)
+        break
 
 session.close()
+if(options.emailto):
+  if(options.dryrun):
+    subject="Dry run file delete report"
+  else:
+    subject="File delete report"
+
+  mailfrom='fitsdata@gemini.edu'
+  mailto=[options.emailto]
+
+  message = "From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n%s" % (mailfrom, ", ".join(mailto), subject, msg)
+
+  server = smtplib.SMTP('mail.gemini.edu')
+  server.sendmail(mailfrom, mailto, message)
+  server.quit()
+
 logger.info("**delete_files.py exiting normally")
