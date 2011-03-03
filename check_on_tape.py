d4a62d0d1089cc6f659a370b81d38f820f971803
fitsdata <fitsdata>
2011-03-03 00:33:07 +0000

    added tapeset and moved to md5

--- check_on_tape.py
+++ check_on_tape.py
@@ -20,6 +20,7 @@ parser.add_option("--tapeserver", action="store", type="string", dest="tapeserve
 parser.add_option("--file-pre", action="store", type="string", dest="filepre", help="File prefix to operate on, eg N20090130, N200812 etc")
 parser.add_option("--notpresent", action="store_true", dest="notpresent", help="Include files that are marked as not present")
 parser.add_option("--mintapes", action="store", type="int", dest="mintapes", default=2, help="Minimum number of tapes file must be on to be eligable for deletion")
+parser.add_option("--tapeset", action="store", type="int", dest="tapset", help="Only consider tapes in this tapeset")
 parser.add_option("--debug", action="store_true", dest="debug", help="Increase log level to debug")
 parser.add_option("--demon", action="store_true", dest="demon", help="Run as a background demon, do not generate stdout")
 
@@ -64,7 +65,7 @@ for diskfileid in diskfileids:
   diskfile = session.query(DiskFile).filter(DiskFile.id == diskfileid).one()
 
   fullpath = diskfile.file.fullpath()
-  dbccrc = diskfile.ccrc
+  dbmd5 = diskfile.md5
   dbfilename = diskfile.file.filename
 
   url = "http://%s/fileontape/%s" % (options.tapeserver, dbfilename)
@@ -81,19 +82,23 @@ for diskfileid in diskfileids:
   tapeids = []
   for fe in fileelements:
     filename = fe.getElementsByTagName("filename")[0].childNodes[0].data
-    ccrc = fe.getElementsByTagName("ccrc")[0].childNodes[0].data
+    md5 = fe.getElementsByTagName("md5")[0].childNodes[0].data
     tapeid = int(fe.getElementsByTagName("tapeid")[0].childNodes[0].data)
-    logger.debug("Filename: %s; ccrc=%s, tapeid=%d" % (filename, ccrc, tapeid))
-    if((filename == dbfilename) and (ccrc == dbccrc) and (tapeid not in tapeids)):
+    tapeset = int(fe.getElementsByTagName("tapeset")[0].childNodes[0].data)
+    logger.debug("Filename: %s; md5=%s, tapeid=%d, tapeset=%d" % (filename, md5, tapeid, tapeset))
+    if((filename == dbfilename) and (md5 == dbmd5) and (tapeid not in tapeids)):
       logger.debug("Found it on tape id %d" % tapeid)
-      tapeids.append(tapeid)
+      if(options.tapeset and tapeset != options.tapeset):
+        logger.debug("But this tape id is not in the requested tapeset")
+      else:
+        tapeids.append(tapeid)
 
   if(len(tapeids) < options.mintapes):
     sumbytes += diskfile.size
     sumfiles += 1
-    logger.info("*** File %s - %s needs to go to tape, it is on %d tapes: %s" % (fullpath, dbccrc, len(tapeids), tapeids))
+    logger.info("*** File %s - %s needs to go to tape, it is on %d tapes: %s" % (fullpath, dbmd5, len(tapeids), tapeids))
   else:
-    logger.info("File %s - %s is OK, it already is on %d tapes: %s" % (fullpath, dbccrc, len(tapeids), tapeids))
+    logger.info("File %s - %s is OK, it already is on %d tapes: %s" % (fullpath, dbmd5, len(tapeids), tapeids))
     
 
 logger.info("Found %d files totalling %.2f GB that should go to tape" % (sumfiles, sumbytes/1.0E9))
