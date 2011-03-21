80c52f298b331644cab7062d507aad351f3299cd
pclementson <pclementson>
2011-03-21 22:06:07 +0000

    updated to match astrodata labels

--- test_livedata_calmgr.py
+++ test_livedata_calmgr.py
@@ -10,11 +10,12 @@ desc_dict = {'instrument':ad.instrument(),
              'data_label':ad.data_label(),
              'detector_x_bin':ad.detector_x_bin(),
              'detector_y_bin':ad.detector_y_bin(),
-             'read_speed_mode':ad.read_speed_mode(),
-             'gain_mode':ad.gain_mode(),
-             'amp_read_area':ad.amp_read_area(asList=True),
-             'ut_date':ad.ut_date(),
-             'ut_time':ad.ut_time(),
+             'read_speed_setting':ad.read_speed_setting(),
+             'gain_setting':ad.gain_setting(),
+             'amp_read_area':ad.amp_read_area(asDict=True),
+             'ut_datetime':ad.ut_datetime(),
+             'exposure_time':ad.exposure_time(),
+             'nodandshuffle':ad.isType('GMOS_NODANDSHUFFLE')
              }
 type_list = ad.types
 ad.close()
@@ -22,7 +23,8 @@ ad.close()
 sequence = (('descriptors', desc_dict), ('types', type_list))
 postdata = urllib.urlencode(sequence)
 
-url = "http://hbffits1/calmgr/processed_bias/"
+url = "http://hbffits3/calmgr/processed_bias/"
+#url = "http://hbffits1/calmgr/processed_bias/"
 u = urllib.urlopen(url, postdata)
 print u.read()
 u.close()
