6f5a5c01a9a7dfd6900fba70ec717bf11284b61e
fitsdata <fitsdata>
2011-06-09 23:31:34 +0000

    updates

--- request_from_tape.py
+++ request_from_tape.py
@@ -20,6 +20,7 @@ from xml.dom.minidom import parseString
 from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("--file-re", action="store", type="string", dest="filere", help="Regular expression used to select files to extract")
+parser.add_option("--tape-label", action="store", type="string", dest="tape_label", help="Request only files that are on this tape.")
 parser.add_option("--all", action="store_true", dest="all", help="When multiple versions of a file are on tape, get them all, not just the most recent")
 parser.add_option("--dryrun", action="store_true", dest="dryrun", help="Dry Run - do not actually do anything")
 parser.add_option("--debug", action="store_true", dest="debug", help="Increase log level to debug")
@@ -35,56 +36,59 @@ requester = options.requester
 # Annouce startup
 logger.info("*********  request_from_tape.py - starting up at %s" % datetime.datetime.now())
 
-if(not options.filere):
-  logger.error("You must specify a file-re")
+if((not options.filere) and (not options.tape_label)):
+  logger.error("You must specify a file-re or a tape-label. You probably want a file-re")
   sys.exit(1)
-
-# Only populate one day at a time or a sequence of days separated by semicolons!!!
-date = re.sub("\D", "", options.filere)
-if(len(date)!=8):
-  logger.info("You must specify one day at a time otherwise this script executes really slowly!")
-  logger.info("Hit ctrl-c within 5 secs to abort")
-  time.sleep(8)
 
 # Query the DB to find a list of files to extract
 # This is a little non trivial, given that there are multiple identical
 # copies of the file on several tapes and also that there can be multiple
 # non identical version of the file on tapes too.
+# OK. In the first pass, we simply get a list of filenames (that exist on valid tapewrites on valid tapes) that we can extract. Then we do another pass to figure out which version of each file we are going to extract.
 session = sessionfactory()
 
-
-# First, we want to query the Tape, TapeWrite and TapeFile tables to populate TapeRead
 #query = session.query(TapeFile).select_from(join(TapeFile, join(TapeWrite, Tape)))
-query = session.query(TapeFile).select_from(Tape, TapeWrite, TapeFile)
+# for some reason, the join doesn't work - do it manually:
+query = session.query(TapeFile.filename).select_from(Tape, TapeWrite, TapeFile)
 query = query.filter(Tape.id == TapeWrite.tape_id).filter(TapeWrite.id == TapeFile.tapewrite_id)
-query = query.filter(TapeFile.filename.like('%'+options.filere+'%'))
+
+# Match against the given filere
+if(options.filere):
+  query = query.filter(TapeFile.filename.like('%'+options.filere+'%'))
+
+if(options.tape_label):
+  query = query.filter(Tape.label == options.tape_label)
+
+# Other housekeeping checks
 query = query.filter(TapeWrite.suceeded == True)
 query = query.filter(Tape.active == True)
-query = query.order_by(TapeFile.filename, desc(TapeFile.lastmod))
+
 query = query.distinct()
-listed = query.all()
 
-# Now, where the same filename occurs with multiple md5s, we should weed out
-# the ones we don't want while populating the TapeRead columns
-i=0
-previous_file = ''
-if(not options.all):
-  for que in listed:
-    i+=1
-    this_file = que.filename
-    if(previous_file != this_file):
-      tr = TapeRead()
-      tr.tapefile_id = que.id
-      tr.filename = que.filename
-      tr.md5 = que.md5
-      tr.tape_id = que.tapewrite.tape.id
-      tr.tape_label = que.tapewrite.tape.label
-      tr.filenum = que.tapewrite.filenum
-      tr.requester = options.requester
-      session.add(tr)
-      logger.info("adding file %s to taperead: (%d in queue)" % (que.filename, i))
-      session.commit()
-    previous_file = this_file
+filenames = query.all()
+
+# OK, now we loop through those filenames, finding the md5 for the version with the most recent lastmod
+
+for filename in filenames:
+  query = session.query(TapeFile.md5).select_from(Tape, TapeWrite, TapeFile)
+  query = query.filter(Tape.id == TapeWrite.tape_id).filter(TapeWrite.id == TapeFile.tapewrite_id)
+
+  query = query.filter(TapeWrite.suceeded == True).filter(Tape.active == True)
+
+  query = query.filter(TapeFile.filename == filename[0])
+  query = query.order_by(desc(TapeFile.lastmod))
+
+  md5 = query.first()
+
+  # Right, add the filename, md5 to the taperead table
+  tr = TapeRead()
+  tr.filename = filename[0]
+  tr.md5 = md5[0]
+  logger.info("Adding %s, %s to taperead", filename[0], md5[0])
+
+  session.add(tr)
+  session.commit()
+
+
 session.close()
-
 
