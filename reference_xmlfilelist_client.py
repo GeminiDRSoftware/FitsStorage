a223b1386a7f58f51b683c77c2a1a63858b04150
Paul Hirst <phirst@gemini.edu>
2011-06-22 23:42:22 +0000

    updates

--- reference_xmlfilelist_client.py
+++ reference_xmlfilelist_client.py
@@ -1,7 +1,7 @@
 import urllib
 from xml.dom.minidom import parseString
 
-# For example:
+# Data selection. For example:
 selection = "today/GMOS/BIAS"
 
 url = "http://fits/xmlfilelist/" + selection
@@ -20,6 +20,10 @@ for fe in dom.getElementsByTagName("file"):
   dict['lastmod']=fe.getElementsByTagName("lastmod")[0].childNodes[0].data
   files.append(dict)
 
-numfiles = len(files)
+# files is now a list, where each element in the list is a dictionary representing a fits file, and having 'filename', 'size', 'lastmod' etc keys.
 
+numfiles = len(files)
 print "Got %d files" % numfiles
+
+for file in files:
+  print "Filename: %s   size: %d   last_modification: %s" % (file['filename'], file['size'], file['lastmod'])
