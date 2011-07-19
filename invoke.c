b716348e2c29bddd441d6b5135de24d4bb52615c
fitsdata <fitsdata>
2011-07-19 22:45:09 +0000

    update to new python

--- invoke.c
+++ invoke.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 int main(int argc, char **argv) {
-  execv("/astro/iraf/x86_64/gempylocal/bin/python", argv);
+  execv("/astro/iraf/rhux-x86_64-glibc2.5/gempylocal/bin/python", argv);
   return 0;
 }
 
