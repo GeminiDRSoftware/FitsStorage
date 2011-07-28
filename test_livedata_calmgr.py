e601c2cb7ec9a7cadc59e347331a17dc812a262b
fitsdata <fitsdata>
2011-07-28 21:50:41 +0000

    updates

--- test_livedata_calmgr.py
+++ test_livedata_calmgr.py
@@ -1,21 +1,22 @@
-import sys
-sys.path.append("/opt/gemini_python")
 from astrodata import AstroData
 import urllib
 
 # This is a GMOS_N imaging science dataset
-ad = AstroData("/net/wikiwiki/dataflow/N20100820S0241.fits")
+ad = AstroData("/net/wikiwiki/dataflow/N20110728S0350.fits")
 
-desc_dict = {'instrument':ad.instrument(),
-             'data_label':ad.data_label(),
-             'detector_x_bin':ad.detector_x_bin(),
-             'detector_y_bin':ad.detector_y_bin(),
-             'read_speed_setting':ad.read_speed_setting(),
-             'gain_setting':ad.gain_setting(),
-             'amp_read_area':ad.amp_read_area(asDict=True),
-             'ut_datetime':ad.ut_datetime(),
-             'exposure_time':ad.exposure_time(),
-             'nodandshuffle':ad.isType('GMOS_NODANDSHUFFLE')
+desc_dict = {'instrument':ad.instrument().for_db(),
+             'observation_type':ad.observation_type().for_db(),
+             'data_label':ad.data_label().for_db(),
+             'detector_x_bin':ad.detector_x_bin().for_db(),
+             'detector_y_bin':ad.detector_y_bin().for_db(),
+             'read_speed_setting':ad.read_speed_setting().for_db(),
+             'gain_setting':ad.gain_setting().for_db(),
+             'amp_read_area':ad.amp_read_area().for_db(),
+             'ut_datetime':ad.ut_datetime().for_db(),
+             'exposure_time':ad.exposure_time().for_db(),
+             'object':ad.object().for_db(),
+             'filter_name':ad.filter_name().for_db(),
+             'focal_plane_mask':ad.focal_plane_mask().for_db(),
              }
 type_list = ad.types
 ad.close()
@@ -23,8 +24,8 @@ ad.close()
 sequence = (('descriptors', desc_dict), ('types', type_list))
 postdata = urllib.urlencode(sequence)
 
+#url = "http://hbffits3/calmgr/processed_flat/"
 url = "http://hbffits3/calmgr/processed_bias/"
-#url = "http://hbffits1/calmgr/processed_bias/"
 u = urllib.urlopen(url, postdata)
 print u.read()
 u.close()
