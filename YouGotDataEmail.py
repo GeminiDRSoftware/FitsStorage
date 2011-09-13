7d3be88fbbb0941b5427ab3fef9904f97b35cf72
fitsdata <fitsdata>
2011-09-13 19:32:17 +0000

    Check for critical fields being None

--- YouGotDataEmail.py
+++ YouGotDataEmail.py
@@ -38,60 +38,65 @@ notifs = session.query(Notification).all()
 
 for notif in notifs:
 
-  if(notif.internal):
-    url = "http://fits/summary/%s/%s" % (options.date, notif.selection)
+  if((notif.selection is None) or (notif.to is None)):
+    logger.error("Critical fields are None in notification id: %s; label: %s" % (notif.id, notif.label))
+
   else:
-    url = "http://fits/summary/nolinks/%s/%s" % (options.date, notif.selection)
 
-  logger.debug("URL is: %s" % url)
-  f = urllib2.urlopen(url)
-  html = f.read()
-  f.close()
-
-  match = cre.search(html)
-
-  if(match):
-
-    subject = "New Data for %s" % notif.selection
-    logger.info(subject)
-
-    msg = MIMEMultipart()
-
-    text = "New data has been taken for %s. The attached html file gives details.\n\n" % (notif.selection)
     if(notif.internal):
-      text += "The fits storage summary table for this data be found at: %s\n\n" % url
+      url = "http://fits/summary/%s/%s" % (options.date, notif.selection)
     else:
-      text += "Access to all Gemini data is via the Gemini Science Archive at http://www1.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/gsa/\n"
-      text += "Data Quality assessment and data package release will proceed as normal over the next few days."
+      url = "http://fits/summary/nolinks/%s/%s" % (options.date, notif.selection)
 
-    part1 = MIMEText(text, 'plain')
-    part2 = MIMEText(html, 'html')
+    logger.debug("URL is: %s" % url)
+    f = urllib2.urlopen(url)
+    html = f.read()
+    f.close()
 
-    msg['Subject'] = subject
-    msg['From'] = options.fromaddr
-    msg['To'] = notif.to
-    msg['Cc'] = notif.cc
-    msg['Reply-To'] = options.replyto
+    match = cre.search(html)
 
-    msg.attach(part1)
-    msg.attach(part2)
+    if(match):
 
-    fulllist = []
-    tolist = notif.to.split(',')
-    fulllist += tolist
-    if(notif.cc):
-      cclist = notif.cc.split(',')
-      fulllist += cclist
-    # For now, I Bcc myself on all the emails to see that it's working...
-    fulllist.append('phirst@gemini.edu')
+      subject = "New Data for %s" % notif.selection
+      logger.info(subject)
 
-    try:
-      logger.info("Sending Email- To: %s; CC: %s; Subject: %s" % (msg['To'], msg['Cc'], msg['Subject']))
-      smtp = smtplib.SMTP(mailhost)
-      smtp.sendmail(options.fromaddr, fulllist, msg.as_string())
-      retval = smtp.quit()
-      logger.info("SMTP seems to have worked OK: %s" % str(retval))
-    except smtplib.SMTPRecipientsRefused:
-      logger.error("Error sending mail message - Exception: %s: %s" % (sys.exc_info()[0], sys.exc_info()[1]))
+      msg = MIMEMultipart()
+
+      text = "New data has been taken for %s. The attached html file gives details.\n\n" % (notif.selection)
+      if(notif.internal):
+        text += "The fits storage summary table for this data be found at: %s\n\n" % url
+      else:
+        text += "Access to all Gemini data is via the Gemini Science Archive at http://www1.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/gsa/\n"
+        text += "Data Quality assessment and data package release will proceed as normal over the next few days."
+
+      part1 = MIMEText(text, 'plain')
+      part2 = MIMEText(html, 'html')
+
+      msg['Subject'] = subject
+      msg['From'] = options.fromaddr
+      msg['To'] = notif.to
+      msg['Cc'] = notif.cc
+      msg['Reply-To'] = options.replyto
+
+      msg.attach(part1)
+      msg.attach(part2)
+
+      fulllist = []
+      tolist = notif.to.split(',')
+      fulllist += tolist
+      if(notif.cc):
+        cclist = notif.cc.split(',')
+        fulllist += cclist
+      # For now, I Bcc myself on all the emails to see that it's working...
+      fulllist.append('phirst@gemini.edu')
+
+      try:
+        logger.info("Sending Email- To: %s; CC: %s; Subject: %s" % (msg['To'], msg['Cc'], msg['Subject']))
+        smtp = smtplib.SMTP(mailhost)
+        smtp.sendmail(options.fromaddr, fulllist, msg.as_string())
+        retval = smtp.quit()
+        logger.info("SMTP seems to have worked OK: %s" % str(retval))
+      except smtplib.SMTPRecipientsRefused:
+        logger.error("Error sending mail message - Exception: %s: %s" % (sys.exc_info()[0], sys.exc_info()[1]))
 
 logger.info("YouveGotDataEmail.py exiting normally")
