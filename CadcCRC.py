f5a5b78c18ffa2eb333a7731b9d0ba0f9aa07e06
fitsdata <fitsdata>
2012-09-28 02:08:22 +0000

    add gsa_get_info function to get md5sum and ingest date from gsa

--- CadcCRC.py
+++ CadcCRC.py
@@ -10,6 +10,8 @@ This module also contains an md5sum function for convenience.
 import subprocess
 import os
 import re
+import urllib2
+import dateutil.parser
 
 # the path to the fitsverify binary
 cadcCRC_bin = '/opt/cadc/cadcCRC'
@@ -68,3 +70,46 @@ def md5sum(filename):
   return m
 
 
+def get_gsa_info(filename, user, passwd):
+  """
+  Queries the GSA for the given filename, using authentication details provided.
+  returns a dict containing md5sum and ingestdate keys
+  """
+
+  # Create an authenticated urllib2 request object set to do http HEAD
+  class HeadRequest(urllib2.Request):
+    def get_method(self):
+      return "HEAD"
+  auth_handler = urllib2.HTTPBasicAuthHandler()
+  auth_handler.add_password(realm='Canadian Astronomy Data Centre', uri='http://www.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/data/auth/GEMINI', user=user, passwd=passwd)
+  opener = urllib2.build_opener(auth_handler)
+  urllib2.install_opener(opener)
+
+  # Form the URL and get the http header. Doesn't fetch the actual data
+  url = 'http://www.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/data/auth/GEMINI/%s' % filename
+
+  request = HeadRequest(url)
+  try:
+    response = urllib2.urlopen(request)
+    headers = response.info()
+    response.close()
+    http_error=0
+  except urllib2.HTTPError:
+    http_error=1
+
+  # Make the empty return dictionary
+  dict = {}
+
+  if http_error:
+    dict['md5sum']=None
+    dict['ingestdate']=None
+  else:
+    # Put the MD5sum from the header into the dict
+    dict['md5sum']=response.headers.get('X-Uncompressed-MD5')
+
+    # Get the ingest date string and parse it into the dict
+    ids = response.headers.get('Last-Modified')
+    id = dateutil.parser.parse(ids)
+    dict['ingestdate'] = id
+
+  return dict
