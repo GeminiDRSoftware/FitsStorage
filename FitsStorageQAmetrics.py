383789d7ddf798de44cc6cc7dfcfa99fe361edac
fitsdata <fitsdata>
2013-01-09 01:06:02 +0000

    initial json

--- FitsStorageQAmetrics.py
+++ FitsStorageQAmetrics.py
@@ -9,107 +9,122 @@ import ApacheReturnCodes as apache
 
 def qareport(req):
   """
-  This function handles submnission of QA metric reports
+  This function handles submission of QA metric reports via xml and json
   """
-  session = sessionfactory()
-  try:
-    if(req.method == 'GET'):
-      return apache.HTTP_NOT_ACCEPTABLE
+  if(req.method == 'GET'):
+    return apache.HTTP_NOT_ACCEPTABLE
  
-    if(req.method == 'POST'):
-      clientdata = req.read()
-      clientstr = urllib.unquote(clientdata)
-      req.log_error(clientstr)
-      dom = parseString(clientstr)
-      for qr in dom.getElementsByTagName("qareport"):
-        qareport = QAreport()
-        qareport.hostname=get_value(qr.getElementsByTagName("hostname"))
-        qareport.userid=get_value(qr.getElementsByTagName("userid"))
-        qareport.processid=get_value(qr.getElementsByTagName("processid"))
-        qareport.executable=get_value(qr.getElementsByTagName("executable"))
-        qareport.software=get_value(qr.getElementsByTagName("software"))
-        qareport.software_version=get_value(qr.getElementsByTagName("software_version"))
-        qareport.context=get_value(qr.getElementsByTagName("context"))
-        qareport.submit_time = datetime.datetime.now()
-        qareport.submit_host = req.get_remote_host(apache.REMOTE_NAME)
-        session.add(qareport)
-        session.commit()
-        for qam in qr.getElementsByTagName("qametric"):
-          datalabel = get_value(qam.getElementsByTagName("datalabel"))
-          filename = get_value(qam.getElementsByTagName("filename"))
-          detector = get_value(qam.getElementsByTagName("detector"))
-          for iq in qam.getElementsByTagName("iq"):
-            qametriciq = QAmetricIQ(qareport)
-            qametriciq.datalabel = datalabel
-            qametriciq.filename = filename
-            qametriciq.detector = detector
-            qametriciq.fwhm = get_value(iq.getElementsByTagName("fwhm"))
-            qametriciq.fwhm_std = get_value(iq.getElementsByTagName("fwhm_std"))
-            qametriciq.isofwhm = get_value(iq.getElementsByTagName("isofwhm"))
-            qametriciq.isofwhm_std = get_value(iq.getElementsByTagName("isofwhm_std"))
-            qametriciq.ee50d = get_value(iq.getElementsByTagName("ee50d"))
-            qametriciq.ee50d_std = get_value(iq.getElementsByTagName("ee50d_std"))
-            qametriciq.elip = get_value(iq.getElementsByTagName("elip"))
-            qametriciq.elip_std = get_value(iq.getElementsByTagName("elip_std"))
-            qametriciq.pa = get_value(iq.getElementsByTagName("pa"))
-            qametriciq.pa_std = get_value(iq.getElementsByTagName("pa_std"))
-            qametriciq.strehl = get_value(iq.getElementsByTagName("strehl"))
-            qametriciq.strehl_std = get_value(iq.getElementsByTagName("strehl_std"))
-            qametriciq.nsamples = get_value(iq.getElementsByTagName("nsamples"))
-            qametriciq.percentile_band = get_value(iq.getElementsByTagName("percentile_band"))
-            qametriciq.comment = get_value(iq.getElementsByTagName("comment"))
-            session.add(qametriciq)
-            session.commit()
-          for zp in qam.getElementsByTagName("zp"):
-            qametriczp = QAmetricZP(qareport)
-            qametriczp.datalabel = datalabel
-            qametriczp.filename = filename
-            qametriczp.detector = detector
-            qametriczp.mag = get_value(zp.getElementsByTagName("mag"))
-            qametriczp.mag_std = get_value(zp.getElementsByTagName("mag_std"))
-            qametriczp.cloud = get_value(zp.getElementsByTagName("cloud"))
-            qametriczp.cloud_std = get_value(zp.getElementsByTagName("cloud_std"))
-            qametriczp.photref = get_value(zp.getElementsByTagName("photref"))
-            qametriczp.nsamples = get_value(zp.getElementsByTagName("nsamples"))
-            qametriczp.percentile_band = get_value(zp.getElementsByTagName("percentile_band"))
-            qametriczp.comment = get_value(zp.getElementsByTagName("comment"))
-            session.add(qametriczp)
-            session.commit()
-          for sb in qam.getElementsByTagName("sb"):
-            qametricsb = QAmetricSB(qareport)
-            qametricsb.filename = filename
-            qametricsb.datalabel = datalabel
-            qametricsb.detector = detector
-            qametricsb.mag = get_value(sb.getElementsByTagName("mag"))
-            qametricsb.mag_std = get_value(sb.getElementsByTagName("mag_std"))
-            qametricsb.electrons = get_value(sb.getElementsByTagName("elecrons"))
-            qametricsb.electrons_std = get_value(sb.getElementsByTagName("elecrons_std"))
-            qametricsb.nsamples = get_value(sb.getElementsByTagName("nsamples"))
-            qametricsb.comment = get_value(sb.getElementsByTagName("comment"))
-            qametricsb.percentile_band = get_value(sb.getElementsByTagName("percentile_band"))
-            session.add(qametricsb)
-            session.commit()
-          for pe in qam.getElementsByTagName("pe"):
-            qametricpe = QAmetricPE(qareport)
-            qametricpe.filename = filename
-            qametricpe.datalabel = datalabel
-            qametricpe.detector = detector
-            qametricpe.dra = get_value(pe.getElementsByTagName("dra"))
-            qametricpe.dra_std = get_value(pe.getElementsByTagName("dra_std"))
-            qametricpe.ddec = get_value(pe.getElementsByTagName("ddec"))
-            qametricpe.ddec_std = get_value(pe.getElementsByTagName("ddec_std"))
-            qametricpe.astref = get_value(pe.getElementsByTagName("astref"))
-            qametricpe.nsamples = get_value(pe.getElementsByTagName("nsamples"))
-            qametricpe.comment = get_value(pe.getElementsByTagName("comment"))
-            session.add(qametricpe)
-            session.commit()
+  if(req.method == 'POST'):
+    clientdata = req.read()
 
-  except IOError:
-    pass
-  finally:
-    session.close()
+    if(clientdata[0:4]=='<xml'):
+      list = parse_xml(clientdata)
 
-  return apache.OK
+    elif(clientdata[0]=='['):
+      list = parse_json(clientdata)
+
+    else:
+      return apache.BAD_REQUEST
+
+    return qareport_ingest(list)
+
+
+def parse_xml(clientdata):
+  """
+  This function takes a string containing an xml document with qareports and makes it
+  into a list of dictionaries
+  """
+  list = []
+  clientstr = urllib.unquote(clientdata)
+  dom = parseString(clientstr)
+  for qr in dom.getElementsByTagName("qareport"):
+    qareport = {}
+    qareport['hostname']=get_value(qr.getElementsByTagName("hostname"))
+    qareport['userid']=get_value(qr.getElementsByTagName("userid"))
+    qareport['processid']=get_value(qr.getElementsByTagName("processid"))
+    qareport['executable']=get_value(qr.getElementsByTagName("executable"))
+    qareport['software']=get_value(qr.getElementsByTagName("software"))
+    qareport['software_version']=get_value(qr.getElementsByTagName("software_version"))
+    qareport['context']=get_value(qr.getElementsByTagName("context"))
+    qareport['submit_time'] = datetime.datetime.now()
+    qareport['submit_host'] = req.get_remote_host(apache.REMOTE_NAME)
+    qareport['qametric']=[]
+    for qam in qr.getElementsByTagName("qametric"):
+      qametric = {}
+      qametric['datalabel'] = get_value(qam.getElementsByTagName("datalabel"))
+      qametric['filename'] = get_value(qam.getElementsByTagName("filename"))
+      qametric['detector'] = get_value(qam.getElementsByTagName("detector"))
+      qametric['iq']=[]
+      for iq in qam.getElementsByTagName("iq"):
+        qametriciq={}
+        qametriciq['fwhm'] = get_value(iq.getElementsByTagName("fwhm"))
+        qametriciq['fwhm_std'] = get_value(iq.getElementsByTagName("fwhm_std"))
+        qametriciq['isofwhm'] = get_value(iq.getElementsByTagName("isofwhm"))
+        qametriciq['isofwhm_std'] = get_value(iq.getElementsByTagName("isofwhm_std"))
+        qametriciq['ee50d'] = get_value(iq.getElementsByTagName("ee50d"))
+        qametriciq['ee50d_std'] = get_value(iq.getElementsByTagName("ee50d_std"))
+        qametriciq['elip'] = get_value(iq.getElementsByTagName("elip"))
+        qametriciq['elip_std'] = get_value(iq.getElementsByTagName("elip_std"))
+        qametriciq['pa'] = get_value(iq.getElementsByTagName("pa"))
+        qametriciq['pa_std'] = get_value(iq.getElementsByTagName("pa_std"))
+        qametriciq['strehl'] = get_value(iq.getElementsByTagName("strehl"))
+        qametriciq['strehl_std'] = get_value(iq.getElementsByTagName("strehl_std"))
+        qametriciq['nsamples'] = get_value(iq.getElementsByTagName("nsamples"))
+        qametriciq['percentile_band'] = get_value(iq.getElementsByTagName("percentile_band"))
+        qametriciq['comment'] = get_value(iq.getElementsByTagName("comment"))
+        qametric['iq'].append(qametriciq)
+
+      qametric['zp']=[]
+      for zp in qam.getElementsByTagName("zp"):
+        qametriczp={}
+        qametriczp['datalabel'] = datalabel
+        qametriczp['filename'] = filename
+        qametriczp['detector'] = detector
+        qametriczp['mag'] = get_value(zp.getElementsByTagName("mag"))
+        qametriczp['mag_std'] = get_value(zp.getElementsByTagName("mag_std"))
+        qametriczp['cloud'] = get_value(zp.getElementsByTagName("cloud"))
+        qametriczp['cloud_std'] = get_value(zp.getElementsByTagName("cloud_std"))
+        qametriczp['photref'] = get_value(zp.getElementsByTagName("photref"))
+        qametriczp['nsamples'] = get_value(zp.getElementsByTagName("nsamples"))
+        qametriczp['percentile_band'] = get_value(zp.getElementsByTagName("percentile_band"))
+        qametriczp['comment'] = get_value(zp.getElementsByTagName("comment"))
+        qametric['zp'].append(qametriczp)
+
+      qametric['sb']=[]
+      for sb in qam.getElementsByTagName("sb"):
+        qametricsb = {}
+        qametricsb['filename'] = filename
+        qametricsb['datalabel'] = datalabel
+        qametricsb['detector'] = detector
+        qametricsb['mag'] = get_value(sb.getElementsByTagName("mag"))
+        qametricsb['mag_std'] = get_value(sb.getElementsByTagName("mag_std"))
+        qametricsb['electrons'] = get_value(sb.getElementsByTagName("elecrons"))
+        qametricsb['electrons_std'] = get_value(sb.getElementsByTagName("elecrons_std"))
+        qametricsb['nsamples'] = get_value(sb.getElementsByTagName("nsamples"))
+        qametricsb['comment'] = get_value(sb.getElementsByTagName("comment"))
+        qametricsb['percentile_band'] = get_value(sb.getElementsByTagName("percentile_band"))
+        qametric['sb'].append(qametricsb)
+
+      qametric['pe']=[]
+      for pe in qam.getElementsByTagName("pe"):
+        qametricpe = {}
+        qametricpe['filename'] = filename
+        qametricpe['datalabel'] = datalabel
+        qametricpe['detector'] = detector
+        qametricpe['dra'] = get_value(pe.getElementsByTagName("dra"))
+        qametricpe['dra_std'] = get_value(pe.getElementsByTagName("dra_std"))
+        qametricpe['ddec'] = get_value(pe.getElementsByTagName("ddec"))
+        qametricpe['ddec_std'] = get_value(pe.getElementsByTagName("ddec_std"))
+        qametricpe['astref'] = get_value(pe.getElementsByTagName("astref"))
+        qametricpe['nsamples'] = get_value(pe.getElementsByTagName("nsamples"))
+        qametricpe['comment'] = get_value(pe.getElementsByTagName("comment"))
+        qametric['pe'].append(qametricpe)
+
+      qareport['qametric'].append(qametric)
+
+    list.append(qareport)
+
+  return list
 
 def get_value(element):
   try:
