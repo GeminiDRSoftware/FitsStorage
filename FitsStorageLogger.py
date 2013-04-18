a5322693cf9ed22edea6b569a699beffa21915c4
fitsdata <fitsdata>
2013-04-18 23:23:18 +0000

    bugfix

--- FitsStorageLogger.py
+++ FitsStorageLogger.py
@@ -24,7 +24,7 @@ except (NameError, AttributeError):
 logfile = os.path.join(FitsStorageConfig.fits_log_dir, logname)
 filehandler=logging.handlers.RotatingFileHandler(logfile, backupCount=10, maxBytes=10000000)
 streamhandler=logging.StreamHandler()
-emailsubject = "Messages from FitsStorage on %s" % os.uname[1]
+emailsubject = "Messages from FitsStorage on %s" % os.uname()[1]
 smtphandler=logging.handlers.SMTPHandler(mailhost='smtp.gemini.edu', fromaddr='fitsdata@gemini.edu', toaddrs=[FitsStorageConfig.email_errors_to], subject=emailsubject)
 
 # The smtp handler should only do WARNINGSs or worse
