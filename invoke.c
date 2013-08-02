5046a67f90482ca00fd9b9efb448a55c53d752a4
Paul Hirst <phirst@gemini.edu>
2013-08-02 01:19:47 +0000

    add comment re return value

--- invoke.c
+++ invoke.c
@@ -3,7 +3,9 @@
 
 int main(int argc, char **argv) {
   execv("/astro/iraf/rhux-x86_64-glibc2.5/gempylocal/bin/python", argv);
-  return 0;
+  /* The execv should never return - it replaces the process image with what it calls. */
+  /* if we get to the line below, something went wrong. */
+  return 1;
 }
 
 
