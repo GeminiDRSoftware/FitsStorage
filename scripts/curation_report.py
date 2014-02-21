5bbfc282c235f36f9c8df7072e4141c0f2b3fb64
fitsdata <fitsdata>
2014-02-21 21:22:27 +0000

    pep8 etc

--- scripts/curation_report.py
+++ scripts/curation_report.py
@@ -1,18 +1,18 @@
 """
 This module retrieves and prints out the desired values from the list created in 
-FitsStorageCuration.py.
+orm.curation.py
 """
-import sys
-sys.path=['/opt/sqlalchemy/lib/python2.5/site-packages', '/astro/iraf/x86_64/gempylocal/lib/stsci_python/lib/python2.5/site-packages']+sys.path
 
-from FitsStorageCuration import *
+from orm import sessionfactory
+from orm.header import Header
+from orm.curation import duplicate_datalabels, duplicate_canonicals, duplicate_present, present_not_canonical
 from optparse import OptionParser
 
 parser = OptionParser()
 parser.add_option("--debug", action="store_true", dest="debug", help="Increase log level to debug")
 parser.add_option("--demon", action="store_true", dest="demon", help="Run as a background demon, do not generate stdout")
 parser.add_option("--checkonly", action="store", type="string", dest="checkonly", help="Limits the search by identifying a specific substring")
-parser.add_option("--exclude", action="store", type="string", dest="exclude" , help="Limits the search by excluding  data with a specific substring")
+parser.add_option("--exclude", action="store", type="string", dest="exclude" , help="Limits the search by excluding    data with a specific substring")
 parser.add_option("--noeng", action="store_const", const="ENG", dest="exclude", help="Limits the search by excluding data with the ENG substring")
 
 (options, args) = parser.parse_args()
@@ -29,17 +29,17 @@ dupdata = duplicate_datalabels(session, checkonly, exclude)
 previous_ans = ''
 # This for loop changes the list of df_ids to the corresponding DiskFile ids
 if dupdata != []:
-  for val in dupdata:
-    this_ans = val
-    header = session.query(Header).filter(Header.diskfile_id == val).first()
-    if header:
-      if previous_ans == '':
-        pass
-      if previous_ans != this_ans:
-        print "duplicate datalabel rows: DiskFile id = %s,  Filename = %s,  Datalabel = %s" %  (header.diskfile.id, header.diskfile.file.name, header.data_label) 
-      previous_ans = this_ans
+    for val in dupdata:
+        this_ans = val
+        header = session.query(Header).filter(Header.diskfile_id == val).first()
+        if header:
+            if previous_ans == '':
+                pass
+            if previous_ans != this_ans:
+                print "duplicate datalabel rows: DiskFile id = %s,    Filename = %s,    Datalabel = %s" %    (header.diskfile.id, header.diskfile.file.name, header.data_label) 
+            previous_ans = this_ans
 else:
-  print "No duplicate datalabel rows detected."
+    print "No duplicate datalabel rows detected."
 
 
 # Work for duplicate_canonicals
@@ -48,15 +48,15 @@ dupcanon = duplicate_canonicals(session)
 previous_file = ''
 empty = 0
 for val in dupcanon: 
-  this_file = val.file_id
-  if previous_file == '':
-    pass
-  elif previous_file == this_file:
-    print "duplicate canonical rows: DiskFile id = %s,  File id = %s,  canonical = %s" %  (val.id, val.file.id, val.canonical)
-    empty =+ 1 
-  previous_file = this_file
+    this_file = val.file_id
+    if previous_file == '':
+        pass
+    elif previous_file == this_file:
+        print "duplicate canonical rows: DiskFile id = %s,    File id = %s,    canonical = %s" %    (val.id, val.file.id, val.canonical)
+        empty += 1 
+    previous_file = this_file
 if empty == 0:
-  print "No duplicate canonical rows detected."
+    print "No duplicate canonical rows detected."
 
 
 # Work for duplicate_present
@@ -65,24 +65,24 @@ duppres = duplicate_present(session)
 previous_file = ''
 empty = 0
 for val in duppres: 
-  this_file = val.file_id
-  if previous_file == '':
-    pass
-  elif previous_file == this_file:
-    print "duplicate present rows: DiskFile id = %s,  File id = %s,  present = %s" %  (val.id,  val.file.id, val.present)
-  previous_file = this_file
+    this_file = val.file_id
+    if previous_file == '':
+        pass
+    elif previous_file == this_file:
+        print "duplicate present rows: DiskFile id = %s,    File id = %s,    present = %s" %    (val.id,    val.file.id, val.present)
+    previous_file = this_file
 if empty == 0:
-  print "No duplicate present rows detected." 
+    print "No duplicate present rows detected." 
 
 
 # Work for present_not_canonical
 presnotcanon = present_not_canonical(session)
 # Iterates through the list of rows and returns the desired values
 if presnotcanon != []:
-  for val in presnotcanon:   
-    print "duplicate present not canonical rows: DiskFile id = %s,  present = %s,  canonical = %s" %  (val.id, val.present, val.canonical)
+    for val in presnotcanon:     
+        print "duplicate present not canonical rows: DiskFile id = %s,    present = %s,    canonical = %s" %    (val.id, val.present, val.canonical)
 else:
-  print "No rows with the conditions present=True and canonical=False."
+    print "No rows with the conditions present=True and canonical=False."
 
 
 session.close()
