ee72e5f4b565824148e3ef2596eca84b734c2549
Paul Hirst <phirst@gemini.edu>
2014-04-22 02:06:28 +0000

    cleanup

--- fits_verify.py
+++ fits_verify.py
@@ -23,39 +23,41 @@ def fitsverify(filename):
     Returns a 4 component array containing
 
     * a boolean that is true if the argument is a fits file
-    * an integer giving the number of warnings
-    * an integer giving the number of errors
-    * a string containing the full fitsverify report
+    * an integer giving the number of warnings (-1 on error)
+    * an integer giving the number of errors (-1 on error)
+    * a string containing the full fitsverify report or an error message
     """
     
     # First check that the filename exists is readable and is a file
     exists = os.access(filename, os.F_OK | os.R_OK)
     isfile = os.path.isfile(filename)
     if(not(exists and isfile)):
-        print "%s is not readable or is not a file" % (filename)
-        return
+        report = "%s is not readable or is not a file" % (filename)
+        isfits = False
+        warnings = 0
+        errors = 1
+    else: 
+        # Fire off the subprocess and capture the output
+        sp = subprocess.Popen([fitsverify_bin, filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        (stdoutstring, stderrstring) = sp.communicate()
 
-    # Fire off the subprocess and capture the output
-    sp = subprocess.Popen([fitsverify_bin, filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    (stdoutstring, stderrstring) = sp.communicate()
-
-    stdoutstring += stderrstring
-    # Check to see if we got a not a fits file situation
-    nfmatch = nfre.search(stdoutstring)
-    if(nfmatch):
-        isfits = 0
-    else:
-        isfits = 1
-
-    # If it is a fits file, parse how many warnings and errors we got
-    if(isfits):
-        match = were.search(stdoutstring)
-        if(match):
-            warnings = match.group(1)
-            errors = match.group(2)
+        report = stdoutstring + stderrstring
+        # Check to see if we got a not a fits file situation
+        nfmatch = nfre.search(stdoutstring)
+        if(nfmatch):
+            isfits = False
         else:
-            print "Could not match warnings and errors string"
-            warnings = 255
-            errors = 255
+            isfits = True
 
-    return [isfits, warnings, errors, stdoutstring]
+        # If it is a fits file, parse how many warnings and errors we got
+        if(isfits):
+            match = were.search(stdoutstring)
+            if(match):
+                warnings = match.group(1)
+                errors = match.group(2)
+            else:
+                report = "Could not match warnings and errors string\n" + stdoutstring + stderrstring
+                warnings = -1
+                errors = -1
+
+    return [isfits, warnings, errors, report]
