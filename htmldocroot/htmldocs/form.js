54bec7a9706d47013e7303afbc0cd519c207a1c6
apaul <apaul>
2014-05-31 02:26:01 +0000

    some variables which might not be necessary

--- htmldocroot/htmldocs/form.js
+++ htmldocroot/htmldocs/form.js
@@ -18,3 +18,19 @@ $(function() {
     });
 });
 
+var progid = $("input#progid").val();
+var instrument = $("input#instrument").val();
+var obsclass = $("input#obs_class").val();
+var obstype = $("input#obs_type").val();
+var mode = $("input#Mode").val();
+var filter = $("input#Filter").val();
+var cntrwvl = $("input#cntrl_wvlngth").val();
+var targetname = $("input#target_name").val();
+var resolver = $("input#resolver").val();
+var RA = $("input#RA").val();
+var dec = $("input#Dec").val();
+var srad = $("input#search_rad").val();
+var binning = $("input#binning").val();
+var disperser = $("input#disperser").val();
+var mask = $("input#mask").val();
+
