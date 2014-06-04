e01ba107e56bb75caf2f782fb106cdf422ef6558
fitsdata <fitsdata>
2014-06-04 22:33:13 +0000

    nicer hr line

--- web/searchform.py
+++ web/searchform.py
@@ -102,7 +102,7 @@ def searchform(req, things):
        req.write('<input type="hidden" id="url" value="%s">' % selectionstring)
    
    req.write('</form>')
-   req.write('<hr>')
+   req.write('<hr noshade>')
    req.write('<div id="searchresults" class="searchresults">')
    # Uncomment this for form processing selection debugging...
    #req.write('<p>selection: %s</p>' % selection)
