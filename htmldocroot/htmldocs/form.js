01b419f2c8dafc2f212153813215fb139ef9fea7
fitsdata <fitsdata>
2014-07-02 23:37:24 +0000

    name resolver use local proxy

--- htmldocroot/htmldocs/form.js
+++ htmldocroot/htmldocs/form.js
@@ -18,6 +18,12 @@ $(function() {
     });
 });
 
+$(function() {
+    $('#advanced_header').click(function() {
+        $('#advanced_options').toggle();
+    });
+});
+
 $(document).ready(function() {
     var urlresults = "/searchresults";
     if ($("#url").length) {
@@ -29,36 +35,34 @@ $(document).ready(function() {
     };
 });
 
-$('#resbutton').click(function() {
-    var object_name = document.getElementById("object_name");
+$(function() {
+    $('#resbutton').click(function() {
+        var object_name = document.getElementById("object_name").value;
 
-    if document.getElementById("resolver").value = "SIMBAD" {
-        var resolverurl = 'http://cdsweb.u-strasbg.fr/cgi-bin/nph-sesame/-ox/S?' + object_name;
-    } else if document.getElementById("resolver").value = "NED" { 
-        var resolverurl = 'http://cdsweb.u-strasbg.fr/cgi-bin/nph-sesame/-ox/N?' + object_name;
-    } else if document.getElementById("resolver").value = "VIZIER" {
-        var resolverurl = 'http://cdsweb.u-strasbg.fr/cgi-bin/nph-sesame/-ox/V?' + object_name;
-    } else {
-        resolverurl = 'none'
-        alert("Please select a resolver from the dropdown menu")
-    }
-        
-    if (window.XMLHttpRequest) {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-                ra = xmlhttp.responseXML.documentElement.getElementsByTagName("jradeg");
-                dec = xmlhttp.responseXML.documentElement.getElementsByTagName("jdecdeg");
-                document.getElementById("ra").innerHTML = ra;
-                document.getElementById("dec").innerHTML = dec;
-            } else {
-                alert("Something went wrong I guess");
+        var url = "/nameresolver"
+
+        if (document.getElementById("resolver").value == "SIMBAD") {
+            url += "/simbad/" + object_name;
+        } else if (document.getElementById("resolver").value == "NED") { 
+            url += "/ned/" + object_name;
+        } else if (document.getElementById("resolver").value == "VIZIER") {
+            url += "/vizier/" + object_name;
+        } else {
+            url = ''
+            alert("Please select a resolver from the dropdown menu");
+        };
+
+        $.ajax({
+            url: url,
+            dataType: "xml",
+            success: function(xml) {
+                ra = xml.getElementsByTagName("jradeg")[0].childNodes[0].nodeValue;
+                dec = xml.getElementsByTagName("jdedeg")[0].childNodes[0].nodeValue;
+                document.getElementById("ra").value = ra;
+                document.getElementById("dec").value = dec;
+                document.getElementById("object_name").value = '';
+ 
             }
-        alert("Old browser alert");
-        }
-    
-    xmlhttp.open("GET", resolverurl, true);
-    xmlhttp.send();
-    }
+        });
+    });
 });
-
