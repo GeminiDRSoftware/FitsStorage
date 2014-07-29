88c3b35045a8d8f6a1161a2cb17caf949366198e
fitsdata <fitsdata>
2014-07-29 20:47:32 +0000

    updates for running under ureka

--- scripts/invoke.c
+++ scripts/invoke.c
@@ -5,22 +5,36 @@
 
 #define PYTHON "/opt/Ureka/Ureka/variants/common/bin/python"
 #define PYTHONPATH "PYTHONPATH=/opt/FitsStorage:/opt/python_modules/lib/python2.7/site-packages:/opt/gemini_python"
+#define UR_DIR "UR_DIR=/opt/Ureka/Ureka"
+#define UR_VARIANT "UR_VARIANT=common"
+#define UR_OS "UR_OS=linux-rhe6"
+#define UR_CPU "UR_CPU=x86_64"
+#define UR_BITS "UR_BITS=64"
 
 #define SCRIPT "/opt/FitsStorage/scripts/ingest_uploaded_file.py"
 
 int main(int argc, char **argv) {
   extern char **environ;
-  char *pythonpath=PYTHONPATH;
   char *python=PYTHON;
+  char *pythonpath=PYTHONPATH;
+  char *urdir=UR_DIR;
+  char *urvariant = UR_VARIANT;
+  char *uros = UR_OS;
+  char *urcpu = UR_CPU;
+  char *urbits = UR_BITS;
   char **p;
 
   uid_t ruid;
   uid_t euid;
 
-  /* dump the environment completely, replace with PYTHONPATH only*/
+  /* dump the environment completely, replace with PYTHONPATH and stuff Ureka needs only*/
   p = environ;
-  *p = pythonpath;
-  p++;
+  *p++ = pythonpath;
+  *p++ = urdir;
+  *p++ = urvariant;
+  *p++ = uros;
+  *p++ = urcpu;
+  *p++ = urbits;
   *p = (char *)NULL;
  
   /* fix argv[0] */
