fa23296ca6998cbfb0f694907a20d18b491ae1fd
fitsdata <fitsdata>
2014-08-02 02:25:57 +0000

    pylint

--- web/fileserver.py
+++ web/fileserver.py
@@ -1,13 +1,12 @@
 from orm import sessionfactory
 
-from fits_storage_config import odbkeypass, using_s3, fits_open_result_limit, fits_closed_result_limit
+from fits_storage_config import using_s3, fits_open_result_limit, fits_closed_result_limit
 
 from gemini_metadata_utils import gemini_fitsfilename
 
 from orm.file import File
 from orm.diskfile import DiskFile
 from orm.header import Header
-from orm.authentication import Authentication
 
 from web.selection import getselection, openquery, selection_to_URL
 from web.summary import list_headers
@@ -15,18 +14,15 @@ from web.user import userfromcookie
 
 # This will only work with apache
 from mod_python import apache
-from mod_python import Cookie
 from mod_python import util
 
 import time
-import urllib
-import re
 import datetime
 import gzip
 import cStringIO
 import tarfile
 
-if(using_s3):
+if using_s3:
     from boto.s3.connection import S3Connection
     from fits_storage_config import aws_access_key, aws_secret_key, s3_bucket_name
 
@@ -34,12 +30,12 @@ from utils.userprogram import icanhave
 
 def download(req, things):
     """
-    This is the download server. Given a selection, it will send a tarball of the 
-    files from the selection that you have access to to the client. 
+    This is the download server. Given a selection, it will send a tarball of the
+    files from the selection that you have access to to the client.
     """
 
     # If we are called via POST, then parse form data rather than selection
-    if(req.method == 'POST'):
+    if req.method == 'POST':
         # Parse form data
         formdata = util.FieldStorage(req)
         thelist = []
@@ -57,7 +53,7 @@ def download(req, things):
     try:
         # Get our username while we have the database session open
         user = userfromcookie(session, req)
-        if(user):
+        if user:
             username = user.username
         else:
             username = 'Not Logged In'
@@ -65,14 +61,15 @@ def download(req, things):
         # Get the header list
         headers = list_headers(session, selection, None)
 
-        if(openquery(selection) and len(headers) > fits_open_result_limit):
+        if openquery(selection) and len(headers) > fits_open_result_limit:
             # Open query. Almost certainly too many files
             req.content_type = "text/plain"
-            req.write("Your selection criteria does not restrict the number of results, and more than %d were found. " % fits_open_result_limit)
+            req.write("Your selection criteria does not restrict the number of results, and more than %d were found. " %
+                        fits_open_result_limit)
             req.write("Please refine your selection more before attempting to download. Queries that can contain an arbitrary number of results have a lower limit applied than more constrained queries. Including a date range or program id will prevent an arbitrary number of results being found will raise the limit")
             return apache.OK
 
-        if(len(headers) > fits_closed_result_limit):
+        if len(headers) > fits_closed_result_limit:
             # Open query. Almost certainly too many files
             req.content_type = "text/plain"
             req.write("More than %d results were found. This is beyond the limit we allow" % fits_closed_result_limit)
@@ -83,7 +80,7 @@ def download(req, things):
         req.content_type = "application/tar"
         req.headers_out['Content-Disposition'] = 'attachment; filename="download.tar"'
 
-        if(using_s3):
+        if using_s3:
             s3conn = S3Connection(aws_access_key, aws_secret_key)
             bucket = s3conn.get_bucket(s3_bucket_name)
 
@@ -94,9 +91,9 @@ def download(req, things):
         # Here goes!
         tar = tarfile.open(name="download.tar", mode="w|", fileobj=req)
         for header in headers:
-            if(icanhave(session, req, header)):
+            if icanhave(session, req, header):
                 md5file += "%s  %s\n" % (header.diskfile.file_md5, header.diskfile.filename)
-                if(using_s3):
+                if using_s3:
                     # Fetch the file into a cStringIO buffer
                     key = bucket.get_key(header.diskfile.filename)
                     buffer = cStringIO.StringIO()
@@ -143,7 +140,7 @@ def download(req, things):
         readme += "We have included a file listing the md5sums of the data files in here.\n"
         readme += "If you have the 'md5sum' utility installed (most Linux machines at least),\n"
         readme += "You can verify file integrity by running 'md5sum -c md5sums.txt'.\n\n"
-        if(denied):
+        if denied:
             readme += "The following files in your search results were not included,\n"
             readme += "because they are proprietary data that you do not have access to:\n"
             readme += '\n'.join(denied) + '\n'
@@ -179,18 +176,18 @@ def fileserver(req, things):
 
     # OK, first find the file they asked for in the database
     # tart up the filename if possible
-    if(len(things) == 0):
+    if len(things) == 0:
         return apache.HTTP_NOT_FOUND
     filenamegiven = things.pop(0)
     filename = gemini_fitsfilename(filenamegiven)
-    if(filename):
+    if filename:
         pass
     else:
         filename = filenamegiven
     session = sessionfactory()
     try:
         query = session.query(File).filter(File.name == filename)
-        if(query.count() == 0):
+        if query.count() == 0:
             return apache.HTTP_NOT_FOUND
         file = query.one()
         # OK, we should have the file record now.
@@ -204,7 +201,7 @@ def fileserver(req, things):
         # Is the client allowed to get this file?
         canhaveit = icanhave(session, req, header)
 
-        if(canhaveit):
+        if canhaveit:
             # Send them the data
             sendonefile(req, header)
             return apache.OK
@@ -227,13 +224,13 @@ def sendonefile(req, header):
     # Send them the data
     req.content_type = 'application/fits'
     req.headers_out['Content-Disposition'] = 'attachment; filename="%s"' % str(header.diskfile.file.name)
-    if(using_s3):
+    if using_s3:
         # S3 file server
         s3conn = S3Connection(aws_access_key, aws_secret_key)
         bucket = s3conn.get_bucket(s3_bucket_name)
         key = bucket.get_key(header.diskfile.filename)
         req.set_content_length(header.diskfile.data_size)
-        if(header.diskfile.gzipped):
+        if header.diskfile.gzipped:
             buffer = cStringIO.StringIO()
             key.get_contents_to_file(buffer)
             buffer.seek(0)
@@ -247,7 +244,7 @@ def sendonefile(req, header):
             key.get_contents_to_file(req)
     else:
         # Serve from regular file
-        if(header.diskfile.gzipped == True):
+        if header.diskfile.gzipped == True:
             # Unzip it on the fly
             req.set_content_length(header.diskfile.data_size)
             gzfp = gzip.open(header.diskfile.fullpath(), 'rb')
