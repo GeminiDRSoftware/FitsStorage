0d585154d2794b72d3e093a830fe46863b5bb338
fitsdata <fitsdata>
2014-08-04 22:47:30 +0000

    pylint

--- fits_verify.py
+++ fits_verify.py
@@ -2,7 +2,7 @@
 This is the FitsVerify module. It provides a python interface to the
 fitsverify command.
 
-The fitsverify_bin string contains the path to the fitsverify
+The FITSVERIFY_BIN string contains the path to the fitsverify
 executable
 """
 
@@ -11,11 +11,7 @@ import os
 import re
 
 # the path to the fitsverify binary
-fitsverify_bin = '/opt/fitsverify/fitsverify'
-
-# Compile the regular expression here for efficiency
-nfre = re.compile('This does not look like a FITS file.')
-were = re.compile('\*\*\*\* Verification found (\d*) warning\(s\) and (\d*) error\(s\). \*\*\*\*')
+FITSVERIFY_BIN = '/opt/fitsverify/fitsverify'
 
 def fitsverify(filename):
     """
@@ -27,32 +23,31 @@ def fitsverify(filename):
     * an integer giving the number of errors (-1 on error)
     * a string containing the full fitsverify report or an error message
     """
-    
+
     # First check that the filename exists is readable and is a file
     exists = os.access(filename, os.F_OK | os.R_OK)
     isfile = os.path.isfile(filename)
-    if(not(exists and isfile)):
+    if not(exists and isfile):
         report = "%s is not readable or is not a file" % (filename)
         isfits = False
         warnings = 0
         errors = 1
-    else: 
+    else:
         # Fire off the subprocess and capture the output
-        sp = subprocess.Popen([fitsverify_bin, filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        (stdoutstring, stderrstring) = sp.communicate()
+        subp = subprocess.Popen([FITSVERIFY_BIN, filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        (stdoutstring, stderrstring) = subp.communicate()
 
         report = stdoutstring + stderrstring
         # Check to see if we got a not a fits file situation
-        nfmatch = nfre.search(stdoutstring)
-        if(nfmatch):
+        if re.search('This does not look like a FITS file.', stdoutstring)
             isfits = False
         else:
             isfits = True
 
         # If it is a fits file, parse how many warnings and errors we got
-        if(isfits):
-            match = were.search(stdoutstring)
-            if(match):
+        if isfits:
+            match = re.search(r'\*\*\*\* Verification found (\d*) warning\(s\) and (\d*) error\(s\). \*\*\*\*', stdoutstring)
+            if match:
                 warnings = match.group(1)
                 errors = match.group(2)
             else:
