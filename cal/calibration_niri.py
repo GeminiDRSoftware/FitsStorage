ffe6745d95cbdc51a36c5f3577310320b6712907
fitsdata <fitsdata>
2014-10-28 00:00:42 +0000

    add photometric_standard for niri

--- cal/calibration_niri.py
+++ cal/calibration_niri.py
@@ -44,7 +44,7 @@ class CalibrationNIRI(Calibration):
         # Return a list of the calibrations applicable to this NIRI dataset
         self.applicable = []
 
-        # Science Imaging OBJECTs require a DARK and FLAT
+        # Science Imaging OBJECTs require a DARK and FLAT, and photometric_standard
         if (self.descriptors['observation_type'] == 'OBJECT' and
                 self.descriptors['spectroscopy'] == False and
                 self.descriptors['observation_class'] == 'science'):
@@ -52,6 +52,7 @@ class CalibrationNIRI(Calibration):
             # No flats for L', M' Br(alpha) or Br(alpha) continuum as per AS 20130514
             if self.descriptors['filter_name'] not in ['Lprime_G0207', 'Mprime_G0208', 'Bra_G0238', 'Bracont_G0237']:
                 self.applicable.append('flat')
+            self.applicable.append('photometric_standard')
 
         # Imaging Lamp-on Flat fields require a lampoff_flat
         if (self.descriptors['observation_type'] == 'FLAT' and
@@ -180,6 +181,49 @@ class CalibrationNIRI(Calibration):
 
         # Absolute time separation must be within 1 hour of the lamp on flats
         max_interval = datetime.timedelta(seconds=3600)
+        datetime_lo = self.descriptors['ut_datetime'] - max_interval
+        datetime_hi = self.descriptors['ut_datetime'] + max_interval
+        query = query.filter(Header.ut_datetime > datetime_lo).filter(Header.ut_datetime < datetime_hi)
+
+        # Order by absolute time separation.
+        # query = query.order_by(func.abs(extract('epoch', Header.ut_datetime - self.descriptors['ut_datetime'])).asc())
+        # Use the ut_datetime_secs column for faster and more portable ordering
+        targ_ut_dt_secs = int((self.descriptors['ut_datetime'] - Header.UT_DATETIME_SECS_EPOCH).total_seconds())
+        query = query.order_by(func.abs(Header.ut_datetime_secs - targ_ut_dt_secs))
+
+        query = query.limit(howmany)
+        return query.all()
+
+    def photometric_standard(self, processed=False, howmany=None):
+        query = self.session.query(Header).select_from(join(join(Niri, Header), DiskFile))
+
+        if processed:
+            # Not a valid concept
+            return []
+        else:
+            query = query.filter(Header.reduction == 'RAW')
+            # Default number to associate
+            howmany = howmany if howmany else 10
+
+        # Search only canonical entries
+        query = query.filter(DiskFile.canonical == True)
+
+        # Knock out the FAILs
+        query = query.filter(Header.qa_state != 'Fail')
+
+        # Phot standards are OBJECT imaging frames
+        query = query.filter(Header.observation_type == 'OBJECT')
+        query = query.filter(Header.spectroscopy == False)
+
+        # Phot standards are phot standards
+        query = query.filter(Header.phot_standard == True)
+
+        # Must match filter and camera
+        query = query.filter(Niri.filter_name == self.descriptors['filter_name'])
+        query = query.filter(Niri.camera == self.descriptors['camera'])
+
+        # Absolute time separation must be within 24 hours of the science
+        max_interval = datetime.timedelta(days=1)
         datetime_lo = self.descriptors['ut_datetime'] - max_interval
         datetime_hi = self.descriptors['ut_datetime'] + max_interval
         query = query.filter(Header.ut_datetime > datetime_lo).filter(Header.ut_datetime < datetime_hi)
