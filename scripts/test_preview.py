37ea0fbbb9cf60cf91a1087cf0711d854de685e8
fitsdata <fitsdata>
2014-11-07 03:14:05 +0000

    test script for render_preview

---
+++ scripts/test_preview.py
@@ -0,0 +1,49 @@
+import sys
+
+from astrodata import AstroData
+from utils.previewqueue import render_preview
+from logger import logger, setdebug, setdemon
+
+
+# Option Parsing
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option("--file", action="store", type="string", dest="file", help="file to preview")
+parser.add_option("--debug", action="store_true", dest="debug", help="Increase log level to debug")
+parser.add_option("--demon", action="store_true", dest="demon", help="Run as a background demon, do not generate stdout")
+
+(options, args) = parser.parse_args()
+
+# Logging level to debug? Include stdio log?
+setdebug(options.debug)
+setdemon(options.demon)
+
+filename = options.file
+
+if not filename:
+    print "Must supply and input filename"
+    sys.exit(1)
+
+ad = AstroData(filename)
+
+if not ad:
+    print "AstroData open failed"
+    sys.exit(2)
+
+
+jpgfile = filename.replace('.fits', '.jpg')
+
+fp = open(jpgfile, 'w')
+
+print "input file: %s" % filename
+print "output file: %s" % jpgfile
+
+print "Rendering Preview..."
+
+render_preview(ad, fp)
+
+print "Done"
+
+fp.close()
+ad.close()
+
