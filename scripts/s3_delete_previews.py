7aeee29d8fae1baa4f0c6d075d94eb50e4993ea4
fitsdata <fitsdata>
2014-11-18 19:15:33 +0000

    initial version

---
+++ scripts/s3_delete_previews.py
@@ -0,0 +1,63 @@
+import datetime
+import sys
+
+from sqlalchemy import join, desc
+
+from orm import sessionfactory
+from orm.preview import Preview
+from fits_storage_config import using_s3, aws_access_key, aws_secret_key, s3_bucket_name
+from logger import logger, setdebug, setdemon
+from boto.s3.connection import S3Connection
+
+
+# Option Parsing
+from optparse import OptionParser
+parser = OptionParser()
+#parser.add_option("--file-pre", action="store", type="string", dest="filepre", help="File prefix to operate on, eg N20090130, N200812 etc")
+parser.add_option("--yesimsure", action="store_true", dest="yesimsure", default=False, help="Needed for sanity check")
+parser.add_option("--dryrun", action="store_true", dest="dryrun", help="Dry Run - do not actually do anything")
+parser.add_option("--debug", action="store_true", dest="debug", help="Increase log level to debug")
+parser.add_option("--demon", action="store_true", dest="demon", help="Run as a background demon, do not generate stdout")
+
+(options, args) = parser.parse_args()
+
+# Logging level to debug? Include stdio log?
+setdebug(options.debug)
+setdemon(options.demon)
+
+
+# Annouce startup
+logger.info("*********    s3_delete_previews.py - starting up at %s" % datetime.datetime.now())
+
+if(using_s3 == False):
+    logger.error("This script is only useable on installations using S3 for storage")
+    sys.exit(1)
+
+if(options.yesimsure != True):
+    logger.info("This is a really dangerous script to run. If you're not sure, don't do this.")
+    logger.info("This will unconditionally delete files from the S3 storage")
+    logger.error("You need to say --yesimsure to make it work")
+    sys.exit(2)
+
+
+# Get a full listing from S3. The preview files might not be in the DB.
+logger.info("Getting file list from S3")
+filelist = []
+s3conn = S3Connection(aws_access_key, aws_secret_key)
+bucket = s3conn.get_bucket(s3_bucket_name)
+filelist = []
+for key in bucket.list():
+    filelist.append(key.name)
+
+#session = sessionfactory()
+
+for filename in filelist:
+    if filename.endswith("_preview.jpg"):
+        if options.dryrun:
+            logger.info("Dryrun - not actually Deleting %s", filename)
+        else:
+            logger.info("Deleting %s", filename)
+            key = bucket.get_key(filename)
+            key.delete()
+    
+logger.info("** s3_delete_previews.py exiting normally")
