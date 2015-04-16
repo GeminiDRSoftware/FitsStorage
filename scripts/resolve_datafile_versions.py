52a0a20cb21ede18e49875dafad64ce48e7302b2
Ricardo Cardenes <rcardenes@gemini.edu>
2015-04-16 00:42:28 +0000

    Refactor resolve_datafile_versions to put some code in functions

--- scripts/resolve_datafile_versions.py
+++ scripts/resolve_datafile_versions.py
@@ -13,13 +13,15 @@ from logger import logger, setdebug, setdemon
 import datetime
 
 parser = OptionParser()
-parser.add_option("--dryrun", action="store_true", dest="dryrun", default=False, help="Don't actually do anything, just say what would be done")
-parser.add_option("--srcdir", action="store", dest="srcdir", default="/sdata/all_gemini_data/from_tape", help="Source directory to pull files from")
+parser.add_option("--dryrun",  action="store_true", dest="dryrun", default=False, help="Don't actually do anything, just say what would be done")
+parser.add_option("--srcdir",  action="store", dest="srcdir", default="/sdata/all_gemini_data/from_tape", help="Source directory to pull files from")
 parser.add_option("--destdir", action="store", dest="destdir", default="/sdata/all_gemini_data/canonical", help="Destination directory to put files in")
-parser.add_option("--scan", action="store_true", default=False, dest="scan", help="Scan directories to DB")
-parser.add_option("--able", action="store_true", default=False, dest="able", help="Reset all unable flags to False")
-parser.add_option("--debug", action="store_true", dest="debug", help="Increase log level to debug")
-parser.add_option("--demon", action="store_true", dest="demon", help="Run as a background demon, do not generate stdout")
+parser.add_option("--scan",    action="store_true", default=False, dest="scan", help="Scan directories to DB")
+parser.add_option("--noidemp", action="store_true", default=False, dest="noidemp", help="When scanning, assume that we start with a blank database and don't worry about inserting duplicates")
+parser.add_option("--able",    action="store_true", default=False, dest="able", help="Reset all unable flags to False")
+parser.add_option("--debug",   action="store_true", dest="debug", help="Increase log level to debug")
+parser.add_option("--uniques", action="store_true", default=False, dest="uniq", help="Look for truly, unique files and mark them as accepted")
+parser.add_option("--demon",   action="store_true", dest="demon", help="Run as a background demon, do not generate stdout")
 
 
 (options, args) = parser.parse_args()
@@ -34,23 +36,107 @@ logger.info("*********    resolve_datafile_versions.py - starting up at %s" % da
 # Get a session
 session = orm.sessionfactory()
 
-if options.scan:
+def scan_directory(sess, which, idemp):
     # Recurse through the directory structure, finding files, add to db.
-    for root, dirs, files in os.walk(options.srcdir):
+    for root, dirs, files in os.walk(which):
         logger.debug("Walking root %s, found %d dirs and %d files", root, len(dirs), len(files))
-        logger.info("Walking root %s...", root)
         for file in files:
             fullpath = os.path.join(root, file)
             if ('fits' in file) and (not file.startswith('.')):
                 # This operation should be idempotent. If we can find the full path in the database,
-                # then skip it!
-                if session.query(Version.id).filter(Version.fullpath == fullpath).count() == 0:
+                # then skip it! Unless user says otherwise
+                if (not idemp) or (sess.query(Version.id).filter(Version.fullpath == fullpath).count() == 0):
                     logger.info("Found %s at %s", file, fullpath)
                     version = Version(file, fullpath)
-                    session.add(version)
+                    sess.add(version)
             else:
                 logger.info("Ignoring non fits file %s", file)
-        session.commit()
+        sess.commit()
+
+def resolve_uniques(sess):
+    # This is a subquery that selects the ids of the unique filenames
+    sq = sess.query(orm.func.min(Version.id).label('mid')).\
+              filter(Version.accepted == None).\
+              group_by(Version.filename).\
+              having(orm.func.count(Version.filename) == 1).\
+              subquery()
+
+    # And this will update the status of the filenames selected by 'sq'
+    stmt = orm.update(Version).\
+               where(Version.id.in_(sq)).\
+               values(accepted = True,
+                      is_clear = True,
+                      score    = 1)
+    rc = sess.execute(stmt).rowcount
+    if rc:
+        logger.info("- Found %s such files. Now marked as accepted", rc)
+    else:
+        logger.info("- No unique, non-accepted files found")
+
+
+# De-duplicate...
+def deduplicate(sess, name):
+    versions = list(sess.query(Version).\
+                         filter(Version.filename == fname).\
+                         filter(Version.is_clear == None).\
+                         filter(Version.accepted == None))
+
+    # Calculate scoring
+    versdict = {}
+    for vers in versions:
+        versdict[vers.fullpath] = vers
+    for path, score in image_validity.score_files(*versdict.keys()):
+        vers = versdict[path]
+        vers.score = score.value
+        # Something really bad happened here...
+        if score.value < -9000:
+            vers.unable = True
+    # Cleanup...
+    versdict = None
+    if all(x.unable for x in versions):
+        # No valid version left
+        sess.commit()
+        return
+
+    versions = sorted(versions, key=attrgetter('score'), reverse = True)
+    max_val = versions[0].score
+    for vers in versions[1:]:
+        if vers.score < max_val:
+            vers.accepted = False
+            vers.is_clear = False
+
+    candidates = filter(lambda x: x.score == max_val, versions)
+
+    if len(candidates) > 1:
+        logger.info("{0}: Found more than one max score. Trying MD5".format(fname))
+
+        for vers in candidates:
+            vers.calc_md5()
+        # MD5 matching. Rejects all exact copies except for one
+        for (md5, group) in itertools.groupby(candidates, attrgetter('data_md5')):
+            for inst in list(group)[1:]:
+                inst.accepted = False
+                inst.is_clear = True
+
+        winners = filter(lambda x: x.accepted != False, candidates)
+        if len(winners) > 1:
+            logger.info("  - Still undecided. Marking the potential winners as unable")
+            for vers in winners:
+                vers.unable = True
+        else:
+            winner = winners[0]
+            winner.accepted = True
+            winner.is_clear = (True if len(candidates) == len(versions) else False)
+    else:
+        logger.info("{0}: Found a winner with score {1}".format(fname, max_val))
+        winner = candidates[0]
+        winner.accepted = True
+        winner.is_clear = False
+
+    sess.commit()
+
+if options.scan:
+    scan_directory(session, options.srcdir, not options.noidemp)
     logger.info("Exiting after scan")
     sys.exit(0)
 
@@ -60,32 +146,13 @@ if options.able:
     session.commit()
 
 ################################################################################
-# First pass: look for filenames that appear just once in the
-#             database. Move them away and remove from the table
+# Look for filenames that appear just once in the database and mark them as
+# accepted (and clear).
 
-logger.info("Looking for purely unique filenames...")
-
-# This is a subquery that selects the ids of the unique filenames
-sq = session.query(orm.func.min(Version.id).label('mid')).\
-             filter(Version.accepted == None).\
-             group_by(Version.filename).\
-             having(orm.func.count(Version.filename) == 1).\
-             subquery()
-
-# And this will update the status of the filenames selected by 'sq'
-stmt = orm.update(Version).\
-           where(Version.id.in_(sq)).\
-           values(accepted = True,
-                  is_clear = True,
-                  score    = 1)
-rc = session.execute(stmt).rowcount
-if rc:
-    logger.info("- Found %s such files. Now marked as accepted", rc)
-else:
-    logger.info("- No unique, non-accepted files found")
-session.commit()
-sq = None    # Just destroying these two...
-stmt = None
+if options.uniq:
+    logger.info("Looking for purely unique filenames...")
+    resolve_uniq(session)
+    session.commit()
 
 ################################################################################
 # De-duplicating rules
@@ -127,7 +194,6 @@ stmt = None
 #
 # If we can't declare a winner, we mark all the instances as "unable"
 logger.info("De-duplicating: scoring + MD5")
-
 query = session.query(Version.filename).\
                 filter(Version.unable == False).\
                 filter(Version.is_clear == None).\
@@ -135,63 +201,6 @@ query = session.query(Version.filename).\
                 group_by(Version.filename)
 
 for (fname,) in query:
-    versions = list(session.query(Version).\
-                            filter(Version.filename == fname).\
-                            filter(Version.is_clear == None).\
-                            filter(Version.accepted == None))
-
-    # Calculate scoring
-    versdict = {}
-    for vers in versions:
-        versdict[vers.fullpath] = vers
-    for path, score in image_validity.score_files(*versdict.keys()):
-        vers = versdict[path]
-        vers.score = score.value
-        # Something really bad happened here...
-        if score.value < -9000:
-            vers.unable = True
-    # Cleanup...
-    versdict = None
-    if all(x.unable for x in versions):
-        # No valid version left
-        session.commit()
-        continue
-
-    versions = sorted(versions, key=attrgetter('score'), reverse = True)
-    max_val = versions[0].score
-    for vers in versions[1:]:
-        if vers.score < max_val:
-            vers.accepted = False
-            vers.is_clear = False
-
-    candidates = filter(lambda x: x.score == max_val, versions)
-
-    if len(candidates) > 1:
-        logger.info("{0}: Found more than one max score. Trying MD5".format(fname))
-
-        for vers in candidates:
-            vers.calc_md5()
-        # MD5 matching. Rejects all exact copies except for one
-        for (md5, group) in itertools.groupby(candidates, attrgetter('data_md5')):
-            for inst in list(group)[1:]:
-                inst.accepted = False
-                inst.is_clear = True
-
-        winners = filter(lambda x: x.accepted != False, candidates)
-        if len(winners) > 1:
-            logger.info("  - Still undecided. Marking the potential winners as unable")
-            for vers in winners:
-                vers.unable = True
-        else:
-            winner = winners[0]
-            winner.accepted = True
-            winner.is_clear = (True if len(candidates) == len(versions) else False)
-    else:
-        logger.info("{0}: Found a winner with score {1}".format(fname, max_val))
-        winner = candidates[0]
-        winner.accepted = True
-        winner.is_clear = False
-
-    session.commit()
+    deduplicate(session, fname)
 
 session.close()
