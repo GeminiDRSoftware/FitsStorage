51e37c0e3e095063a6dd00dc3a005b2f3aebf554
fitsdata <fitsdata>
2015-05-14 01:35:36 +0000

    split off notification update method

--- scripts/get_notifications_from_odb.py
+++ scripts/get_notifications_from_odb.py
@@ -5,13 +5,13 @@ from xml.dom.minidom import parseString
 from optparse import OptionParser
 
 from orm import sessionfactory
-from orm.notification import Notification
 from logger import logger, setdebug, setdemon
+
+from utils.notifications import ingest_odb_xml
 
 parser = OptionParser()
 parser.add_option("--odb", action="store", dest="odb", help="ODB server to query. Probably gnodb or gsodb")
 parser.add_option("--semester", action="store", dest="semester", help="Query ODB for only the given semester")
-parser.add_option("--dryrun", action="store_true", dest="dryrun", default=False, help="Do not actually update notification, just say what would be done")
 parser.add_option("--debug", action="store_true", dest="debug", default=False, help="Increase log level to debug")
 parser.add_option("--demon", action="store_true", dest="demon", default=False, help="Run as a background demon, do not generate stdout")
 (options, args) = parser.parse_args()
@@ -21,90 +21,22 @@ setdebug(options.debug)
 setdemon(options.demon)
 
 url = "http://%s:8442/odbbrowser/programs" % options.odb
-if(options.semester):
+if options.semester:
     url += "?programSemester=%s" % options.semester
 logger.info("Fetching XML from ODB server: %s", url)
 u = urllib2.urlopen(url)
 xml = u.read()
 u.close()
-logger.debug("Got %d bytes from server. Parsing." % len(xml))
+logger.debug("Got %d bytes from server. Parsing.", len(xml))
 
 # Get a database session
 session = sessionfactory()
 
-nprogs = 0
-dom = parseString(xml)
-for pe in dom.getElementsByTagName("program"):
-    readok = True
-    try:
-        progid = pe.getElementsByTagName("reference")[0].childNodes[0].data
-        logger.debug("got %s" % progid)
-    except:
-        logger.error("Failed to process program node")
-        readok = False
-    piEmail = ""
-    ngoEmail = ""
-    csEmail = ""
-    notifyPi = "No"
-    nprogs += 1
-    # Sometimes people use ;s for separators in the odb email fields...
-    try:
-        piEmail = pe.getElementsByTagName("piEmail")[0].childNodes[0].data
-        piEmail = piEmail.replace(';', ',')
-    except:
-        pass
-    try:
-        ngoEmail = pe.getElementsByTagName("ngoEmail")[0].childNodes[0].data
-        ngoEmail = ngoEmail.replace(';', ',')
-    except:
-        pass
-    try:
-        csEmail = pe.getElementsByTagName("contactScientistEmail")[0].childNodes[0].data
-        csEmail = csEmail.replace(';', ',')
-    except:
-        pass
-    try:
-        notifyPi = pe.getElementsByTagName("notifyPi")[0].childNodes[0].data
-    except:
-        pass
+report = ingest_odb_xml(session, xml)
 
-    if(readok):
-        # Search for this program ID in notification table
-        label = "Auto - %s" % progid
-        query = session.query(Notification).filter(Notification.label == label)
-        if(query.count() == 0):
-            n = Notification(label)
-            n.selection = "%s/science" % progid
-            n.to = piEmail
-            if(len(ngoEmail) == 0):
-                n.cc = csEmail
-            elif(len(csEmail) == 0):
-                n.cc = ngoEmail
-            else:
-                n.cc = "%s,%s" % (ngoEmail, csEmail)
-
-            if(not options.dryrun):
-                logger.info("Adding notification %s" % label)
-                session.add(n)
-                session.commit()
-            else:
-                logger.info("Dryrun mode - not really adding %s" % label)
-        else:
-            logger.info("%s is already present, check for updates" % label)
-            n = query.first()
-            if(n.to != piEmail):
-                if(not options.dryrun):
-                    logger.info("Updating to for %s" % label)
-                    n.to = piEmail
-                    session.commit()
-                else:
-                    logger.info("Dryrun - not actually updating Email to for %s" % label)
-            if(n.cc != "%s,%s" % (ngoEmail, csEmail)):
-                if(not options.dryrun):
-                    logger.info("Updating cc for %s" % label)
-                    n.cc = "%s,%s" % (ngoEmail, csEmail)
-                    session.commit()
-                else:
-                    logger.info("Dryrun - not actually updating Email CC for %s" % label)
-        
-logger.info("Processed %s programs" % nprogs)
+# Replay the report into the log files
+for l in report:
+    if l.startswith("ERROR"):
+        logger.error(l)
+    else:
+        logger.info(l)
---
+++ utils/notifications.py
@@ -0,0 +1,78 @@
+"""
+Notifications utils - add / update notification table entries from ODB XML
+"""
+
+from xml.dom.minidom import parseString
+from orm.notification import Notification
+
+
+def ingest_odb_xml(session, xml):
+    report = []
+    nprogs = 0
+    dom = parseString(xml)
+    for pe in dom.getElementsByTagName("program"):
+        readok = True
+        try:
+            progid = pe.getElementsByTagName("reference")[0].childNodes[0].data
+        except:
+            report.append("ERROR: Failed to process program node")
+            readok = False
+        piEmail = ""
+        ngoEmail = ""
+        csEmail = ""
+        notifyPi = "No"
+        nprogs += 1
+        # Sometimes people use ;s for separators in the odb email fields...
+        try:
+            piEmail = pe.getElementsByTagName("piEmail")[0].childNodes[0].data
+            piEmail = piEmail.replace(';', ',')
+        except:
+            pass
+        try:
+            ngoEmail = pe.getElementsByTagName("ngoEmail")[0].childNodes[0].data
+            ngoEmail = ngoEmail.replace(';', ',')
+        except:
+            pass
+        try:
+            csEmail = pe.getElementsByTagName("contactScientistEmail")[0].childNodes[0].data
+            csEmail = csEmail.replace(';', ',')
+        except:
+            pass
+        try:
+            notifyPi = pe.getElementsByTagName("notifyPi")[0].childNodes[0].data
+        except:
+            pass
+    
+        if readok:
+            # Search for this program ID in notification table
+            label = "Auto - %s" % progid
+            query = session.query(Notification).filter(Notification.label == label)
+            if query.count() == 0:
+                n = Notification(label)
+                n.selection = "%s/science" % progid
+                n.to = piEmail
+                if len(ngoEmail) == 0:
+                    n.cc = csEmail
+                elif len(csEmail) == 0:
+                    n.cc = ngoEmail
+                else:
+                    n.cc = "%s,%s" % (ngoEmail, csEmail)
+    
+                report.append("Adding notification %s" % label)
+                session.add(n)
+                session.commit()
+            else:
+                report.append("%s is already present, check for updates" % label)
+                n = query.first()
+                if n.to != piEmail:
+                    report.append("Updating to for %s" % label)
+                    n.to = piEmail
+                    session.commit()
+                if n.cc != "%s,%s" % (ngoEmail, csEmail):
+                    report.append("Updating cc for %s" % label)
+                    n.cc = "%s,%s" % (ngoEmail, csEmail)
+                    session.commit()
+            
+    report.append("Processed %s programs" % nprogs)
+
+    return report
