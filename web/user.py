19cb49a8984ff0c49a89134e0fa6e7c50de7ad32
fitsdata <fitsdata>
2015-05-23 02:06:15 +0000

    https for password reset link

--- web/user.py
+++ web/user.py
@@ -175,7 +175,7 @@ valid for 15 minutes, so please do that promptly.
     finally:
         session.close()
 
-    url = "http://%s/password_reset/%d/%s" % (fits_servername, userid, token)
+    url = "https://%s/password_reset/%d/%s" % (fits_servername, userid, token)
 
     message = "Hello %s,\n" % fullname
     message += message_text
