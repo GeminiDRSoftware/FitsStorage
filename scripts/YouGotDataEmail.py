c5acb12765368cc0d4853b57cc707f940d2fce7f
Paul Hirst <phirst@gemini.edu>
2015-06-03 23:23:19 +0000

    drop internal concepts, hardware URLs to archive, pylint cleanup

--- scripts/YouGotDataEmail.py
+++ scripts/YouGotDataEmail.py
@@ -14,7 +14,7 @@ from logger import logger, setdebug, setdemon
 parser = OptionParser()
 parser.add_option("--emailfrom", action="store", dest="fromaddr", default="fitsdata@gemini.edu", help="Email Address to send from")
 parser.add_option("--replyto", action="store", dest="replyto", default="fitsadmin@gemini.edu", help="Set a Reply-To email header")
-parser.add_option("--date", action="store", dest="date", default = "today", help="Specify an alternate date to check for data from")
+parser.add_option("--date", action="store", dest="date", default="today", help="Specify an alternate date to check for data from")
 parser.add_option("--debug", action="store_true", dest="debug", help="Increase log level to debug")
 parser.add_option("--demon", action="store_true", dest="demon", help="Run as a background demon, do not generate stdout")
 (options, args) = parser.parse_args()
@@ -22,12 +22,11 @@ parser.add_option("--demon", action="store_true", dest="demon", help="Run as a b
 setdebug(options.debug)
 setdemon(options.demon)
 
-
 # ISG have local smtp servers set up on the fits hosts (mkofits1 and cpofits1) that relay mail to the gemini smtp
 # servers without needing to authenticate. Otherwise, the gemini smtp will not relay to external addresses.
 
 mailhost = "localhost"
-cre = re.compile('\.fits')
+cre = re.compile(r'\.fits')
 
 logger.info("YouveGotDataEmail.py starting for date %s" % options.date)
 
@@ -37,36 +36,31 @@ notifs = session.query(Notification).all()
 
 for notif in notifs:
 
-    if((notif.selection is None) or (notif.to is None)):
-        logger.error("Critical fields are None in notification id: %s; label: %s" % (notif.id, notif.label))
+    if (notif.selection is None) or (notif.to is None):
+        logger.error("Critical fields are None in notification id: %s; label: %s", notif.id, notif.label)
 
     else:
 
-        if(notif.internal):
-            url = "http://fits/summary/%s/%s" % (options.date, notif.selection)
-        else:
-            url = "http://fits/summary/nolinks/%s/%s" % (options.date, notif.selection)
+        url = "https://archive.gemini.edu/summary/%s/%s" % (options.date, notif.selection)
+        searchform_url = "https://archive.gemini.edu/searchform/%s/%s" % (options.date, notif.selection)
 
-        logger.debug("URL is: %s" % url)
+        logger.debug("URL is: %s", url)
         f = urllib2.urlopen(url)
         html = f.read()
         f.close()
 
         match = cre.search(html)
 
-        if(match):
+        if match:
 
             subject = "New Data for %s" % notif.selection
             logger.info(subject)
 
             msg = MIMEMultipart()
 
-            text = "New data has been taken for %s. The attached html file gives details.\n\n" % (notif.selection)
-            if(notif.internal):
-                text += "The fits storage summary table for this data be found at: %s\n\n" % url
-            else:
-                text += "Access to all Gemini data is via the Gemini Science Archive at http://www1.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/gsa/\n"
-                text += "Data Quality assessment and data package release will proceed as normal over the next few days."
+            text = "New data has been taken for %s. The attached html file gives details.\n\n" % notif.selection
+            text += "The archive search for this data may be found at: %s\n\n" % searchform_url
+            text += "Data Quality assessment will proceed as normal over the next few days.\n"
 
             part1 = MIMEText(text, 'plain')
             part2 = MIMEText(html, 'html')
@@ -83,18 +77,18 @@ for notif in notifs:
             fulllist = []
             tolist = notif.to.split(',')
             fulllist += tolist
-            if(notif.cc):
+            if notif.cc:
                 cclist = notif.cc.split(',')
                 fulllist += cclist
-            # For now, I Bcc myself on all the emails to see that it's working...
-            fulllist.append('phirst@gemini.edu')
+            # For now, Bcc fitsadmin on all the emails to see that it's working...
+            fulllist.append('fitsadmin@gemini.edu')
 
             try:
-                logger.info("Sending Email- To: %s; CC: %s; Subject: %s" % (msg['To'], msg['Cc'], msg['Subject']))
+                logger.info("Sending Email- To: %s; CC: %s; Subject: %s", msg['To'], msg['Cc'], msg['Subject'])
                 smtp = smtplib.SMTP(mailhost)
                 smtp.sendmail(options.fromaddr, fulllist, msg.as_string())
                 retval = smtp.quit()
-                logger.info("SMTP seems to have worked OK: %s" % str(retval))
+                logger.info("SMTP seems to have worked OK: %s", str(retval))
             except smtplib.SMTPRecipientsRefused:
                 logger.error("Error sending mail message - Exception: %s: %s" % (sys.exc_info()[0], sys.exc_info()[1]))
 
