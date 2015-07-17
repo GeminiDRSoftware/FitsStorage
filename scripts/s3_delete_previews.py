41350ee61235877bdcbb0ca5319a8575db48bceb
fitsdata <fitsdata>
2015-07-17 02:54:48 +0000

    add option to parrallelize delete

--- scripts/s3_delete_previews.py
+++ scripts/s3_delete_previews.py
@@ -7,6 +7,7 @@ from fits_storage_config import using_s3, aws_access_key, aws_secret_key, s3_buc
 from logger import logger, setdebug, setdemon
 from boto.s3.connection import S3Connection
 
+from multiprocessing import Pool
 
 # Option Parsing
 from optparse import OptionParser
@@ -16,6 +17,8 @@ parser.add_option("--yesimsure", action="store_true", dest="yesimsure", default=
 parser.add_option("--dryrun", action="store_true", dest="dryrun", help="Dry Run - do not actually do anything")
 parser.add_option("--debug", action="store_true", dest="debug", help="Increase log level to debug")
 parser.add_option("--demon", action="store_true", dest="demon", help="Run as a background demon, do not generate stdout")
+parser.add_option("--count", action="store_true", dest="count", help="Only count the number of previews, do not delete them")
+parser.add_option("--threads", action="store", dest="threads", help="Run in parallel with this many threads")
 
 (options, args) = parser.parse_args()
 
@@ -31,7 +34,7 @@ if(using_s3 == False):
     logger.error("This script is only useable on installations using S3 for storage")
     sys.exit(1)
 
-if(options.yesimsure != True):
+if options.yesimsure != True and options.count != True:
     logger.info("This is a really dangerous script to run. If you're not sure, don't do this.")
     logger.info("This will unconditionally delete files from the S3 storage")
     logger.error("You need to say --yesimsure to make it work")
@@ -45,16 +48,33 @@ s3conn = S3Connection(aws_access_key, aws_secret_key)
 bucket = s3conn.get_bucket(s3_bucket_name)
 filelist = []
 for key in bucket.list():
-    filelist.append(key.name)
+    if key.name.endswith("_preview.jpg"):
+        filelist.append(key.name)
+
+if options.count:
+    logger.info("Found %d preview files", len(filelist))
+    sys.exit(0)
+
+def delete_it(filename, logger=None):
+    key = bucket.get_key(filename)
+    if logger:
+        logger.info("Deleting %s", filename)
+    else:
+        print "Deleting %s" % filename
+    key.delete()
 
 
-for filename in filelist:
-    if filename.endswith("_preview.jpg"):
-        if options.dryrun:
-            logger.info("Dryrun - not actually Deleting %s", filename)
-        else:
-            logger.info("Deleting %s", filename)
-            key = bucket.get_key(filename)
-            key.delete()
+if options.threads:
+    threads = int(options.threads)
+    logger.info("Starting parallel delete with %d threads", threads)
+    pool = Pool(threads)
+    pool.map(delete_it, filelist)
+else:
+    for filename in filelist:
+        if filename.endswith("_preview.jpg"):
+            if options.dryrun:
+                logger.info("Dryrun - not actually Deleting %s", filename)
+            else:
+                delete_it(filename, logger=logger)
     
 logger.info("** s3_delete_previews.py exiting normally")
