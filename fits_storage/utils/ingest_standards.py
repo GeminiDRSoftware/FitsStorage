ecd39802492d726d60753ca1e5617dbc1fb008d4
Ricardo Cardenes <rcardenes@gemini.edu>
2015-08-03 20:50:48 +0000

    [utils/ingest_standard] Streamlined the code a bit

--- fits_storage/utils/ingest_standards.py
+++ fits_storage/utils/ingest_standards.py
@@ -5,17 +5,28 @@ in the Fits Storage System.
 from ..orm.photstandard import PhotStandard
 from ..orm.geometryhacks import add_point
 
+mag_pairs = (
+    (4, 'u_mag'),
+    (5, 'v_mag'),
+    (6, 'g_mag'),
+    (7, 'r_mag'),
+    (8, 'i_mag'),
+    (9, 'z_mag'),
+    (10, 'y_mag'),
+    (11, 'j_mag'),
+    (12, 'h_mag'),
+    (13, 'k_mag'),
+    (14, 'lprime_mag'),
+    (15, 'm_mag')
+)
+
 def ingest_standards(session, filename):
     """
     Load the standards text file into the Standards table
     """
-    # open the standards text file
-    fobj = open(filename, 'r')
-    lines = fobj.readlines()
-    fobj.close()
 
-    # Loop through entries, adding to table
-    for line in lines:
+    # Loop through entries in the standards text file, adding to table
+    for line in open(filename, 'r'):
         if line[0] != '#':
             fields = line.strip().split(',')
 
@@ -26,30 +37,9 @@ def ingest_standards(session, filename):
                 std.field = fields[1]
                 std.ra = 15.0*float(fields[2])
                 std.dec = float(fields[3])
-                if fields[4] != 'None':
-                    std.u_mag = float(fields[4])
-                if fields[5] != 'None':
-                    std.v_mag = float(fields[5])
-                if fields[6] != 'None':
-                    std.g_mag = float(fields[6])
-                if fields[7] != 'None':
-                    std.r_mag = float(fields[7])
-                if fields[8] != 'None':
-                    std.i_mag = float(fields[8])
-                if fields[9] != 'None':
-                    std.z_mag = float(fields[9])
-                if fields[10] != 'None':
-                    std.y_mag = float(fields[10])
-                if fields[11] != 'None':
-                    std.j_mag = float(fields[11])
-                if fields[12] != 'None':
-                    std.h_mag = float(fields[12])
-                if fields[13] != 'None':
-                    std.k_mag = float(fields[13])
-                if fields[14] != 'None':
-                    std.lprime_mag = float(fields[14])
-                if fields[15] != 'None':
-                    std.m_mag = float(fields[15])
+                for n, mag in mag_pairs:
+                    if fields[n] != 'None':
+                        setattr(std, mag, float(fields[n]))
             except ValueError:
                 print "Fields: %s" % str(fields)
                 raise
