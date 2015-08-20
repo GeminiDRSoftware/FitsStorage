e34b3fe887d0ecbf1440ed8dcad614ea1ea58b5c
Ricardo Cardenes <rcardenes@gemini.edu>
2015-08-20 00:32:42 +0000

    Remove the --clean option from ingest_standards

--- fits_storage/scripts/ingest_standards.py
+++ fits_storage/scripts/ingest_standards.py
@@ -1,5 +1,5 @@
 import os
-from fits_storage.orm import sessionfactory
+from fits_storage.orm import session_scope
 from fits_storage.logger import logger, setdebug, setdemon
 from fits_storage.utils.ingest_standards import ingest_standards
 from fits_storage.fits_storage_config import fits_aux_datadir
@@ -9,7 +9,6 @@ import datetime
 from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("--file", action="store", type="string", dest="filename", help="Standards text filename")
-parser.add_option("--clean", action="store_true", dest="clean", help="Delete all rows in the table before adding")
 parser.add_option("--debug", action="store_true", dest="debug", default=False, help="Increase log level to debug")
 parser.add_option("--demon", action="store_true", dest="demon", default=False, help="Run in background mode")
 
@@ -22,21 +21,14 @@ setdemon(options.demon)
 # Annouce startup
 logger.info("*********  ingest_standards.py - starting up at %s" % datetime.datetime.now())
 
-session = sessionfactory()
+with session_scope() as session:
+    if options.filename:
+        filename = options.filename
+    else:
+        filename = os.path.join(fits_aux_datadir, "standards.txt")
 
-if(options.clean):
-    logger.info("Deleting all rows in standards table")
-    session.execute("DELETE FROM standards")
+    logger.info("Reading from file: %s" % filename)
 
-if(options.filename):
-    filename = options.filename
-else:
-    filename = os.path.join(fits_aux_datadir, "standards.txt")
+    ingest_standards(session, filename)
 
-logger.info("Reading from file: %s" % filename)
-
-ingest_standards(session, filename)
-
-session.close()
 logger.info("*** ingest_standards exiting normally at %s" % datetime.datetime.now())
-
