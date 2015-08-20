e6a7e1761eaa1d9104ad1a69cafabeca1ccb4446
Ricardo Cardenes <rcardenes@gemini.edu>
2015-08-20 00:32:45 +0000

    [cal] Add comments to CalQuery and remove extra ORDER BY

--- fits_storage/cal/calibration.py
+++ fits_storage/cal/calibration.py
@@ -45,23 +45,44 @@ def not_spectroscopy(f):
     return wrapper
 
 class CalQuery(object):
+    """Class that helps to build the queries for calibration.
+
+       Using CalQuery reduces a great deal of the boilerplate code and provides a
+       kind of DSL (Domain Specific Language) that should make query building more
+       natural -or easier to understand- for the calibrations."""
+
     def __init__(self, session, instrClass, descriptors):
+        # Keep a copy of the instrument descriptors and start the query with
+        # some common filters
         self.descr = descriptors
         self.query = (session.query(Header).select_from(join(join(instrClass, Header), DiskFile))
                                            .filter(DiskFile.canonical == True) # Search only canonical entries
                                            .filter(Header.qa_state != 'Fail')) # Knock out the FAILs
 
     def __call_through(self, query_method, *args, **kw):
+        "Used to make arbitrary calls to the internal SQLAlchemy query object"
         self.query = query_method(*args, **kw)
         return self
 
     def add_filters(self, *args):
+        """Takes a number of arguments (`args`) which are SQLAlchemy expressions.
+
+           Adds each one of those expressions to the internal query as filters, effectively
+           adding conditions like this: `args[0] AND args[1] AND ...`"""
         for arg in args:
             self.query = self.query.filter(arg)
 
         return self
 
     def match_descriptors(self, *args):
+        """In our usual queries, there are plenty of simple filters like this:
+
+              Header.foo == descriptors['foo']
+              Instrument.bar == descriptors['bar']
+
+           `match_descriptors` takes a numbers of expressions (eg. `Header.foo`, `Instrument.bar`),
+           figures out the descriptor to use from the column name, and adds the right filter to
+           the query."""
         for arg in args:
             field = arg.expression.name
             self.query = self.query.filter(arg == self.descr[field])
@@ -69,24 +90,47 @@ class CalQuery(object):
         return self
 
     def max_interval(self, **kw):
+        """Max interval requires keyword arguments. The arguments it accepts are the same as
+           `datetime.timedelta` (`days`, `seconds`, ...)
+
+           Using those arguments, it will construct a timedelta and a filter will be added
+           that constrains the returned calibrations so that their datetimes fit in:
+
+                header.ut_datetime-delta < cal.ut_datetime < header.ut_datetime+delta"""
         max_int = timedelta(**kw)
         datetime_lo = self.descr['ut_datetime'] - max_int
         datetime_hi = self.descr['ut_datetime'] + max_int
-        targ_ut_dt_secs = int((self.descr['ut_datetime'] - Header.UT_DATETIME_SECS_EPOCH).total_seconds())
 
         self.query = (self.query.filter(Header.ut_datetime > datetime_lo) # Absolute time separation
-                                .filter(Header.ut_datetime < datetime_hi)
-                                .order_by(func.abs(Header.ut_datetime_secs - targ_ut_dt_secs))) # Order by absolute time separation.
+                                .filter(Header.ut_datetime < datetime_hi))
 
         return self
 
     def if_(self, condition, methodname, *args, **kw):
+        """A convenience for filters that are added conditionally. To be used like in this example
+           from GMOS:
+
+             if_(self.descriptors['nodandshuffle'], 'match_descriptors', Gmos.nod_count, Gmos.nod_pixels)
+
+           This method exists only so that we can construct a whole query just by chaining."""
         if condition:
             return getattr(self, methodname)(*args, **kw)
 
         return self
 
     def __getattr__(self, name):
+        """The CalQuery instances define only a handful of methods that are used often, but we would
+           still want to expose other elements. Instead of creating methods for them, we try to figure
+           out what the user wanted from the name of the attribute. The attribute search follows this
+           order, returning the first that matches:
+
+             1) See if the the internal SQLAlchemy query has that attribute. In this way we allow
+                arbitrary queries to be formed, but this shouldn't be abused.
+             2) See if the attribute is one of the observation types. If it is, then invoking the
+                returned object will be the same as doing `.observation_type('attribute_name')`
+             3) Same as 2), but with observation classes and the `.observation_class` method
+             4) Same as 2), but with reduction states and the `.reduction` method
+             5) If nothing matches, raise an AttributeError"""
         try:
             return functools.partial(self.__call_through, getattr(self.query, name))
         except AttributeError:
@@ -99,15 +143,30 @@ class CalQuery(object):
 
             raise AttributeError("'{}' object has no attribute '{}'".format(self.__class__.__name__, name))
 
-    def all(self, limit):
-        targ_ut_dt_secs = int((self.descr['ut_datetime'] - Header.UT_DATETIME_SECS_EPOCH).total_seconds())
-        return (self.query.order_by(func.abs(Header.ut_datetime_secs - targ_ut_dt_secs)) # Order by absolute time separation.
-                          .limit(limit)
-                          .all())
+    def all(self, limit, default_order = True):
+        """Returns a list of results, limited in number by the `limit` argument.
+
+           If `default_order` is `True` (the default), then the results are sorted by absolute
+           time separation. If it's `False`, no ordering will be performed; if the user wanted some sorting,
+           it has to be applied *before* invoking this method."""
+        if default_order:
+            # Order by absolute time separation.
+            targ_ut_dt_secs = int((self.descr['ut_datetime'] - Header.UT_DATETIME_SECS_EPOCH).total_seconds())
+            self.query = self.query.order_by(func.abs(Header.ut_datetime_secs - targ_ut_dt_secs))
+        return self.query.limit(limit).all()
 
     # The following add filters specific to certain types of calibs
 
     def tolerance(self, condition=True, **kw):
+        """Takes a number of descriptors as keyword arguments (only valid for the common header, not
+           the instrument specific descriptors), in the form `descriptor=tolerance`.
+
+           The internal query will get added filters for each descriptor to ensure that the results
+           match within the specified tolerance.
+
+           There's an extra keyword argument, `condition`: if condition is false, the tolerances
+           won't be added (by default it's true). This is so that we can add or skip the tolerance
+           tests to the query using just call chaining, to keep everything compact."""
         if condition:
             for descriptor, tol in kw.items():
                 # Occassionally we get a None for some descriptors, so run this in a try except
@@ -118,7 +177,7 @@ class CalQuery(object):
                     column = getattr(Header, descriptor)
                     self.query = (self.query.filter(column > lo).filter(column < hi))
                 except KeyError:
-                    raise RuntimeError("No such descriptor '{}' defined in this instrument".format(descriptor))
+                    raise RuntimeError("No such descriptor '{}' defined in the header".format(descriptor))
                 except TypeError:
                     pass
 
