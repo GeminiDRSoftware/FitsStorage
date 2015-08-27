c90e673710d285c20ae582ad7519450999e3ccea
Ricardo Cardenes <rcardenes@gemini.edu>
2015-08-27 22:05:55 +0000

    Added packages to setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,33 @@ class PyTest(TestCommand):
 setup(
     name='FitsStorage',
     version=fits_storage.__version__,
+    # The following is need only if publishing this under PyPI or similar
+    #description = '...',
+    #author = 'Paul Hirst',
+    #author_email = 'phirst@gemini.edu',
+    #license = 'BSD',
     tests_require=['pytest'],
-    cmdclass = {'pytest': PyTest}
+    cmdclass = {'pytest': PyTest},
+    packages = ['fits_storage',
+                'fits_storage.cal',
+                'fits_storage.orm',
+                'fits_storage.utils',
+                'fits_storage.web'],
+    install_requires = ['psycopg2 >= 2.5',
+                        'sqlalchemy >= 0.9.9',
+                        'pyinotify >= 0.9',
+                        'docopt >= 0.6',
+                        'pyfits >= 3.2'],
+    # We should use entry_points instead of scripts, but that requires
+    # modifying the scripts...
+    scripts = [
+        'fits_storage/scripts/add_to_calcache_queue.py',
+        'fits_storage/scripts/add_to_export_queue.py',
+        'fits_storage/scripts/add_to_ingest_queue.py',
+        'fits_storage/scripts/add_to_prevew_queue.py',
+        'fits_storage/scripts/service_calcache_queue.py',
+        'fits_storage/scripts/service_export_queue.py',
+        'fits_storage/scripts/service_ingest_queue.py',
+        'fits_storage/scripts/service_preview_queue.py',
+        ]
 )
