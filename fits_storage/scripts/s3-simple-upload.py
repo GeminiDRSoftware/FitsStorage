a3fef40e6200fee9d41f2aa45a5995bff678e134
fitsdata <fitsdata>
2015-09-14 20:14:20 +0000

    add filelist and force option

--- fits_storage/scripts/s3-simple-upload.py
+++ fits_storage/scripts/s3-simple-upload.py
@@ -8,8 +8,10 @@ from multiprocessing import Pool
 from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("--file", action="store", dest="file", help="Filename to upload")
+parser.add_option("--filelist", action="store", dest="filelist", help="File giving list of Filenames to upload")
 parser.add_option("--all", action="store_true", dest="all", help="Upload all files in directory")
 parser.add_option("--sort", action="store_true", dest="sort", help="Sort file list before uploading")
+parser.add_option("--force", action="store_true", default=False, dest="force", help="Force upload even if it appears to already exist")
 parser.add_option("--path", action="store", dest="path", default="/net/wikiwiki/dataflow", help="Path to directory where file is")
 parser.add_option("--file-sw", action="store", dest="filesw", help="Starts-with filename filter, for use with --all")
 parser.add_option("--debug", action="store_true", dest="debug", help="Increase log level to debug")
@@ -47,7 +49,7 @@ def do_file(filename, logger=None):
         k = s3.get_key(filename)
         s3md5 = s3.get_md5(k)
         filemd5 = md5sum(fullpath)
-        if s3md5 == filemd5:
+        if s3md5 == filemd5 and not options.force:
             # Identical file already there
             log("%s: Already exists with size %d and MD5 %s", filename, s3.get_size(k), s3md5)
         else:
@@ -76,9 +78,15 @@ if options.all:
         # Get a listing of the directory
         file_list = os.listdir(path)
         logger.info("All files mode - found %d files" % len(file_list))
+elif options.filelist:
+    file_list = []
+    with open(options.filelist) as f:
+        for line in f:
+            file_list.append(line.strip())
 else:
     logger.info("Single file mode: %s" % file)
     file_list = [file]
+
 
 if options.sort:
     # Sort the file list
