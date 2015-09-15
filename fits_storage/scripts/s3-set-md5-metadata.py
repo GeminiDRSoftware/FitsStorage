e5c5bbf87ae72e8f8652c6d6557d5391601c9635
fitsdata <fitsdata>
2015-09-15 21:43:48 +0000

    go multi-threading

--- fits_storage/scripts/s3-set-md5-metadata.py
+++ fits_storage/scripts/s3-set-md5-metadata.py
@@ -10,11 +10,18 @@ from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("--debug", action="store_true", dest="debug", default=False, help="Increase log level to debug")
 parser.add_option("--demon", action="store_true", dest="demon", default=False, help="Run as a background demon, do not generate stdout")
-parser.add_option("--filepre", action="store", dest="filepre", help="File prefix to operate on")
+parser.add_option("--threads", action="store", dest="threads", help="Number of threads to run")
 options, args = parser.parse_args()
 # Logging level to debug? Include stdio log?
 setdebug(options.debug)
 setdemon(options.demon)
+
+def feed_names():
+    # Get a full listing from S3.
+    logger.info("Getting file list from S3")
+
+    for obj in s3.bucket.objects.all():
+        yield obj.key
 
 # Annouce startup
 logger.info("*********    s3-set-md5-metadata - starting up at %s" % datetime.datetime.now())
@@ -27,22 +34,18 @@ from fits_storage.utils.aws_s3 import get_helper
 from fits_storage.utils.hashes import md5sum, md5sum_size_fp
 
 s3 = get_helper()
-logger.info("Querying files from S3 bucket: %s" % s3.bucket.name)
 
 def fetch_and_compute(keyname):
     with s3.fetch_temporary(keyname, skip_tests=True) as fileobj:
         return md5sum_size_fp(fileobj)[0]
 
-for objsum in s3.bucket.objects.all():
+def process_it(objsum):
     name = objsum.key
     if name.endswith('_preview.jpg'):
-        continue
-    if options.filepre and not name.startswith(options.filepre):
-        continue
+        return "skipping preview {}".format(name)
 
     if s3.get_md5(name):
-        logger.info("Found metadata for {}".format(name))
-        continue
+        return "Found metadata for {}".format(name)
 
     etag = s3.get_etag(s3.get_key(name)).replace('"', '')
     computed_md5 = None
@@ -52,4 +55,18 @@ for objsum in s3.bucket.objects.all():
     except ValueError:
         pass
     s3.set_metadata(name, md5=(computed_md5 if computed_md5 is not None else fetch_and_compute(name)))
-    logger.info("Metadata MD5 set for {}".format(name))
+    return "Metadata MD5 set for {}".format(name)
+
+if options.threads:
+    threads = int(options.threads)
+    logger.info("Starting parallel process with %d threads", threads)
+    pool = Pool(threads)
+    for result in pool.imap_unordered(process_it, feed_names(), chunksize=100):
+        logger.info(result)
+else:
+    for filename in feed_names():
+        logger.info(process_ie(filename))
+
+
+logger.info("*********    s3-set-md5-metadata - ending at %s" % datetime.datetime.now())
+
