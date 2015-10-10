f387f3ab7fc885f61aadb06bbee2faea3a2aa183
Ricardo Cardenes <rcardenes@gemini.edu>
2015-10-10 02:13:59 +0000

    On dbmigrate, create table using the existing definition

--- dbmigration/versions/007_Add_misc_files_table.py
+++ dbmigration/versions/007_Add_misc_files_table.py
@@ -3,16 +3,7 @@ from migrate import *
 from fits_storage.orm.miscfile import MiscFile
 
 def upgrade(migrate_engine):
-    meta = MetaData(bind = migrate_engine)
-    diskfile = Table('diskfile', meta, autoload=True)
-    miscfile = Table('miscfile', meta,
-            Column('id', Integer, primary_key=True),
-            Column('diskfile_id', Integer, ForeignKey('diskfile.id'), nullable=False, index=True),
-            Column('release', DateTime, nullable=False),
-            Column('description', Text),
-            Column('program_id', Text, index=True)
-        )
-    meta.create_all()
+    MiscFile.metadata.create_all(migrate_engine)
 
 def downgrade(migrate_engine):
     meta = MetaData(bind = migrate_engine)
