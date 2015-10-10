50efe0bc7e8429b92faaa7af00690471fce493cb
fitsdata <fitsdata>
2015-10-10 00:41:26 +0000

    add option to exclude queue tables

--- fits_storage/scripts/database_backup.py
+++ fits_storage/scripts/database_backup.py
@@ -12,6 +12,7 @@ from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("--dontdelete", action="store_true", dest="dontdelete", help="Don't actually delete anything, just say what would be deleted")
 parser.add_option("--dontbackup", action="store_true", dest="dontbackup", help="Don't back up the database, just clean up")
+parser.add_option("--exclude-queues", action="store_true", dest="queues", help="Dont dump the queue tables as this conflicts with their locking and causes the queues to hang up during backup.")
 parser.add_option("--debug", action="store_true", dest="debug", help="Increase log level to debug")
 parser.add_option("--demon", action="store_true", dest="demon", help="Run as a background demon, do not generate stdout")
 
@@ -25,11 +26,15 @@ setdemon(options.demon)
 now = datetime.datetime.now()
 logger.info("*********    database_backup.py - starting up at %s" % now)
 
+
 # BACKUP STUFF
 if not options.dontbackup:
     # The backup filename
     filename = "%s.%s.pg_dump_c" % (fits_dbname, datestring)
-    command = ["/usr/bin/pg_dump", "--format=c", "--file=%s/%s" % (fits_db_backup_dir, filename), fits_dbname]
+    if options.queues:
+        command = ["/usr/bin/pg_dump", "--format=c", "--file=%s/%s" % (fits_db_backup_dir, filename), '--exclude-table=*queue', fits_dbname]
+    else:
+        command = ["/usr/bin/pg_dump", "--format=c", "--file=%s/%s" % (fits_db_backup_dir, filename), fits_dbname]
 
     logger.info("Executing pg_dump")
     sp = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
