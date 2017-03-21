42a0cfa9f3ff61e3cf5f837d6ca5b2d4f40bde87
Paul Hirst <phirst@gemini.edu>
2017-03-21 19:47:09 +0000

    fixes

--- fits_storage/scripts/local_delete_files.py
+++ fits_storage/scripts/local_delete_files.py
@@ -11,12 +11,16 @@ parser = OptionParser()
 parser.add_option("--tapeserver", action="store", type="string", dest="tapeserver", default="hbffitstape1", help="The Fits Storage Tape server to use to check the files are on tape")
 parser.add_option("--file-pre", action="store", type="string", dest="filepre", help="File prefix to operate on, eg N20090130, N200812 etc")
 parser.add_option("--mintapes", action="store", type="int", dest="mintapes", default=2, help="Minimum number of tapes file must be on to be eligable for deletion")
+parser.add_option("--dir", action="store", type="string", dest="dir", help="Directory to operate in")
 parser.add_option("--nomd5", action="store_true", dest="nomd5", help="Do not check md5, match on filename only")
 parser.add_option("--dryrun", action="store_true", dest="dryrun", help="Dry Run - do not actually do anything")
 parser.add_option("--debug", action="store_true", dest="debug", help="Increase log level to debug")
 parser.add_option("--demon", action="store_true", dest="demon", help="Run as a background demon, do not generate stdout")
 
 options, args = parser.parse_args()
+
+if options.dir:
+  os.chdir(options.dir)
 
 rawlist = os.listdir('.')
 restring = '^' + options.filepre + '.*'
@@ -29,7 +33,7 @@ if options.dryrun:
     def remove(fname, md5, tapes):
         print "Dry run - not actually deleting File %s - %s which is on %d tapes: %s" % (fname, md5, len(tapes), tapes)
 else:
-    def remove(fname, md5, tapes)
+    def remove(fname, md5, tapes):
         print "Deleting File %s - %s which is on %d tapes: %s" % (fname, md5, len(tapes), tapes)
         try:
             os.unlink(fname)
