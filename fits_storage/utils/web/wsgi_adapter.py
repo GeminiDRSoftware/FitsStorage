76a9fec4e25cd7870432d0997f1bc69d1d8106da
Kenneth Anderson <kanderso@gemini.edu>
2018-05-03 21:33:13 +0000

    fix on FieldStorage.__init__

--- fits_storage/utils/web/wsgi_adapter.py
+++ fits_storage/utils/web/wsgi_adapter.py
@@ -19,8 +19,7 @@ def Uploadedfile(object):
 
 class ItemizedFieldStorage(FieldStorage):
     def __init__(self, fp, environ):
-        #super(FieldStorage, self).__init__(fp, environ)
-        FieldStorage.__init__(self, fp, environ)
+        FieldStorage.__init__(self, fp, environ=environ)
         if self.filename is None:
             self.uploaded_file = None
         else:
