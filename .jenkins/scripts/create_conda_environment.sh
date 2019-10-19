e5c34b0936bd839f5248a8029366a48c450af614
ooberdorf <ooberdorf>
2019-10-19 01:56:18 +0000

    More fiddling with conda env create to get py3

--- .jenkins/scripts/create_conda_environment.sh
+++ .jenkins/scripts/create_conda_environment.sh
@@ -6,12 +6,12 @@
 # can be skipped
 
 echo " Removing ${CONDA_ENV_NAME} ---"
-conda env remove -n "${CONDA_ENV_NAME}" || echo 0
+conda remove -n "${CONDA_ENV_NAME}" || echo 0
 
 
 echo " Installing ${CONDA_ENV_NAME} ---"
 if conda info --envs | grep -q $CONDA_ENV_NAME; then
     echo " Skipping creating of existing conda environment: ${CONDA_ENV_NAME}";
 else
-    conda env create --quiet --file ${CONDA_ENV_FILE} -n "${CONDA_ENV_NAME}" python=3;
+    conda create --file ${CONDA_ENV_FILE} --name "${CONDA_ENV_NAME}" python=3 ;
 fi
