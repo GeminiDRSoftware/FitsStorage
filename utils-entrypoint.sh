c99d9452179465d4f074427573c0338fed90747a
ooberdorf <ooberdorf>
2019-10-24 01:07:01 +0000

    Removed unneeded entrypoint script

--- utils-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Needed for good Jenkins/Docker integration
-# See akso best practices for ENTRYPOINT:
-# https://docs.docker.com/develop/develop-images/dockerfile_best-practices/
-
-set -e
-
-exec "$@"
