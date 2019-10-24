318e3748755f154827d101b771ddf60cebe23d94
ooberdorf <ooberdorf>
2019-10-24 02:35:54 +0000

    Adding back in ORM tests with hopefully now working containers to support them

--- tests/orm/test_orm.py
+++ tests/orm/test_orm.py
@@ -15,7 +15,6 @@ def user(request, session):
     return user
 
 @pytest.mark.usefixtures("rollback")
-@pytest.mark.slow
 class TestUser:
     def test_reset_and_validate_password(self, session, user):
         assert user.password is None  # For a just created user, this should be null
