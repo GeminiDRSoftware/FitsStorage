220c81388068f40fab20c197f740369ba6b56230
Oliver <oly@oberdorf.org>
2020-01-16 10:42:47 -1000

    enhancements to spectral preview

--- fits_storage/utils/previewqueue.py
+++ fits_storage/utils/previewqueue.py
@@ -394,13 +394,15 @@ class PreviewQueueUtil(object):
             # full = numpy.squeeze(full)
             spek = Spek1D(ad[0])
             flux = spek.flux
+            uncertainty = spek.uncertainty.quantity
             #mask values below a certain threshold
             flux_masked = numpy.ma.masked_where(spek.mask == 16, flux)
+            uncertainty_masked = numpy.ma.masked_where(spek.mask == 16, uncertainty)
 
             try:
                 plt.title(spek.filename)
-                plt.xlabel(spek.spectral_axis_unit)
-                plt.ylabel(spek.unit)
+                plt.xlabel("wavelength %s" % spek.spectral_axis_unit)
+                plt.ylabel("flux density %s" % spek.unit)
             except Exception as e:
                 pass
             try:
@@ -408,10 +410,12 @@ class PreviewQueueUtil(object):
                 # full = full[~numpy.isnan(full)]
                 # full = numpy.squeeze(full)
                 plt.plot(x_axis, flux_masked)
+                plt.plot(x_axis, uncertainty_masked, color='r')
             except Exception as e:
                 string = "".join(traceback.format_tb(sys.exc_info()[2]))
                 #self.l.error("Recovering (simplified preview) from Exception: %s : %s... %s" % (sys.exc_info()[0], sys.exc_info()[1], string))
                 plt.plot(flux_masked)
+                plt.plot(uncertainty_masked, color='r')
         else:
             ax = plt.Axes(fig, [0, 0, 1, 1])
             ax.set_axis_off()
