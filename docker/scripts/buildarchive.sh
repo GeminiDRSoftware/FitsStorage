b845638d9e13c2a2d942fda754bfe11345610668
Oliver <oly@oberdorf.org>
2020-01-17 10:31:08 -1000

    added -u option to docker build scripts to push tagged image :latest or :2020-1 to gitlab (for master and 2020-1 only, obvs not on 2020-1 branch but oh well)

--- docker/scripts/buildarchive.sh
+++ docker/scripts/buildarchive.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 BASEDIR=$(dirname "$0")
+UPLOAD=$1
 pushd "$BASEDIR/../.."
 docker image build -t fitsimage:latest -f docker/archive//Dockerfile .
+
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+echo Branch is $BRANCH
+if [[ $UPLOAD == "-u" ]]
+then
+  if [[ "$BRANCH" == "master" ]]
+  then
+    echo "setting label to latest"
+    docker login gitlab.gemini.edu:4567
+    docker tag fitsimage:latest gitlab.gemini.edu:4567/drsoftware/fitsstorage/fitsimage:latest
+    docker push gitlab.gemini.edu:4567/drsoftware/fitsstorage/fitsimage:latest
+  elif [[ "$BRANCH" == "2020-1" ]]
+  then
+    echo "setting label to 2020-1"
+  fi
+fi
+
 popd
--- docker/scripts/buildfitsstorageutils.sh
+++ docker/scripts/buildfitsstorageutils.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 BASEDIR=$(dirname "$0")
+UPLOAD=$1
 pushd "$BASEDIR/../.."
 docker image build -t fitsstorageutils:latest -f docker/fitsstorage/Dockerfile .
+
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+echo Branch is $BRANCH
+if [[ $UPLOAD == "-u" ]]
+then
+  if [[ "$BRANCH" == "master" ]]
+  then
+    echo "setting label to latest"
+    docker login gitlab.gemini.edu:4567
+    docker tag fitsstorageutils:latest gitlab.gemini.edu:4567/drsoftware/fitsstorage/fitsstorageutils:latest
+    docker push gitlab.gemini.edu:4567/drsoftware/fitsstorage/fitsstorageutils:latest
+  elif [[ "$BRANCH" == "2020-1" ]]
+  then
+    echo "setting label to 2020-1"
+  fi
+fi
+
 popd
