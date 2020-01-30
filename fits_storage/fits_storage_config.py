d23c55da19a09f0df3361481c84d4270e1a82ddd
Oliver <oly@oberdorf.org>
2020-01-30 13:52:27 -1000

    Added entry for archive hostname in configs

--- fits_storage/fits_storage_config.py
+++ fits_storage/fits_storage_config.py
@@ -51,8 +51,11 @@ _host_based_configs = {
         'BLOCKED_URLS': ''
     },
     "archive": {
+        'FITS_SERVERTITLE': 'Gemini Observatory Archive',
         'USE_AS_ARCHIVE': 'True',
-        'FITS_SYSTEM_STATUS': 'production'
+        'FITS_SYSTEM_STATUS': 'production',
+        'EXPORT_DESTINATIONS': '',
+        'BLOCKED_URLS': 'fileontape,qareport,qametrics,qaforgui,tape,tapewrite,tapefile,taperead,xmltape,gmoscal,update_headers,ingest_files'
     },
     "arcdev": {
         'FITS_SERVERTITLE': 'TEST Archive (AWS) FitsServer (CentOS 7)',
