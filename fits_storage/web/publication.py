f01a7dc039810d41859c3c4b014cffd315a8fe61
Oliver <oly@oberdorf.org>
2020-02-18 12:46:34 -1000

    Adding the template for the publication ADS data

--- fits_storage/web/publication.py
+++ fits_storage/web/publication.py
@@ -1,22 +1,41 @@
 # Provides functionality to get reports on publications by their bibcode
 
+from . import templating
+
 from ..orm.publication import Publication
 
 from ..utils.web import get_context, Return
 
 
-def publication_ads(bibcode):
+@templating.templated("publication_ads.txt", content_type='text/plain')
+def publication_ads(bibcode=None):
     ctx = get_context()
     resp = ctx.resp
     session = ctx.session
 
-    if bibcode is None:
-        # OK, they must have fed us garbage
-        resp.content_type = "text/plain"
-        resp.client_error(Return.HTTP_NOT_FOUND, "Need to provide a bibcode")
+    print("In publication_ads")
 
-    query = session.query(Publication).filter(Publication.bibcode == bibcode)
-    publication = query.one()
+    # if bibcode is None:
+    #     # OK, they must have fed us garbage
+    #     resp.content_type = "text/plain"
+    #     resp.client_error(Return.HTTP_NOT_FOUND, "Need to provide a bibcode")
+
+    if bibcode is not None and bibcode.startswith('bibcode='):
+        bibcode = bibcode[8:]
+    print("Doing query, bibcode is %s" % bibcode)
+    query = session.query(Publication) # .filter(Publication.bibcode == bibcode)
+    publication = query.first()
+    print("Did query for one row")
+    if publication is None:
+        resp.content_type = "text/plain"
+        resp.client_error(Return.HTTP_NOT_FOUND, "No rows found")
+    return dict(
+        bibcode = publication.bibcode,
+        author = publication.author,
+        journal = publication.journal,
+        year = publication.year,
+        title = publication.title
+    )
 
     resp.content_type = "text/plain"
     resp.append(publication.publication_ads())
