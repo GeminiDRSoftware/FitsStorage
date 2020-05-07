d70e6ddc21123679d0c6388780a9a0632bb16798
Oliver <oly@oberdorf.org>
2020-05-06 16:34:48 -1000

    fixed inverted logic for what was set wrong, incorporated BORKED idea from Paul

--- fits_storage/scripts/unbork_nodandshuffle.py
+++ fits_storage/scripts/unbork_nodandshuffle.py
@@ -10,11 +10,8 @@ from fits_storage.orm.fulltextheader import FullTextHeader
 from fits_storage.orm.header import Header
 
 parser = OptionParser()
-parser.add_option("--start", action="store", type="int", help="specify a start id")
 parser.add_option("--limit", action="store", type="int", help="specify a limit on the number of headers to examine")
-parser.add_option("--step", action="store", type="int", help="specify a limit on the number of headers to do per batch")
 parser.add_option("--debug", action="store_true", dest="debug", help="Increase log level to debug")
-parser.add_option("--dryrun", action="store_true", dest="dryrun", help="Don't actually fix")
 
 (options, args) = parser.parse_args()
 
@@ -26,37 +23,27 @@ setdebug(options.debug)
 logger.info("*********    unbork_nodandshuffle.py - starting up at %s" % datetime.datetime.now())
 
 # Get a database session
-start = options.start
-end = start + options.limit
-while start <= end:
-    stop = min(end, start+options.step)
+done = False
+while not done:
+    done = True
     with session_scope() as session:
         # Get a list of all diskfile_ids marked as present
         query = session.query(Header, FullTextHeader) \
-            .filter(Header.id >= start) \
-            .filter(Header.id < stop) \
             .filter(FullTextHeader.diskfile_id == Header.diskfile_id) \
             .filter(Header.instrument.like('GMOS%')) \
-            .filter(Header.detector_readmode_setting == 'BORKED').all()
+            .filter(Header.detector_readmode_setting == 'BORKED').limit(options.limit)
 
         logger.info("Starting checking...")
 
-        count = 0
         for hdr, fth in query:
+            # if we got any matches, we'll want to repeat our search
+            done = False
             if "'NODANDSHUFFLE'" in fth.fulltext:  # always true, we could do something clever to validate it's the tags
                 readmode = 'NodAndShuffle'
             else:
                 readmode = 'Classic'
-            if options.dryrun:
-                logger.info("Would update header: %s to mode %s" % (hdr.id, readmode))
-            else:
-                hdr.detector_readmode_setting = readmode
-                count = count + 1
-                if (count % 1000) == 0:
-                    session.commit()
-                    count = 0
-        if count > 0:
+            hdr.detector_readmode_setting = readmode
+        if not done:
             session.commit()
-    start = start + options.step
 
 logger.info("*** unbork_nodandshuffle.py exiting normally at %s" % datetime.datetime.now())
