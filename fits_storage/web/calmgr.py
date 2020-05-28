8408164e0981eca7e12f951db6142c49bb1a2627
Oliver <oly@oberdorf.org>
2020-05-28 13:35:58 -1000

    Added simplejson for sane generator handling in json conversions

--- fits_storage/web/calmgr.py
+++ fits_storage/web/calmgr.py
@@ -1,7 +1,7 @@
 """
 This module contains the calmgr html generator function.
 """
-import json
+import simplejson as json
 
 from fits_storage.utils.api import WSGIError, BAD_REQUEST
 
@@ -216,14 +216,8 @@ def generate_get_calmgr(selection, caltype):
 
 
 def jsoncalmgr(selection):
-    first = True
     dat = calmgr(selection)
-    get_context().resp.append('[\n')
-    for i in dat['generator']:
-        if not first:
-            get_context().resp.append(',\n')
-        get_context().resp.append(json.dumps(i, indent=4))
-    get_context().resp.append(']\n')
+    get_context().resp.append(json.dumps(dat['generator'], indent=4, iterable_as_array=True))
 
 
 @templating.templated("calmgr.xml", content_type='text/xml')
