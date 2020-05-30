93702ea07024de4115c44d042e3a89e3c7673f34
Oliver <oly@oberdorf.org>
2020-05-29 16:01:34 -1000

    Adding PROCESSED_STANDARD reduction type, fix to obstype migration

--- dbmigration/versions/012_add_processed_standard_reduction_state.py
+++ dbmigration/versions/012_add_processed_standard_reduction_state.py
@@ -4,17 +4,17 @@ from migrate import *
 
 def upgrade(migrate_engine):
     with migrate_engine.connect() as connection:
-        # connection.execute("ALTER TYPE obstype ADD VALUE 'STANDARD'")
+        # connection.execute("ALTER TYPE reduction_state ADD VALUE 'PROCESSED_STANDARD'")
         # connection.commit()
 
         # HACK HACK HACK
-        # Can't use ALTER TYPE obstype ADD VALUE inside a transaction because postgres
+        # Can't use ALTER TYPE reduction_state ADD VALUE inside a transaction because postgres
         # So, we do this
         # see: https://stackoverflow.com/questions/1771543/adding-a-new-value-to-an-existing-enum-type/41696273#41696273
         connection.execute("INSERT INTO pg_enum (enumtypid, enumlabel, enumsortorder) "
-                           "SELECT 'obstype'::regtype::oid, 'STANDARD', "
+                           "SELECT 'reduction_state'::regtype::oid, 'PROCESSED_STANDARD', "
                            "( SELECT MAX(enumsortorder) + 1 FROM pg_enum "
-                           "WHERE enumtypid = 'obstype'::regtype )")
+                           "WHERE enumtypid = 'reduction_state'::regtype )")
 
 
 def downgrade(migrate_engine):
