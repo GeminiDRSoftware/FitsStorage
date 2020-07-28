41889a55aaefb0127be507d5230da79359abc67f
Oliver <oly@oberdorf.org>
2020-07-28 13:30:27 -1000

    fixes for date parsing utility, tests for diskfile timestamp column

--- tests/orm/test_diskfile.py
+++ tests/orm/test_diskfile.py
@@ -10,11 +10,6 @@ def test_standard_filename():
     assert(dt == datetime(year=2020, month=5, day=1))
 
 
-def test_future_filename():
-    dt = _determine_timestamp_from_filename('N20300501S0101.fits')
-    assert(dt is None)
-
-
 def test_igrins_filename():
     dt = _determine_timestamp_from_filename('SDCK_20200210_0071.fits')
     assert(dt == datetime(year=2020, month=2, day=10))
@@ -22,7 +17,7 @@ def test_igrins_filename():
 
 def test_skycam_filename():
     dt = _determine_timestamp_from_filename('img_20170810_11h03m35s.fits')
-    assert(dt == datetime(year=2018, month=8, day=10))
+    assert(dt == datetime(year=2017, month=8, day=10))
 
 
 def test_bizzare_filename():
