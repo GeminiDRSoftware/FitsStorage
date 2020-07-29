93795c3cb7a4f52b85abbe27992b2489814f2517
Oliver <oly@oberdorf.org>
2020-07-28 14:49:04 -1000

    Added initialization logic for new timestamp column based on filenames

--- dbmigration/versions/017_datafile_timestamp.py
+++ dbmigration/versions/017_datafile_timestamp.py
@@ -13,6 +13,12 @@ def upgrade(migrate_engine):
     i = Index('idx_diskfile_datafile_timestamp', datafile_timestamp)
     i.create(migrate_engine)
 
+    with migrate_engine.connect() as connection:
+        connection.execute("update diskfile set datafile_timestamp=to_date(substring(filename from 2 for 8), 'YYYYMMDD') where filename like 'N20[0-9][0-9][0-1][0-9][0-3][0-9]%'")
+        connection.execute("update diskfile set datafile_timestamp=to_date(substring(filename from 5 for 8), 'YYYYMMDD') where filename like 'img_20[0-9][0-9][0-1][0-9][0-3][0-9]%'")
+        connection.execute("update diskfile set datafile_timestamp=to_date(substring(filename from 5 for 8), 'YYYYMMDD') where filename like '[A-Z][A-Z][A-Z][A-Z]_20[0-9][0-9][0-1][0-9][0-3][0-9]%'")
+        connection.execute("update diskfile set datafile_timestamp=lastmod where datafile_timestamp is NULL")
+
 
 def downgrade(migrate_engine):
     meta = MetaData(bind = migrate_engine)
