d62621f977388ba1c73ec8438d8a77fdf92f19ca
Oliver <oly@oberdorf.org>
2020-08-19 13:45:18 -1000

    cleanup pre-test for bad DB state

--- tests/orm/test_calcachequeue.py
+++ tests/orm/test_calcachequeue.py
@@ -15,11 +15,14 @@ def test_constructor():
 
 @pytest.mark.usefixtures("rollback")
 def test_find_not_in_progress(session):
+    for oldccq in session.query(CalCacheQueue).filter(CalCacheQueue.filename == 'filename').all():
+        session.delete(oldccq)
     ccq = CalCacheQueue(1, 'filename', 'aaasortkey')
-    session.save(ccq)
+    session.add(ccq)
     results = CalCacheQueue.find_not_in_progress(session)
     assert(results is not None)
     ccq_result = results.first()
     assert(ccq_result.obs_hid == 1)
     assert(ccq_result.filename == 'filename')
     assert(ccq_result.sortkey == 'aaasortkey')
+    session.delete(ccq)
