028f968a1bbe2307496b65482d93fdec6e62e2ee
Oliver <oly@oberdorf.org>
2020-08-19 14:13:50 -1000

    removing sortkey, was assuming string incorrectly

--- tests/orm/test_calcachequeue.py
+++ tests/orm/test_calcachequeue.py
@@ -17,12 +17,11 @@ def test_constructor():
 def test_find_not_in_progress(session):
     for oldccq in session.query(CalCacheQueue).filter(CalCacheQueue.filename == 'filename').all():
         session.delete(oldccq)
-    ccq = CalCacheQueue(1, 'filename', 'aaasortkey')
+    ccq = CalCacheQueue(1, 'filename')
     session.add(ccq)
     results = CalCacheQueue.find_not_in_progress(session)
     assert(results is not None)
     ccq_result = results.first()
     assert(ccq_result.obs_hid == 1)
     assert(ccq_result.filename == 'filename')
-    assert(ccq_result.sortkey == 'aaasortkey')
     session.delete(ccq)
