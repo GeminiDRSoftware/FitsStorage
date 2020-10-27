696abce4cfd516eb4f1d8a8d04411be14c2a60f6
Oliver <oly@oberdorf.org>
2020-10-26 14:29:00 -1000

    added force option to previewqueue which will force regeneration of the preview file even if we already have it

--- fits_storage/scripts/add_to_prevew_queue.py
+++ fits_storage/scripts/add_to_prevew_queue.py
@@ -19,11 +19,17 @@ if __name__ == "__main__":
     # Option Parsing
     from optparse import OptionParser
     parser = OptionParser()
-    parser.add_option("--file-pre", action="store", type="string", dest="file_pre", help="filename iprefix to select files to queue by")
-    parser.add_option("--instrument", action="store", type="string", dest="instrument", help="add files from this instrument only")
-    parser.add_option("--all", action="store_true", dest="all", help="queue all observations in database. Use with Caution")
+    parser.add_option("--file-pre", action="store", type="string", dest="file_pre",
+                      help="filename iprefix to select files to queue by")
+    parser.add_option("--instrument", action="store", type="string", dest="instrument",
+                      help="add files from this instrument only")
+    parser.add_option("--all", action="store_true", dest="all",
+                      help="queue all observations in database. Use with Caution")
     parser.add_option("--debug", action="store_true", dest="debug", help="Increase log level to debug")
-    parser.add_option("--demon", action="store_true", dest="demon", help="Run as a background demon, do not generate stdout")
+    parser.add_option("--demon", action="store_true", dest="demon",
+                      help="Run as a background demon, do not generate stdout")
+    parser.add_option("--force", action="store_true", dest="force",
+                      help="Force (re)creation of the preview even if we already have one")
 
     options, args = parser.parse_args()
 
@@ -55,6 +61,6 @@ if __name__ == "__main__":
 
         logger.info("Got %d diskfileitems to queue" % len(dfs))
 
-        PreviewQueueUtil(session, logger).process(dfs)
+        PreviewQueueUtil(session, logger).process(dfs, force=options.force)
 
     logger.info("*** add_to_preview_queue.py exiting normally at %s" % datetime.datetime.now())
