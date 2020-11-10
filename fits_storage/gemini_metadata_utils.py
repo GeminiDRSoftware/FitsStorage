603682260c8bee9b9f8adaa70993d70ff040af22
Oliver <oly@oberdorf.org>
2020-11-09 14:09:17 -1000

    adding db update for reduction state, pulled in preview queue force flag since it needs to be earlier in sequence (for the numbering)

--- fits_storage/gemini_metadata_utils.py
+++ fits_storage/gemini_metadata_utils.py
@@ -584,7 +584,7 @@ def gemini_observation_class(string):
 reduction_states = ('RAW', 'PREPARED', 'PROCESSED_FLAT', 'PROCESSED_BIAS',
                     'PROCESSED_FRINGE', 'PROCESSED_ARC', 'PROCESSED_DARK',
                     'PROCESSED_TELLURIC', 'PROCESSED_SCIENCE', 'PROCESSED_STANDARD',
-                    'PROCESSED_SLITILLUM')
+                    'PROCESSED_SLITILLUM', 'PROCESSED_UNKNOWN')
 def gemini_reduction_state(string):
     """
     A utility function matching Gemini reduction states.
