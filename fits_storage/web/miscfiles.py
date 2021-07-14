b5dc4a608e6c4eb46c7fb7320c8bd8cde884ecdf
Oliver <oly@oberdorf.org>
2021-07-14 09:34:27 -1000

    moved another exception import from the old location for the older SqlAlchemy

--- fits_storage/web/miscfiles.py
+++ fits_storage/web/miscfiles.py
@@ -1,8 +1,7 @@
 from ..orm.miscfile import MiscFile, normalize_diskname
 from gemini_obs_db.orm.diskfile import DiskFile
 from gemini_obs_db.orm.file import File
-from sqlalchemy.exc import MultipleResultsFound
-from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 
 from . import templating
 
