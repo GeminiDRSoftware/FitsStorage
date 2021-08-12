49c9b0cca3a74a560a2ed363a9680a6f3e8572d5
Oliver <oly@oberdorf.org>
2021-08-12 11:08:59 -1000

    updates to fix jenkins test script testing script

--- docker/scripts/testlikejenkins.sh
+++ docker/scripts/testlikejenkins.sh
@@ -15,10 +15,10 @@ docker container rm archive-tester || true
 docker run --network fitsstorage-jenkins --name fitsdata-jenkins -e POSTGRES_USER=fitsdata -e POSTGRES_PASSWORD=fitsdata -e POSTGRES_DB=fitsdata -d postgres:12
 
 # Start Archive
-docker run --network fitsstorage-jenkins --name archive-jenkins -e FITS_DB_SERVER="fitsdata:fitsdata@fitsdata-jenkins" -e TEST_IMAGE_PATH=/tmp/archive_test_images -e TEST_IMAGE_CACHE=/tmp/cached_archive_test_images -e CREATE_TEST_DB=False -e PYTHONPATH=/opt/FitsStorage:/opt/DRAGONS -d gemini/archive:jenkins
+docker run --network fitsstorage-jenkins --name archive-jenkins -e FITS_DB_SERVER="fitsdata:fitsdata@fitsdata-jenkins" -e TEST_IMAGE_PATH=/tmp/archive_test_images -e TEST_IMAGE_CACHE=/tmp/cached_archive_test_images -e CREATE_TEST_DB=False -e PYTHONPATH=/opt/FitsStorage:/opt/DRAGONS:/opt/GeminiCalMgr:/opt/FitsStorageDB -d gemini/archive:jenkins
 
 # Run Table init and tests
-docker run --name archive-tester  --network fitsstorage-jenkins -e FITS_DB_SERVER="fitsdata:fitsdata@fitsdata-jenkins" -e PYTEST_SERVER=http://archive-jenkins -e TEST_IMAGE_PATH=/tmp/archive_test_images -e TEST_IMAGE_CACHE=/tmp/cached_archive_test_images -e CREATE_TEST_DB=False -e PYTHONPATH=/opt/FitsStorage:/opt/DRAGONS -d gemini/fitsarchiveutils:jenkins bash -c "python fits_storage/scripts/create_tables.py; mkdir -p /tmp/archive_test_images; mkdir -p /tmp/cached_archive_test_images; cd /opt/FitsStorage && pytest tests "
+docker run --name archive-tester  --network fitsstorage-jenkins -e FITS_DB_SERVER="fitsdata:fitsdata@fitsdata-jenkins" -e PYTEST_SERVER=http://archive-jenkins -e TEST_IMAGE_PATH=/tmp/archive_test_images -e TEST_IMAGE_CACHE=/tmp/cached_archive_test_images -e CREATE_TEST_DB=False -e PYTHONPATH=/opt/FitsStorage:/opt/DRAGONS:/opt/GeminiCalMgr:/opt/FitsStorageDB -d gemini/fitsarchiveutils:jenkins bash -c "python3 fits_storage/scripts/create_tables.py; mkdir -p /tmp/archive_test_images; mkdir -p /tmp/cached_archive_test_images; cd /opt/FitsStorage && pytest tests "
 
 docker container stop archive-jenkins
 docker container stop fitsdata-jenkins
