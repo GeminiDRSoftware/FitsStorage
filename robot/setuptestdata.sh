769685e19b803da42ab40e25befd5d33aee28d2d
Oliver <oly@oberdorf.org>
2021-10-15 09:02:09 -1000

    added more data files to robot download

--- robot/setuptestdata.sh
+++ robot/setuptestdata.sh
@@ -2,4 +2,13 @@
 
 cd /tmp/jenkins_pytest/dataflow/
 
-wget --no-check-certificate https://archive.gemini.edu/file/N20130711S0203.fits
+for file in \
+  N20130711S0203.fits \
+  20130711_GN-2013B-Q-60_obslog.txt \
+  N20120629S0171.fits \
+  abu01aug16_001.fits \
+  S20070131S0145.fits \
+  2003jun29_3341.fits
+  do
+    wget --no-check-certificate https://archive.gemini.edu/file/$file
+  done
