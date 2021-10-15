0994a376b2d81599fa92e1f7a3f090c4b398d0d0
Oliver <oly@oberdorf.org>
2021-10-15 11:15:47 -1000

    trying an explicit Loader of None because yaml now complains about it on Jenkins builds

--- fits_storage/utils/fits_validator.py
+++ fits_storage/utils/fits_validator.py
@@ -795,7 +795,7 @@ class RuleSetFactory(object):
 
         source = self._cls.get_raw(sourcename)
 
-        data = yaml.load(source)
+        data = yaml.load(source, None)
         if not data:
             return self._cls.build_empty(sourcename, *args, **kw)
 
