522ddfef948988ff4231d2579ab1f75ea94ae810
Oliver <oly@oberdorf.org>
2021-10-20 11:10:26 -1000

    had to explicitly add for tuple return

--- date_query_debug.py
+++ date_query_debug.py
@@ -1,7 +1,7 @@
 from gemini_obs_db.utils.gemini_metadata_utils import get_time_period, get_date_offset
 
 print("Checking get_time_period(20130711)")
-print("Got: %s" % get_time_period("20130711"))
+print("Got: %s %s" % get_time_period("20130711"))
 
 print("Checking get_date_offset()")
 print("Got: %s" % get_date_offset())
