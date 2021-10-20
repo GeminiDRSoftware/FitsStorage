863ff50fab80dbb5e6d776b6ef01229e6db9c7c0
Oliver <oly@oberdorf.org>
2021-10-20 13:18:01 -1000

    extra config debug statements

--- date_query_debug.py
+++ date_query_debug.py
@@ -1,7 +1,9 @@
 from fits_storage import fits_storage_config
+from gemini_obs_db import db_config
 from gemini_obs_db.utils.gemini_metadata_utils import get_time_period, get_date_offset
 
-print("Is Archive: %s" % fits_storage_config.is_archive)
+print("Use As Archive: %s" % fits_storage_config.use_as_archive)
+print("Use UTC: %s" % db_config.use_utc)
 
 print("Checking get_time_period(20130711)")
 print("Got: %s %s" % get_time_period("20130711"))
