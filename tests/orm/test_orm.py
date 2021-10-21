da31abf29f0fcc64fe92c678baafdfa1d3877ea8
Oliver <oly@oberdorf.org>
2021-10-21 10:23:04 -1000

    reworked user out of being a fixture to simplify test_orm

--- tests/orm/test_orm.py
+++ tests/orm/test_orm.py
@@ -3,8 +3,8 @@ import sqlalchemy.orm.exc as orm_exc
 
 from fits_storage.orm.user import User
 
-@pytest.fixture(scope='session')
-def user(request, session):
+
+def test_user(session):
     try:
         user = session.query(User).filter(User.username == 'testuser').one()
     except orm_exc.NoResultFound:
@@ -14,14 +14,17 @@ def user(request, session):
 
     return user
 
+
 @pytest.mark.usefixtures("rollback")
 class TestUser:
-    def test_reset_and_validate_password(self, session, user):
+    def test_reset_and_validate_password(self, session):
+        user = test_user(session)
         assert user.password is None  # For a just created user, this should be null
         user.reset_password('foobar')
         assert user.validate_password('foobar') == True
 
-    def test_generate_and_validate_reset_token(self, session, user):
+    def test_generate_and_validate_reset_token(self, session):
+        user = test_user(session)
         assert user.reset_token is None
         token = user.generate_reset_token()
         assert user.validate_reset_token('foo') == False
@@ -29,7 +32,8 @@ class TestUser:
         assert user.validate_reset_token(token) == True
         assert (user.reset_token is None and user.reset_token_expires is None) == True
 
-    def test_log_in(self, session, user):
+    def test_log_in(self, session):
+        user = test_user(session)
         # Test that we start with an empty cookie
         assert user.cookie is None
         # Test that a new cookie has been generated after log in
