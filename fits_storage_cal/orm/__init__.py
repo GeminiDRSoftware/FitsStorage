2a494a2f4c218d1b1a8afcd794bfa2dc43c71fc3
phirst <phirst@gemini.edu>
2023-02-07 16:48:27 -1000

    merge fits_storage_cal repo back into fits_storage

--- fits_storage_cal/orm/__init__.py
+++
@@ -1,7 +0,0 @@
-
-# for back compatibility for DRAGONS
-import gemini_obs_db.orm.file as file
-import gemini_obs_db.orm.diskfile as diskfile
-import gemini_obs_db.orm.preview as preview
-import gemini_obs_db.orm.header as header
-from gemini_obs_db.db import sessionfactory
