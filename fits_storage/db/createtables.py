9281b130cf14c6dc27051bec5d0b2d95923a90a9
phirst <phirst@gemini.edu>
2023-02-15 13:42:26 -1000

    reorg

--- fits_storage/db/createtables.py
+++ fits_storage/db/createtables.py
@@ -5,6 +5,7 @@ in the Fits Storage System.
 from sqlalchemy.orm import Session
 
 import fits_storage.db as db
+import fits_storage.config as fsc
 
 # Importing orm classes here (or even within imports that get called from
 # here) will cause those tables to be created even though there is no
@@ -19,10 +20,18 @@ from fits_storage.core.footprint import Footprint
 from fits_storage.core.fulltextheader import FullTextHeader
 from fits_storage.core.photstandard import PhotStandard
 
+# Calibration instrument classes
+from fits_storage.cal import instruments
+
+# Server tables
+if fsc.is_server:
+    from fits_storage import server
+
 def create_tables(session: Session):
     """
-    Creates the database tables and grants the apache user
-    SELECT on the appropriate ones
+    Creates the database tables appropriate for the configuration.
+    If we're in a server configuration, also handles granting persmissions
+    for the databse user that the web queries run under.
 
     Parameters
     ----------
@@ -30,21 +39,26 @@ def create_tables(session: Session):
         Session to create tables in
     """
 
-    # Create the tables
+    # Create the tables. You only need to call create_all on one orm object,
+    # and all tables for all imported ORM classes will be created.
     Header.metadata.create_all(bind=db._saved_engine)
-    # Gmos.metadata.create_all(bind=db._saved_engine)
-    # Niri.metadata.create_all(bind=db._saved_engine)
-    # Nifs.metadata.create_all(bind=db._saved_engine)
-    # Gnirs.metadata.create_all(bind=db._saved_engine)
-    # F2.metadata.create_all(bind=db._saved_engine)
-    # Ghost.metadata.create_all(bind=db._saved_engine)
-    # Gpi.metadata.create_all(bind=db._saved_engine)
-    # Gsaoi.metadata.create_all(bind=db._saved_engine)
-    # Michelle.metadata.create_all(bind=db._saved_engine)
-    # Nici.metadata.create_all(bind=db._saved_engine)
-    # CalCache.metadata.create_all(bind=db._saved_engine)
 
+    # Add the geometry types separately, because SQLAlchemy only partially
+    # supports these. TODO: Is this still true?
+    # This is postgres specific and referencing these column in sqlite mode
+    # isn't going to work.
+    if not fsc.using_sqlite:
+        db._saved_engine.execute("ALTER TABLE footprint ADD IF NOT EXISTS area polygon;")
+        db._saved_engine.execute("ALTER TABLE photstandard ADD IF NOT EXISTS coords point;")
 
+    # Grant access to server tables for the unprivelidged user that runs the
+    # wsgi code for the web server
+    # TODO: this needs sorting out.
+    # if using_apache and not using_sqlite:
+    # pg_db.execute("GRANT SELECT ON file, diskfile, diskfilereport, header, fulltextheader, gmos, niri, michelle, gnirs, gpi, nifs, f2, gsaoi, nici, tape, tape_id_seq, tapewrite, taperead, tapefile, notification, photstandard, photstandardobs, footprint, qareport, qametriciq, qametriczp, qametricsb, qametricpe, ingestqueue, exportqueue, archiveuser, userprogram, usagelog, querylog, downloadlog, filedownloadlog, fileuploadlog, calcache, preview, obslog, miscfile, ingestqueue, exportqueue, previewqueue, calcachequeue, queue_error, obslog_comment, program, publication, programpublication TO fitsdata;")
+    # pg_db.execute("GRANT INSERT,UPDATE ON tape, notification, qareport, qametriciq, qametriczp, qametricsb, qametricpe, archiveuser, userprogram, usagelog, querylog, downloadlog, filedownloadlog, fileuploadlog, miscfile, ingestqueue, obslog_comment, program, publication, programpublication TO fitsdata;")
+    # pg_db.execute("GRANT UPDATE ON tape_id_seq, notification_id_seq, qareport_id_seq, qametriciq_id_seq, qametriczp_id_seq, qametricsb_id_seq, qametricpe_id_seq, archiveuser_id_seq, userprogram_id_seq, usagelog_id_seq, querylog_id_seq, downloadlog_id_seq, filedownloadlog_id_seq, fileuploadlog_id_seq, ingestqueue_id_seq, obslog_comment_id_seq, program_id_seq, publication_id_seq, programpublication_id_seq TO fitsdata;")
+    # pg_db.execute("GRANT DELETE ON notification, ingestqueue TO fitsdata;")
 def drop_tables(session: Session):
     """
     Drops all the database tables. Very unsubtle. Use with caution
---
+++ fits_storage/server/__init__.py
@@ -0,0 +1,24 @@
+from .user import User
+from .userprogram import UserProgram
+
+from usagelog import UsageLog
+from querylog import QueryLog
+from downloadlog import DownloadLog
+from filedownloadlog import FileDownloadLog
+from fileuploadlog import FileUploadLog
+
+from glacier import Glacier
+from preview import Preview
+from notification import Notification
+from miscfile import MiscFile
+from program import Program
+from publication import Publication
+from programpublication import ProgramPublication
+
+from logcomments import LogComments
+from obslog import Obslog
+from obslog_comment import ObslogComment
+
+from qastuff import QAreport, QAmetricIQ, QAmetricZP, QAmetricSB, QAmetricPE
+
+from tapestuff import Tape, TapeWrite, TapeFile, TapeRead
