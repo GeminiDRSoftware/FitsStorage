323ebabc7f36a105eeaaee31ece53f35117015e4
phirst <phirst@gemini.edu>
2023-04-18 17:24:06 -1000

    fix ingest_standards.py

---
+++ fits_storage/scripts/ingest_standards.py
@@ -0,0 +1,48 @@
+import os
+import datetime
+
+from fits_storage.logger import logger, setdebug, setdemon
+from fits_storage.server.ingest_standards import ingest_standards
+
+from fits_storage.db import session_scope
+
+from fits_storage.config import get_config
+fsc = get_config()
+
+"""
+Read in the list of standards from `standards.txt` and add them to the database.
+"""
+
+if __name__ == "__main__":
+
+    # Option Parsing
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option("--file", action="store", type="string",
+                      dest="filename", help="Standards text filename")
+    parser.add_option("--debug", action="store_true", dest="debug",
+                      default=False, help="Increase log level to debug")
+    parser.add_option("--demon", action="store_true", dest="demon",
+                      default=False, help="Run in background mode")
+
+    (options, args) = parser.parse_args()
+
+    # Logging level to debug?
+    setdebug(options.debug)
+    setdemon(options.demon)
+
+    # Announce startup
+    logger.info("*********  ingest_standards.py - starting up at %s" %
+                datetime.datetime.now())
+
+    if options.filename:
+        filename = options.filename
+    else:
+        filename = os.path.join(fsc.fits_aux_datadir, "standards.txt")
+    logger.info("Reading from file: %s" % filename)
+
+    with session_scope() as session:
+        ingest_standards(session, filename, logger=logger)
+
+    logger.info("*** ingest_standards exiting normally at %s" %
+                datetime.datetime.now())
--- fits_storage/server/ingest_standards.py
+++ fits_storage/server/ingest_standards.py
@@ -2,8 +2,9 @@
 This module provides various utility functions for ingest_standards.py
 in the Fits Storage System.
 """
-from ..orm.photstandard import PhotStandard
-from ..orm.geometryhacks import add_point
+from fits_storage.core.orm.photstandard import PhotStandard
+from fits_storage.core.geometryhacks import add_point
+from fits_storage.logger import DummyLogger
 
 mag_pairs = (
     (4, 'u_mag'),
@@ -21,21 +22,30 @@ mag_pairs = (
 )
 
 
-def ingest_standards(session, filename):
+def ingest_standards(session, filename, logger=DummyLogger()):
     """
     Load the standards text file into the Standards table
     """
 
     # Loop through entries in the standards text file, adding to table
-    for line in open(filename, 'r'):
-        if line[0] != '#':
+    with open(filename, 'r') as standards:
+        for line in standards:
+            if line[0] == '#':
+                continue
+
             fields = line.strip().split(',')
 
-            # Create and populate a standard instance
-            std = session.query(PhotStandard).filter(PhotStandard.name == fields[0]).first()
+            # Do we already have an entry for this standard?
+            std = session.query(PhotStandard).\
+                filter(PhotStandard.name == fields[0]).first()
             if std is None:
+                # Create an entry for it
                 std = PhotStandard()
                 session.add(std)
+            else:
+                logger.warning("Standard %s already exists. Updating from"
+                               "provided file.", std.name)
+            # Populate the details
             try:
                 std.name = fields[0]
                 std.field = fields[1]
@@ -48,8 +58,6 @@ def ingest_standards(session, filename):
                 print("Fields: %s" % str(fields))
                 raise
 
-            # Add to database session
-            # session.add(std)
             session.commit()
 
             # Hack in the geometrical point column
