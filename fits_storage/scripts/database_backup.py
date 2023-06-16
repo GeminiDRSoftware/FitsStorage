8e70de96e142958e2c8ff0186be9787d76a746e3
phirst <phirst@gemini.edu>
2023-06-16 10:55:34 -1000

    cleanups

--- fits_storage/scripts/database_backup.py
+++ fits_storage/scripts/database_backup.py
@@ -13,20 +13,22 @@ from fits_storage.logger import logger, setdebug, setdemon
 Script to backup the Postgres database.
 """
 
-# FitsStorage Configuration
-fsc = get_config()
-
-# Get a single 'now' value to use for consistency
-now = datetime.datetime.now()
-
 if __name__ == "__main__":
     from optparse import OptionParser
     parser = OptionParser()
-    parser.add_option("--dontdelete", action="store_true", dest="dontdelete", help="Don't actually delete any old backups, just say what would be deleted")
-    parser.add_option("--dontbackup", action="store_true", dest="dontbackup", help="Don't back up the database, just clean up")
-    parser.add_option("--exclude-queues", action="store_true", dest="queues", help="Dont dump the queue tables as this conflicts with their locking and causes the queues to hang up during backup.")
-    parser.add_option("--debug", action="store_true", dest="debug", help="Increase log level to debug")
-    parser.add_option("--demon", action="store_true", dest="demon", help="Run as a background demon, do not generate stdout")
+    parser.add_option("--dontdelete", action="store_true", dest="dontdelete",
+                      help="Don't actually delete any old backups, "
+                           "just say what would be deleted")
+    parser.add_option("--dontbackup", action="store_true", dest="dontbackup",
+                      help="Don't back up the database, just clean up")
+    parser.add_option("--exclude-queues", action="store_true", dest="queues",
+                      help="Dont dump the queue tables as this conflicts with "
+                           "their locking and causes the queues to hang up "
+                           "during backup.")
+    parser.add_option("--debug", action="store_true", dest="debug",
+                      help="Increase log level to debug")
+    parser.add_option("--demon", action="store_true", dest="demon",
+                      help="Run as a background demon, do not generate stdout")
 
     (options, args) = parser.parse_args()
 
@@ -34,8 +36,14 @@ if __name__ == "__main__":
     setdebug(options.debug)
     setdemon(options.demon)
 
+    # FitsStorage Configuration
+    fsc = get_config()
+
+    # Get a single 'now' value to use for consistency
+    now = datetime.datetime.now()
+
     # Announce startup
-    logger.info("*********    database_backup.py - starting up at %s" % now)
+    logger.info("*********    database_backup.py - starting up at %s", now)
 
     # Check if destination directory exists
     if not os.path.isdir(fsc.fits_db_backup_dir):
@@ -50,7 +58,7 @@ if __name__ == "__main__":
     # Do the pg_dump backup
     if not options.dontbackup:
 
-        filename = f"{fsc.fits_db_backup_dir}/{fsc.fits._dbname}." \
+        filename = f"{fsc.fits_db_backup_dir}/fitsdata." \
                    f"{datetime.datetime.now().isoformat()}.pg_dump_c"
         logger.debug("Backup Filename is %s", filename)
 
@@ -63,12 +71,12 @@ if __name__ == "__main__":
         logger.debug("Command is: %s", str(command))
 
         logger.info("Executing pg_dump...")
-        sp = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        sp = subprocess.Popen(command, stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE)
         (stdoutstring, stderrstring) = sp.communicate()
         logger.info(stderrstring)
         logger.info(stdoutstring)
         logger.info("... pg_dump complete.")
-
 
     # Cleanup old backup files
     today = now.date()
@@ -77,14 +85,16 @@ if __name__ == "__main__":
 
     for filename in backup_files:
         # Strip filename
-        match = re.match('fitsdata.(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}).(\d{6}).pg_dump_c', filename)
+        match = re.match('fitsdata.(\d{4})-(\d{2})-(\d{2})'
+                         'T(\d{2}):(\d{2}):(\d{2}).(\d{6}).pg_dump_c', filename)
         try:
             datematch = match.groups()[:3]
             y, m, d = int(datematch[0]), int(datematch[1]), int(datematch[2])
             date = datetime.date(y, m, d)
 
             if date > today:
-                logger.error("The file %s has a date larger than today's date" % filename)
+                logger.error("The file %s has a date larger than today's date",
+                             filename)
                 break
 
             time_difference = (today-date).days
@@ -92,26 +102,28 @@ if __name__ == "__main__":
             if time_difference <= 365:
                 # Keep one file per day for the past 10 days
                 if time_difference < 10:
-                    logger.info("This file is less than 10 days old: %s" % filename)
+                    logger.info("This file is less than 10 days old: %s",
+                                filename)
                 # Keep one file every 10 days in the last 2 months
                 elif time_difference < 60 and d in (11, 21):
-                    logger.info("This file is less than 2 months old and falls on 01, 11 or 21 of the month: %s" % filename)
+                    logger.info("This file is less than 2 months old and falls "
+                                "on 01, 11 or 21 of the month: %s", filename)
                 # Keep one file per month for the past year
                 elif d == 1:
-                    logger.info("This file is less than 1 year old and falls on 01 of the month: %s" % filename)
+                    logger.info("This file is less than 1 year old and falls on"
+                                " 01 of the month: %s", filename)
                 else:
                     if options.dontdelete:
-                        logger.info("This file would be deleted: %s" % filename)
+                        logger.info("This file would be deleted: %s", filename)
                     else:
                         os.remove("%s/%s" % (fsc.fits_db_backup_dir, filename))
-                        logger.info("Deleting file: %s" % filename)
+                        logger.info("Deleting file: %s", filename)
             else:
                 if options.dontdelete:
-                    logger.info("This file would be deleted: %s" % filename)
+                    logger.info("This file would be deleted: %s", filename)
                 else:
-                    logger.info("Deleting file: %s" % filename)
+                    logger.info("Deleting file: %s", filename)
                     os.remove("%s/%s" % (fsc.fits_db_backup_dir, filename))
         except AttributeError:
             # No match
-            logger.info("The file %s is not in the expected format." % filename)
-
+            logger.info("The file %s is not in the expected format.", filename)
--- fits_storage/scripts/database_vacuum.py
+++ fits_storage/scripts/database_vacuum.py
@@ -16,8 +16,10 @@ Script to vacuum the Postgres database.
 if __name__ == "__main__":
 
     parser = OptionParser()
-    parser.add_option("--debug", action="store_true", dest="debug", help="Increase log level to debug")
-    parser.add_option("--demon", action="store_true", dest="demon", help="Run as a background demon, do not generate stdout")
+    parser.add_option("--debug", action="store_true", dest="debug",
+                      help="Increase log level to debug")
+    parser.add_option("--demon", action="store_true", dest="demon",
+                      help="Run as a background demon, do not generate stdout")
 
     (options, args) = parser.parse_args()
 
@@ -25,9 +27,9 @@ if __name__ == "__main__":
     setdebug(options.debug)
     setdemon(options.demon)
 
-    # Annouce startup
-    now = datetime.datetime.now()
-    logger.info("*********  database_vacuum.py - starting up at %s" % now)
+    # Announce startup
+    logger.info("*********  database_vacuum.py - starting up at %s",
+                datetime.datetime.now())
 
     command = ["/usr/bin/vacuumdb",
                "--verbose",
@@ -36,11 +38,13 @@ if __name__ == "__main__":
 
     logger.info("Executing vacuumdb")
 
-    sp = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    sp = subprocess.Popen(command, stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE)
+
     (stdoutstring, stderrstring) = sp.communicate()
 
-    logger.info(stderrstring)
+    logger.info(stderrstring.decode('UTF-8', "replace"))
 
-    logger.info(stdoutstring)
+    logger.info(stdoutstring.decode('UTF-8', "replace"))
 
-    logger.info("-- Finished, Exiting")
+    logger.info("-- Finished, Exiting at %s", datetime.datetime.now())
