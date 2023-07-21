4f1a3984e44edd3f181b931973242cadc0d25f7b
phirst <phirst@gemini.edu>
2023-07-21 13:49:18 -1000

    2022 data for gmos cals tests

--- fits_storage_tests/liveserver_tests/test_cals_gmos.py
+++ fits_storage_tests/liveserver_tests/test_cals_gmos.py
@@ -4,23 +4,44 @@ from fits_storage_tests.liveserver_tests.helpers import calhelper
 server = 'https://archive.gemini.edu:/jsoncalmgr'
 
 # This dict of dicts defines the expected calibration associations.
-cals = {
+cals_2022_gmos = {
     # GMOS imaging
-    'GN-2019B-Q-122-23-001': {'bias': 'GN-CAL20200101-24-031',
-                            'flat': 'GN-CAL20191231-30-010',
-                            'photometric_standard': 'GN-CAL20200101-26-004',
-                            'dark': None,
-                            'arc': None,
-                            # TODO - add processed BPM
-                            },
+    'GN-2021B-Q-117-83-001': {'bias': 'GN-CAL20220108-4-031',
+                             'flat': 'GN-CAL20220122-7-004',
+                             'photometric_standard': 'GN-CAL20220108-2-002',
+                             'dark': None,
+                             'arc': None,
+                             # TODO - add processed BPM
+                             },
     # GMOS longslit
-    'GN-2019B-FT-214-24-003': {'bias': 'GN-CAL20200101-24-021',
-                            'flat': 'GN-2019B-FT-214-24-002',
-                            'arc': 'GN-2019B-FT-214-25-001',
-                            'dark': None,
-                            # TODO - add processed BPM
-                            }
+    'GN-2022A-Q-303-9-001': {'bias': 'GN-CAL20220108-4-086',
+                             'flat': 'GN-2022A-Q-303-9-003',
+                             'arc': 'GN-2022A-Q-303-10-001',
+                             'dark': None,
+                             'specphot': 'GN-2022A-Q-303-10-001',
+                             # TODO - add processed BPM
+                             },
+    # GMOS MOS
+    'GN-2021B-Q-107-91-001': {'bias': 'GN-CAL20220108-4-051',
+                              'flat': 'GN-2021B-Q-107-91-004',
+                              'arc': 'GN-2021B-Q-107-50-001',
+                              'dark': None,
+                              'spectwilight': 'GN-2021B-Q-107-89-001',
+                              'mask': 'GN2021BQ107-01',
+                              'slitillum': 'GN-2021B-Q-107-89-001',
+                              # TODO - add processed BPM
+                              },
+    # GMOS IFU
+    'GN-2021B-FT-212-12-002': {'bias': 'GN-CAL20220123-2-001',
+                               'flat': 'GN-2021B-FT-212-12-001',
+                               'arc': 'GN-2021B-FT-212-16-001',
+                               'dark': None,
+                               'spectwilight': 'GN-2021B-FT-212-4-001',
+                               'specphot': 'GN-2021B-FT-212-7-001',
+                               'slitillum': 'GN-2021B-FT-212-4-001',
+                               # TODO - add processed BPM
+                               },
 }
 
 def test_gmoscals():
-    calhelper(server, cals)
+    calhelper(server, cals_2022_gmos)
