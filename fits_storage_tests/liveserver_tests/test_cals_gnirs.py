dbae92101486e04cf9ed77659742e4891082f78a
phirst <phirst@gemini.edu>
2023-07-21 14:27:43 -1000

    add initial niri and gnirs cal association tests

---
+++ fits_storage_tests/liveserver_tests/test_cals_gnirs.py
@@ -0,0 +1,44 @@
+from fits_storage_tests.liveserver_tests.helpers import calhelper
+
+# The server to test against
+server = 'https://archive.gemini.edu:/jsoncalmgr'
+
+# This dict of dicts defines the expected calibration associations.
+cals_2022_gnirs = {
+    # GNIRS Imaging JPHOT
+    'GN-2022A-Q-326-16-001': {'bias': None,
+                              'dark': 'GN-2022A-Q-326-18-001',
+                              'flat': 'GN-2022A-Q-326-16-010',
+                              'arc': None,
+                              'lampoff_flat': 'GN-2022A-Q-326-16-018',
+                              'processed_bpm': 'GN-CAL20121205-10-001-BPM'
+                              },
+    # GNIRS LS 111l/mm J
+    'GN-2021B-Q-212-180-001': {'bias': None,
+                               'dark': None,
+                               'flat': 'GN-2021B-Q-212-180-009',
+                               'arc': 'GN-2021B-Q-212-180-014',
+                               'telluric_standard': 'GN-2021B-Q-212-266-012',
+                               'processed_bpm': 'GN-CAL20121205-10-001-BPM',
+                               },
+    # GNIRS XD 32l/mm H
+    'GN-2021B-Q-308-295-001': {'bias': None,
+                               'dark': None,
+                               'flat': 'GN-2021B-Q-308-295-013',
+                               'arc': 'GN-2021B-Q-308-295-029',
+                               'pinhole_mask': 'GN-2021B-Q-308-238-001',
+                               'processed_bpm': 'GN-CAL20121205-10-001-BPM',
+                               },
+    # GNIRS XD 111l/mm K
+    'GN-2021B-Q-305-18-001': {'bias': None,
+                              'dark': None,
+                              'flat': 'GN-2021B-Q-305-18-005',
+                              'arc': 'GN-2021B-Q-305-27-001',
+                              'pinhole_mask': 'GN-2021B-Q-305-28-001',
+                              'processed_bpm': 'GN-CAL20121205-10-001-BPM',
+                              },
+}
+
+
+def test_gnirscals():
+    calhelper(server, cals_2022_gnirs)
---
+++ fits_storage_tests/liveserver_tests/test_cals_niri.py
@@ -0,0 +1,34 @@
+from fits_storage_tests.liveserver_tests.helpers import calhelper
+
+# The server to test against
+server = 'https://archive.gemini.edu:/jsoncalmgr'
+
+# This dict of dicts defines the expected calibration associations.
+cals_2022_niri = {
+    # NIRI Imaging L'
+    'GN-2022A-Q-131-6-001': {'bias': None,
+                             'dark': 'GN-2022A-Q-131-7-001',
+                             'flat': None,  # L-prime
+                             'arc': None,
+                             'processed_bpm': 'GN-2012B-DD-5-15-099-BPM',
+                             },
+    # NIRI Imaging K
+    'GN-2022A-Q-106-53-001': {'bias': None,
+                              'dark': 'GN-2022A-Q-106-54-001',
+                              'flat': 'GN-2022A-Q-106-50-011',
+                              'arc': None,
+                              'lampoff_flat': 'GN-2022A-Q-106-50-001',
+                              'processed_bpm': 'GN-2012B-DD-5-15-099-BPM',
+                              },
+    # NIRI Imaging J
+    'GN-2021A-Q-319-35-001': {'bias': None,
+                              'dark': 'GN-2021A-Q-319-81-041',
+                              'flat': 'GN-2021A-Q-319-81-011',
+                              'arc': None,
+                              'lampoff_flat': 'GN-2021A-Q-319-81-001',
+                              'processed_bpm': 'GN-2012B-DD-5-15-099-BPM',
+                              },
+}
+
+def test_niricals():
+    calhelper(server, cals_2022_niri)
