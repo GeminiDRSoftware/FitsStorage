ad8fcdb74bf84a31ef735f75b182c74be05cdc40
phirst <phirst@gemini.edu>
2024-05-31 11:01:59 -1000

    test json representation

--- fits_storage_tests/code_tests/test_odb_interface.py
+++ fits_storage_tests/code_tests/test_odb_interface.py
@@ -1,4 +1,5 @@
 import os.path
+import json
 
 import fits_storage_tests
 
@@ -8,11 +9,16 @@ from fits_storage.server.odb_program_interface import get_odb_prog_dicts
 def test_odb_interface():
     sample_file = os.path.join(fits_storage_tests.__path__[0],
                                'odbbrowser_sample.xml')
-
+    json_file = os.path.join(fits_storage_tests.__path__[0],
+                               'odbbrowser_sample.json')
     with open(sample_file, 'r') as f:
         xml = f.read()
 
     result = get_odb_prog_dicts(None, None, xml_inject=xml)
+
+    # Uncomment to write new json file
+    # with open(json_file, 'w') as g:
+    #    json.dump(result, g)
 
     assert isinstance(result, list)
     assert len(result) == 5
@@ -25,4 +31,9 @@ def test_odb_interface():
     assert p0['piEmail'] == 'mm@disney.edu'
     assert p0['csEmail'] == 'csperson@gemini.edu, anothercs@gemini.edu'
     assert p0['ngoEmail'] == 'ngoperson@gemini.edu'
+
+    with open(json_file, 'r') as g:
+        jsontext = g.read()
+
+    assert jsontext == json.dumps(result)
     
