6fcb57d0a22e7328175df980aa331c8901bd14bb
phirst <phirst@gemini.edu>
2024-06-28 14:40:08 -1000

    wsgiref.simple_server bug workaround for when upload file size is less than chunksize

--- fits_storage/web/upload_file.py
+++ fits_storage/web/upload_file.py
@@ -66,6 +66,10 @@ def upload_file(filename, processed_cal=False):
     try:
         with open(fullfilename, 'wb') as f:
             fileuploadlog.ut_transfer_start = datetime.datetime.utcnow()
+            # wsgiref workaround to ensure initial read is not bigger than
+            # content_length if content_length is smaller than chunksize
+            chunksize = content_length \
+                if content_length and content_length < chunksize else chunksize
             while chunk := ctx.input.read(chunksize):
                 size += len(chunk)
                 m.update(chunk)
