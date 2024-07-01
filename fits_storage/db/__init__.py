234a09d8a00ce8a7aafd313b5c760fca43d8b751
teald <teal.dillon@gmail.com>
2024-07-01 15:36:36 -0700

    Use declarative_base from orm.

--- fits_storage/db/__init__.py
+++ fits_storage/db/__init__.py
@@ -3,7 +3,7 @@ from datetime import date, datetime
 
 from sqlalchemy import create_engine, String, Date, DateTime
 from sqlalchemy.dialects import postgresql
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql.sqltypes import NullType
 
