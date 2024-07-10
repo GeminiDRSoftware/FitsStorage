9796a34fffc30f2feeef70edbfd04bdab1d237f0
teald <teal.dillon@gmail.com>
2024-07-10 12:10:21 -0700

    Add derivative _Base class with __allow_unmapped__ = True
    This overrides SQLAlchemy's declarative_base in both instances when
    creating the declarative base class.

--- fits_storage/db/__init__.py
+++ fits_storage/db/__init__.py
@@ -45,7 +45,14 @@ def sessionfactory(reload=False):
     return _saved_sessionfactory()
 
 
-Base = declarative_base()
+# Add __allow_unmapped__ for SQLAlchemy 2.0 compatibility
+class _Base:
+    """Compatibility class that enforces the __allow_unmapped__ attribute in
+    the declarative Base class.
+    """
+    __allow_unmapped__ = True
+
+Base = declarative_base(cls=_Base)
 
 
 @contextmanager
