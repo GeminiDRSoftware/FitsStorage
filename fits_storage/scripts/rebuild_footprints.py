fb82c8ed3ec2d6c8f4787787aa68077b12d8fe2c
phirst <phirst@gemini.edu>
2024-08-22 15:52:22 -1000

    initial rebuild_footprints.py script

---
+++ fits_storage/scripts/rebuild_footprints.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
+from datetime import datetime
+
+from fits_storage.logger import logger, setdebug, setdemon
+
+from fits_storage.db import session_scope
+
+from fits_storage.core.orm.header import Header
+from fits_storage.core.orm.diskfile import DiskFile
+
+from fits_storage.core.orm.footprint import Footprint, footprints
+
+from fits_storage.core.geometryhacks import add_footprint
+
+parser = ArgumentParser(
+    description="This script is used to rebuild the footprints table. "
+                "It is needed after improvements or bugfixes that affect"
+                "calculation of footprints.")
+parser.add_argument("--debug", action="store_true", dest="debug",
+                    help="Increase log level to debug")
+parser.add_argument("--demon", action="store_true", dest="demon",
+                    help="Run as a background demon, do not generate stdout")
+parser.add_argument("--canonical", action="store_true", dest="canonical",
+                    help="Only add entries for footprints from canonical "
+                         "diskfiles")
+args = parser.parse_args()
+
+# Logging level to debug? Include stdio log?
+setdebug(args.debug)
+setdemon(args.demon)
+
+# Announce startup
+logger.info("***   rebuild_footprints.py - starting up at %s", datetime.now())
+
+with session_scope() as session:
+    # First up, we need to get a list of Headers that we're going to process.
+    query = session.query(Header)
+    if args.canonical:
+        query = query.join(DiskFile).filter(DiskFile.canonical == True)
+
+    headers = query.all()
+    n = len(headers)
+    logger.info("Got %d headers to process", n)
+
+    i = 0
+    for header in headers:
+        i += 1
+
+        diskfile = header.diskfile
+        ad = diskfile.get_ad_object()
+
+        for label, fp in footprints(diskfile.ad_object, logger).items():
+            footprint = Footprint(header, label)
+            session.add(footprint)
+            session.flush()
+            add_footprint(session, footprint.id, fp)
+        if i % 1000 == 0:
+            logger.info("Processed hid %d (%d / %d)", header.id, i, n)
+            session.commit()
+
+    logger.info("Processed hid %d (%d / %d)", header.id, i, n)
+    session.commit()
