1f08e6fe3775d098605b75f94ec8a1df1d93f216
phirst <phirst@gemini.edu>
2024-12-27 15:36:39 -1000

    login confirmation working version

--- htmldocroot/login.js
+++ htmldocroot/login.js
@@ -2,7 +2,7 @@ function copyCookie() {
   var cookieDiv = document.getElementById("cookie");
   var cookieText = cookieDiv.textContent;
   navigator.clipboard.writeText(cookieText);
-};
+}
 
 function showCookie() {
   var cookieText = document.getElementById("cookie");
@@ -15,23 +15,27 @@ function showCookie() {
 }
 
 function initConfirm() {
+    // Are we logged in?
     var cookieDiv = document.getElementById("cookie");
     if (cookieDiv) {
-        confirm("CookieDiv");
-    } else {
-        confirm("NoCookieDiv");
+        $(".loginlink").on("click", doConfirm);
     }
-    $(".loginlink").on("click", doConfirm)
 }
 
 function doConfirm() {
-    var confirmstring = "You are already logged in. Your account currently has the following authentication methods defined: ";
-    // confirmstring += methods.textContent;
-    confirmstring += ". Logging in by an another method will attach the new login to the archive account you are currently logged in as. ";
-    // confirmstring += "You should only do this if the new login is the same person as your current login. If you intent is to log in as ";
-    // confirmstring += "a different user, you should Cancel this and Log Out of your current session first."
+    var login_methods = document.getElementById("login_methods");
+    if (login_methods) {
+        var login_methods_text = login_methods.textContent;
+    } else {
+        var login_methods_text = "Unknown";
+    }
+    var confirmstring = "You are already logged in and your account currently has the following authentication methods defined: ";
+    confirmstring += login_methods.textContent;
+    confirmstring += ". \n\nLogging in by an another method will attach the new login to the archive account you are currently logged in as.\n\n";
+    confirmstring += "You should only OK this if the new login is for the same person as your current login.\n\nIf you intent is to log in as ";
+    confirmstring += "a different user, Cancel this and Log Out of your current session first.";
 
-    return confirm(confirmstring)
+    return confirm(confirmstring);
 }
 
 $(document).ready(initConfirm)
