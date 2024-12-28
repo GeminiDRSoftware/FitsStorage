9dfee8a67d94b809bcbdef325f08c1ad17045384
phirst <phirst@gemini.edu>
2024-12-27 15:14:33 -1000

    idiot testing javascript code

--- htmldocroot/login.js
+++ htmldocroot/login.js
@@ -14,17 +14,24 @@ function showCookie() {
   }
 }
 
-$(document).ready(function(){
-    // Find if the user is logged in
-    var methods = document.getElementById("login_methods")
-    if (methods) {
-        var confirmstring = "You are already logged in. Your account currently has the following authentication methods defined: ";
-        confirmstring += methods.textContent;
-        confirmstring += ". Logging in by an another method will attach the new login to the archive account you are currently logged in as. ";
-        confirmstring += "You should only do this if the new login is the same person as your current login. If you intent is to log in as ";
-        confirmstring += "a different user, you should Cancel this and Log Out of your current session first."
-        $(".loginlink").on("click", function(){
-            return confim(confirmstring);
-        })
+function initConfirm() {
+    var cookieDiv = document.getElementById("cookie");
+    if (cookieDiv) {
+        confirm("CookieDiv");
+    } else {
+        confirm("NoCookieDiv");
     }
-});
+    $(".loginlink").on("click", doConfirm)
+}
+
+function doConfirm() {
+    var confirmstring = "You are already logged in. Your account currently has the following authentication methods defined: ";
+    // confirmstring += methods.textContent;
+    confirmstring += ". Logging in by an another method will attach the new login to the archive account you are currently logged in as. ";
+    // confirmstring += "You should only do this if the new login is the same person as your current login. If you intent is to log in as ";
+    // confirmstring += "a different user, you should Cancel this and Log Out of your current session first."
+
+    return confirm(confirmstring)
+}
+
+$(document).ready(initConfirm)
