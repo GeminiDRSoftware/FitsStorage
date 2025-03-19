fc249cdcf323ad90cbea61ebd5224769f88c0462
phirst <phirst@gemini.edu>
2025-03-19 10:04:44 -1000

    add GPP nightCal obstype to searchform

--- fits_storage/web/searchform.py
+++ fits_storage/web/searchform.py
@@ -912,7 +912,8 @@ dropdown_options = {
          ("progCal", "progCal"),
          ("dayCal", "dayCal"),
          ("partnerCal", "partnerCal"),
-         ("acqCal", "acqCal")],
+         ("acqCal", "acqCal"),
+         ("nightCal", "nightCal")],
     "obs_typ_options":
         [("OBJECT", "OBJECT"),
          ("BIAS", "BIAS"),
