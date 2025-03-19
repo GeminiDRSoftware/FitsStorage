43a5bef1cffb1f3dc7a838e21bb684a7f99326cc
phirst <phirst@gemini.edu>
2025-03-19 07:40:08 -1000

    include path in log message

--- fits_storage/server/fileops.py
+++ fits_storage/server/fileops.py
@@ -138,7 +138,7 @@ def ingest_upload(args, session, logger):
 
     # Queue it for ingest. We can pass no_defer=True here as we know that
     # the file is complete and ready to go.
-    logger.info("Queueing %s for Ingest", filename)
+    logger.info(f"Queueing {path}/{filename} for Ingest")
     iq = IngestQueue(session, logger)
     iqe = iq.add(filename, path, no_defer=True)
 
