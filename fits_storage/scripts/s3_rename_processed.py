a9b1b80b4853597a065a8911c6f1f696227576a5
phirst <phirst@gemini.edu>
2025-03-21 13:33:18 -1000

    initial s3_rename script

---
+++ fits_storage/scripts/s3_rename_processed.py
@@ -0,0 +1,91 @@
+#! /usr/bin/env python3
+
+import datetime
+
+from fits_storage.logger import logger, setdebug, setdemon
+from fits_storage.config import get_config
+
+from fits_storage.db import sessionfactory
+from fits_storage.core.orm.diskfile import DiskFile
+
+fsc = get_config()
+
+def moveorlist(df, move=False, dest=None):
+    if move:
+        if dest is None:
+            raise ValueError("Refusing to move to destinaion=None")
+        oldkey = df.keyname
+        newkey = f"{options.dest}/{df.filename}"
+        logger.info(f"Rename S3 key: {oldkey} to {newkey}")
+        try:
+            s3.rename(oldkey, newkey)
+            df.path = dest
+        except Exception:
+            logger.error("S3 rename failed", exc_info=True)
+            raise
+    else:
+        logger.info(f"Filename: {df.filename} at path: {df.path}")
+
+
+if __name__ == "__main__":
+    # Option Parsing
+    from argparse import ArgumentParser
+    # ------------------------------------------------------------------------------
+    parser = ArgumentParser()
+
+    parser.add_argument("--debug", action="store_true", dest="debug",
+                        help="Increase log level to debug")
+
+    parser.add_argument("--demon", action="store_true", dest="demon",
+                        help="Run in the background, do not generate stdout")
+
+    parser.add_argument("--dest", action="store", dest="dest", default="",
+                        help="Destination folder (prefix) to use")
+
+    parser.add_argument("--by-filepre", action="store", dest="filepre",
+                        default=None, help="Select files by (case sensitive) "
+                                           "filename prefix. Note - filename, "
+                                           "not S3 keyname")
+    parser.add_argument("--current-path", action="store", dest="currentpath",
+                        default=None, help="Select files on current path."
+                                           "Leave blank to specify files in"
+                                           "root folder only")
+
+    parser.add_argument("--move", action="store_true", dest="move",
+                        default=False, help="move files and update database")
+
+    options = parser.parse_args()
+
+    # Logging level to debug? Include stdio log?
+    setdebug(options.debug)
+    setdemon(options.demon)
+
+    # Announce startup
+    logger.info("*** s3_rename_processed.py - starting up at {}"
+                .format(datetime.datetime.now()))
+    logger.debug("Config files used: %s", ', '.join(fsc.configfiles_used))
+
+
+    if fsc.using_s3:
+        from fits_storage.server.aws_s3 import get_helper
+        s3 = get_helper()
+    else:
+        logger.error("Not an S3 configuration")
+        exit()
+
+    session = sessionfactory()
+    if options.filepre == 'by-filepre':
+        query = session.query(DiskFile).filter(DiskFile.present==True) \
+            .filter(DiskFile.filename.startswith(options.filepre))
+
+        if options.currentpath is None:
+            query = query.filter(DiskFile.path=="")
+        else:
+            query = query.filter(DiskFile.path==options.currentpath)
+
+        for df in query:
+            try:
+                moveorlist(df, move=options.move, dest=options.dest)
+            except:
+                break
+        session.commit()
