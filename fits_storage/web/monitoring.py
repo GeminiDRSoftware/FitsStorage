0e9f24665c2f5749acd19d31f326ebd8546e9c93
phirst <phirst@gemini.edu>
2025-05-20 16:28:42 -1000

    extra fields in monitoring report

--- fits_storage/web/monitoring.py
+++ fits_storage/web/monitoring.py
@@ -37,7 +37,10 @@ def monitoring(thing):
                 results.append(result)
             filename = row.filename
             label = row.label
-            result = {'filename': filename, 'label': label}
+            result = {'filename': filename, 'label': label,
+                      'data_label': row.data_label,
+                      'ut_datetime': row.ut_datetime,
+                      'ad_id': row.adid}
         # If we want this rows keyword, grab it now
         if row.keyword in keywords:
             result[row.keyword] = row.get_value()
@@ -46,10 +49,10 @@ def monitoring(thing):
         results.append(result)
 
     # Transmit the results
-    items = ['filename', 'label', *keywords]
+    items = ['filename', 'label', 'data_label', 'ut_datetime', 'ad_id',
+             *keywords]
     ctx.resp.append('# ' +  '\t'.join(items) + '\n')
     for result in results:
-        line = ""
         resultitems = [str(result[item]) for item in items]
         line = '\t'.join(resultitems)
         line += '\n'
