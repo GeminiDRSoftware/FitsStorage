0145c643376305e971d8d29bf4903bf2a182f0b2
Paul Hirst <phirst@gemini.edu>
2025-07-22 17:26:26 -1000

    WIP: s3_delete_previews.py

--- bespoke_housekeeping_scripts/s3_delete_previews.py
+++ bespoke_housekeeping_scripts/s3_delete_previews.py
@@ -1,84 +1,58 @@
 import datetime
 import sys
 
-from sqlalchemy import join, desc
-
-from fits_storage.fits_storage_config import using_s3
+from fits_storage.config import get_config
 from fits_storage.logger import logger, setdebug, setdemon
-from fits_storage.utils.aws_s3 import get_helper
 
-from multiprocessing import Pool, Process, Queue
-
+fsc = get_config()
+if fsc.using_s3:
+    from fits_storage.server.aws_s3 import Boto3Helper
+    s3 = Boto3Helper()
 
 if __name__ == "__main__":
 
     # Option Parsing
-    from optparse import OptionParser
-    parser = OptionParser()
-    #parser.add_option("--file-pre", action="store", type="string", dest="filepre", help="File prefix to operate on, eg N20090130, N200812 etc")
-    parser.add_option("--yesimsure", action="store_true", dest="yesimsure", default=False, help="Needed for sanity check")
-    parser.add_option("--dryrun", action="store_true", dest="dryrun", help="Dry Run - do not actually do anything")
-    parser.add_option("--debug", action="store_true", dest="debug", help="Increase log level to debug")
-    parser.add_option("--demon", action="store_true", dest="demon", help="Run as a background demon, do not generate stdout")
-    parser.add_option("--count", action="store_true", dest="count", help="Only count the number of previews, do not delete them")
-    parser.add_option("--threads", action="store", dest="threads", help="Run in parallel with this many threads")
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("--filepre", action="store", type="string", dest="filepre", help="File prefix to operate on, eg N20090130, N200812 etc")
+    parser.add_argument("--dryrun", action="store_true", dest="dryrun", help="Dry Run - do not actually do anything")
+    parser.add_argument("--debug", action="store_true", dest="debug", help="Increase log level to debug")
+    parser.add_argument("--demon", action="store_true", dest="demon", help="Run as a background demon, do not generate stdout")
+    parser.add_argument("--delete", action="store_true", dest="delete", help="Actually delete files rather than just count them")
 
-    options, args = parser.parse_args()
+    options = parser.parse_args()
 
     # Logging level to debug? Include stdio log?
     setdebug(options.debug)
     setdemon(options.demon)
 
-    def feed_names():
-        # Get a full listing from S3. The preview files might not be in the DB.
-        logger.info("Getting file list from S3")
-
-        for obj in s3.bucket.objects.all():
-            name = obj.key
-            if name.endswith("_preview.jpg"):
-                logger.info("Found {}".format(name))
-                yield name
-
-    # Annouce startup
+    # Announce startup
     logger.info("*********    s3_delete_previews.py - starting up at %s" % datetime.datetime.now())
 
-    if not using_s3:
+    if not fsc.using_s3:
         logger.error("This script is only useable on installations using S3 for storage")
         sys.exit(1)
 
-    if not (options.yesimsure or options.count):
-        logger.info("This is a really dangerous script to run. If you're not sure, don't do this.")
-        logger.info("This will unconditionally delete files from the S3 storage")
-        logger.error("You need to say --yesimsure to make it work")
-        sys.exit(2)
-
-    s3 = get_helper()
-    #filelist = filter(lambda n: n.endswith("_preview.jpg"), s3.key_names())
-
-    if options.count:
-        logger.info("Found %d preview files", len(filelist))
-        sys.exit(0)
-
-    if options.dryrun:
-        def delete_it(filename):
-            return "Dryrun - not actually Deleting, {}".format(filename)
+    if options.filepre:
+        logger.info("Querying files from S3 bucket by filepre")
+        fulllist = s3.key_names_with_prefix(options.filepre)
     else:
-        def delete_it(filename):
-    #        if logger:
-    #            logger.info("Deleting %s", filename)
-    #        else:
-    #            print "Deleting %s" % filename
-            s3.get_key(filename).delete()
-            return "Removed {}".format(filename)
+        logger.info("Querying files for ingest from S3 bucket")
+        fulllist = s3.key_names()
 
-    if options.threads:
-        threads = int(options.threads)
-        logger.info("Starting parallel delete with %d threads", threads)
-        pool = Pool(threads)
-        for result in pool.imap_unordered(delete_it, feed_names(), chunksize=100):
-            logger.info(result)
+    logger.info(f"Got total of {len(fulllist)} files to consider")
+
+    previews = []
+    for f in fulllist:
+        if f.endswith("_preview.jpg"):
+            previews.append(f)
+            logger.debug(f"{f} is a preview file")
+        else:
+            logger.debug(f"{f} is not a preview file")
+
+    if options.delete:
+        logger.info("Delete not implemented yet")
     else:
-        for filename in feed_names():
-            logger.info(delete_it(filename))
+        logger.info("Found %d preview files", len(fulllist))
 
     logger.info("** s3_delete_previews.py exiting normally")
