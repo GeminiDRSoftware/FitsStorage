40e505450a149a4447886dd83799bed8529ff992
Paul Hirst <phirst@gemini.edu>
2025-10-22 16:30:16 -1000

    add OVERRDNS header in monitoring. Requires DRAGONS 4.1

--- fits_storage/server/monitoring.py
+++ fits_storage/server/monitoring.py
@@ -8,7 +8,8 @@ name as the outpur suffix - ie checkBias1 writes _checkBias1.fits files.
 
 # These tell us the keywords that are recorded by each recipe
 recipe_keywords = {
-    'checkBiasOSCO': ['OVERSCAN', 'OVERRMS', 'OSCOMEAN', 'OSCOSTDV', 'OSCOMED'],
+    'checkBiasOSCO': ['OVERSCAN', 'OVERRMS', 'OVERRDNS',
+                      'OSCOMEAN', 'OSCOSTDV', 'OSCOMED'],
     'checkBiasBICO': ['BICOMEAN', 'BICOSTDV', 'BICOMED'],
     'checkBiasSTCO': ['STCOMEAN', 'STCOSTDV', 'STCOMED'],
     'checkFlatCounts': ['FLATMEAN', 'FLATSTDV', 'FLATMED'],
@@ -17,7 +18,8 @@ recipe_keywords = {
 # These tell us the keywords to include (if they are present) in each report
 # type
 report_keywords = {
-    'checkBias': ['OVERSCAN', 'OVERRMS', 'OSCOMEAN', 'OSCOSTDV', 'OSCOMED',
+    'checkBias': ['OVERSCAN', 'OVERRMS', 'OVERRDNS',
+                  'OSCOMEAN', 'OSCOSTDV', 'OSCOMED',
                   'BICOMEAN', 'BICOSTDV', 'BICOMED',
                   'STCOMEAN', 'STCOSTDV', 'STCOMED'],
     'checkFlat': ['FLATMEAN', 'FLATSTDV', 'FLATMED'],
