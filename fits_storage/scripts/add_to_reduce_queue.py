220282169694891c8d5c663c436dcfbe2a04bbb9
Paul Hirst <phirst@gemini.edu>
2025-11-06 13:46:23 -1000

    memory use seems too cautious, back off overhead allowance

--- fits_storage/scripts/add_to_reduce_queue.py
+++ fits_storage/scripts/add_to_reduce_queue.py
@@ -22,9 +22,9 @@ from fits_storage.server.reduce_list import parse_listfile
 # Helper function for memory estimate from number of pixels
 def memory_estimate(numpixlist):
     # Assume raw data becomes 12 bytes per pix: 4 data, 4 var, 2 dq, 2 objmask
-    # Add one file for the reduced product. Assume 20% overhead / safety margin
+    # Add one file for the reduced product. Assume 10% overhead / safety margin
     numpix = sum(numpixlist) + numpixlist[0] # For the reduced product
-    gb = 1.2 * 12 * numpix / 1E9
+    gb = 1.1 * 12 * numpix / 1E9
     return gb
 
 if __name__ == "__main__":
