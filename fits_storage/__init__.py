d3512acf0ea53324bad7056d3bcbb9c97ac587a9
Paul Hirst <phirst@gemini.edu>
2026-02-24 14:53:38 -1000

    fix version numbers

--- fits_storage/__init__.py
+++ fits_storage/__init__.py
@@ -1,5 +1,5 @@
 # Don't forget to also update pyproject.toml when you update this
-__version__ = '3.x.x'
+__version__ = '3.6.x-dev'
 
 # Convenience utcnow() function since this was deprecated in python 3.12, and
 # we use this a lot with timezone-naieve database columns that are defined to
