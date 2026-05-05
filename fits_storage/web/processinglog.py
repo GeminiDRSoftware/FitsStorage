f8dc04edcf9c145222987d9e2e55a28b16b6eb39
Paul Hirst <phirst@gemini.edu>
2026-05-04 14:24:03 -1000

    add search feature on processinglog web endpoint

--- fits_storage/web/processinglog.py
+++ fits_storage/web/processinglog.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select, join
+from sqlalchemy import select, join, or_
 
 from fits_storage.server.orm.processinglog import ProcessingLog, ProcessingLogFile
 
@@ -10,15 +10,25 @@ def processinglog(thing):
 
     processinglog_id = None
     filename = None
+    search = False
     try:
         processinglog_id = int(thing)
     except ValueError:
         filename = thing
 
+    if filename.startswith('search='):
+        search = True
+        filename = filename[7:]
+
     ctx = get_context()
 
     if processinglog_id:
         stmt = select(ProcessingLog).where(ProcessingLog.id == processinglog_id)
+    elif search:
+        stmt = (select(ProcessingLog)
+                .where(or_(ProcessingLog.input_files.contains(filename),
+                           ProcessingLog.output_files.contains(filename)))
+                )
     else:
         stmt = (select(ProcessingLog)
                 .select_from(join(ProcessingLog, ProcessingLogFile))
