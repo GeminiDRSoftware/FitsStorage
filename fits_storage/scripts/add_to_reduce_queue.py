c87bea19fe91f91e01ba1e7c343b78010a7fff16
Paul Hirst <phirst@gemini.edu>
2026-05-19 09:42:39 -1000

    report explicitly if no files found rather than falling through to calling it an invalid selectiongroup

--- fits_storage/scripts/add_to_reduce_queue.py
+++ fits_storage/scripts/add_to_reduce_queue.py
@@ -211,6 +211,8 @@ if __name__ == "__main__":
             # is really slow if the list is big.
             logger.info("Building filename lists")
             lists = []
+            if len(headers) == 0:
+                logger.info("No files found matching selection criteria")
             if options.selectiongroup is None or options.selectiongroup == 'None':
                 logger.info("Adding each file as an individual reducequeue entry")
                 for header in headers:
